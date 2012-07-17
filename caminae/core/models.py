9957c64ba88963886bc7c65a755cdd1751b11f1e
Gilles Bassière <gilles.bassiere@makina-corpus.com>
2012-07-17 17:36:08 +0200

    Update models for path and topology (#85)

--- caminae/core/models.py
+++ caminae/core/models.py
@@ -8,11 +8,17 @@ from django.conf import settings
 
 class Path(models.Model):
     geom = models.LineStringField(srid=settings.SRID, spatial_index=False)
-    date_insert = models.DateField(editable=False)
-    date_update = models.DateField(editable=False)
-    valid = models.BooleanField(db_column='troncon_valide')
-    code = models.CharField(null=True, max_length=2)
-    name = models.CharField(null=True, max_length=128, db_column='nom')
+    geom_cadastre = models.LineStringField(null=True, srid=settings.SRID, spatial_index=False)
+    date_insert = models.DateField(auto_now_add=True)
+    date_update = models.DateField(auto_now=True)
+    valid = models.BooleanField(db_column='troncon_valide', default=True)
+    name = models.CharField(null=True, max_length=20, db_column='nom_troncon')
+    comments = models.TextField(null=True, db_column='remarques')
+
+    # Override default manager
+    objects = models.GeoManager()
+
+    # Computed values (managed at DB-level with triggers)
     length = models.IntegerField(editable=False, db_column='longueur')
     ascent = models.IntegerField(
             editable=False, db_column='denivelee_positive')
@@ -23,7 +29,6 @@ class Path(models.Model):
     max_elevation = models.IntegerField(
             editable=False, db_column='altitude_maximum')
 
-    objects = models.GeoManager()
     class Meta:
         db_table = 'troncons'
 
@@ -32,11 +37,15 @@ class TopologyMixin(models.Model):
     date_update = models.DateField(editable=False)
     troncons = models.ManyToManyField(Path, through='PathAggregation')
     offset = models.IntegerField(db_column='decallage')
-    length = models.FloatField(editable=False, db_column='longueur')
     deleted = models.BooleanField(db_column='supprime')
+
+    # Override default manager
+    objects = models.GeoManager()
+
+    # Computed values (managed at DB-level with triggers)
+    length = models.FloatField(editable=False, db_column='longueur')
     geom = models.LineStringField(
             editable=False, srid=settings.SRID, spatial_index=False)
-    objects = models.GeoManager()
     class Meta:
         db_table = 'evenements'
 
@@ -45,5 +54,8 @@ class PathAggregation(models.Model):
     topo_object = models.ForeignKey(TopologyMixin, null=False, db_column='evenement')
     start_position = models.FloatField(db_column='pk_debut')
     end_position = models.FloatField(db_column='pk_fin')
+
+    # Override default manager
+    objects = models.GeoManager()
     class Meta:
         db_table = 'evenements_troncons'
