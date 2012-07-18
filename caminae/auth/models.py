a34c3ca79ad95df2e313d9473ab2d2cc417cbcf8
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2012-07-18 09:19:31 +0200

    Structure related class (ref #15)

--- caminae/auth/models.py
+++ caminae/auth/models.py
@@ -7,6 +7,9 @@ from django.contrib.auth.signals import user_logged_in
 
 
 class Structure(models.Model):
+    """
+    Represents an organisational structure, to which users are related.
+    """
     name = models.CharField(max_length=256)
 
     def __unicode__(self):
@@ -17,12 +20,27 @@ def default_structure():
     return Structure.objects.get_or_create(name=settings.DEFAULT_STRUCTURE_NAME)[0]
 
 
-class UserProfile(models.Model):
+class StructureRelated(models.Model):
+    """
+    A mixin used for any entities that belong to a structure
+    """
+    structure = models.ForeignKey(Structure, default=default_structure)
+
+    @classmethod
+    def forUser(cls, user):
+        qs = cls.objects.get_query_set()
+        return qs.filter(structure=user.profile.structure)
+
+    class Meta:
+        abstract = True
+
+
+class UserProfile(StructureRelated):
     user = models.OneToOneField(User, unique=True)
 
-    language = models.CharField(_('Language'), max_length=10, choices=settings.LANGUAGES,
+    language = models.CharField(_('Language'), max_length=10,
+                                choices=settings.LANGUAGES,
                                 default=settings.LANGUAGE_CODE)
-    structure = models.ForeignKey(Structure, default=default_structure)
 
 User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
 
@@ -32,20 +50,3 @@ def lang(sender, **kwargs):
     """ Set user's language in session when he logs in. """
     lang_code = kwargs['user'].profile.language
     kwargs['request'].session['django_language'] = lang_code
-
-
-class StructureRelatedManager(models.Manager):
-    def for_user(self, user):
-        qs = super(StructureRelatedManager, self).get_query_set()
-        return qs.filter(structure=user.profile.structure)
-
-
-class StructureRelated(models.Model):
-    """A mixin used for any entities that belong to a structure"""
-
-    structure = models.ForeignKey(Structure, default=default_structure)
-
-    objects = StructureRelatedManager()
-
-    class Meta:
-        abstract = True
