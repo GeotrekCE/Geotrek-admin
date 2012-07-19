362b78c41237289785d49dc5968bb9dc04f27848
Simon Thépot <simon.thepot@gmail.com>
2012-07-19 14:41:30 +0200

    Maintenance admin: small ModelAdmin config - get rid of default field for translation

--- caminae/maintenance/admin.py
+++ caminae/maintenance/admin.py
@@ -2,14 +2,30 @@
 
 
 from django.contrib import admin
+from modeltranslation.admin import TranslationAdmin
 
 from caminae.maintenance.models import (
         Contractor, InterventionStatus, InterventionTypology, InterventionDisorder
 )
 
 
+class InterventionStatusAdmin(TranslationAdmin):
+    list_display = ('status',)
+    search_fields = ('status', )
+
+
+class InterventionTypologyAdmin(TranslationAdmin):
+    list_display = ('typology',)
+    search_fields = ('typology',)
+
+
+class InterventionDisorderAdmin(TranslationAdmin):
+    list_display = ('disorder',)
+    search_fields = ('disorder',)
+
+
 admin.site.register(Contractor)
-admin.site.register(InterventionStatus)
-admin.site.register(InterventionTypology)
-admin.site.register(InterventionDisorder)
+admin.site.register(InterventionStatus, InterventionStatusAdmin)
+admin.site.register(InterventionTypology, InterventionTypologyAdmin)
+admin.site.register(InterventionDisorder, InterventionDisorderAdmin)
 
