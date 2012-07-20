31c48234a23f3d88fc4d54403d2d069e42e937fb
Sylvain Beorchia <sylvain.beorchia@makina-corpus.com>
2012-07-20 14:55:28 +0200

    Poc ll39 tests

--- poc/ll93/js/main.js
+++ poc/ll93/js/main.js
@@ -28,7 +28,7 @@ function init_map_setup(footpath_geojson_data) {
     footpath_layer = new L.GeoJSON();
     footpath_layer.addData(footpath_geojson_data);
 
-    var mapUrl = 'http://localhost/cgi-bin/tilecache.cgi/1.0.0/scan25/{z}/{x}/{y}.png';
+    var mapUrl = 'http://localhost/cgi-bin/tilecache.cgi/1.0.0/scan100/{z}/{x}/{y}.png';
     var attrib = 'Ecrins';
     var tilelayer = new L.TileLayer(mapUrl, {
         //scheme: 'xyz'
@@ -39,6 +39,14 @@ function init_map_setup(footpath_geojson_data) {
         ,attribution: attrib
     });
 
+    // Tilecache.cfg :
+    // 700000,6325197,1060000,6617738
+    // 1142.7383
+
+    // L93 to 4326, on convertit l'origine pour voir
+    // 700000 , 6325197   => 3.0000000000000004 , 44.0257746719149
+    
+    
     var res = [131072, 65536, 32768, 16384, 8192, 4096, 2048, 1024, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1, 0.5, 0.25];
     var start = new L.LatLng(44.818, 6.2993);
     var map = new L.Map('map', {
@@ -50,21 +58,24 @@ function init_map_setup(footpath_geojson_data) {
             //,new L.Transformation(1, 700000, 1, -6600000))
             //,new L.Transformation(1, -997000, -1, 6460000))
             //, new L.Transformation(1 / 360, 0.5, -1 / 360, 0.5))
-            , new L.Transformation(1 / 20000, 700000, -1 / 20000, 6600000))
+            //, new L.Transformation(1 / 20000, 700000, -1 / 20000, 6617738))
+            //, new L.Transformation(1 / 170000, -4.2588, -1 / 170000, 38.2579))
+            //, new L.Transformation(1 / 170000, 3.0000000000000004, -1 / 170000, 44.0257746719149))
+            , new L.Transformation(1 / 1142.7383, 3.0000000000000004, -1 / 1142.7383, 44.0257746719149))            
 
         ,scale: function(zoom) {
             return 1 / res[zoom];
         }
         ,continuousWorld: true
         ,'layers': [ 
-                     wms_scan25,
-                     wms_scan100,
                      tilelayer , 
+                     //wms_scan25,
+                     //wms_scan100,
                      new L.Marker(start, { title: 'Test' })
                    ]
         ,scheme: 'wms'
         ,center: start
-        ,zoom : 0
+        ,zoom : 5
         }
     );
     
@@ -77,4 +88,11 @@ function init_map_setup(footpath_geojson_data) {
     map.on('mousemove', function(e) {
         document.getElementById("infos").innerHTML = e.latlng + " - Zoom = " + map.getZoom();
     });
+
+    map.on('click', function(e) {
+        var marker = new L.Marker(e.latlng);
+        map.addLayer(marker);
+    });
+
+    
 }
