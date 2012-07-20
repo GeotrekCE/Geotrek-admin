b408f70638a6c1321de698de7596e1d1aeb3f0f4
Simon Thépot <simon.thepot@gmail.com>
2012-07-20 11:42:07 +0200

    authent: marked translation for model and fields. Added simple unicode methods.

--- caminae/authent/models.py
+++ caminae/authent/models.py
@@ -10,10 +10,14 @@ class Structure(models.Model):
     """
     Represents an organisational structure, to which users are related.
     """
-    name = models.CharField(max_length=256)
+    name = models.CharField(max_length=256, verbose_name=_(u"Nom"))
 
     def __unicode__(self):
         return self.name
+
+    class Meta:
+        verbose_name = _(u"Structure")
+        verbose_name_plural = _(u"Structures")
 
 
 def default_structure():
@@ -30,7 +34,7 @@ class StructureRelated(models.Model):
     """
     A mixin used for any entities that belong to a structure
     """
-    structure = models.ForeignKey(Structure, default=default_structure)
+    structure = models.ForeignKey(Structure, default=default_structure, verbose_name=_(u"Related structure")
 
     objects = models.Manager()
     in_structure = StructureRelatedManager()
@@ -41,14 +45,22 @@ class StructureRelated(models.Model):
 
     class Meta:
         abstract = True
+        verbose_name = _(u"Related structures")
+        verbose_name_plural = _(u"Related structure")
 
 
 class UserProfile(StructureRelated):
     user = models.OneToOneField(User, unique=True)
 
-    language = models.CharField(_('Language'), max_length=10,
+    language = models.CharField(_(u"Language"), max_length=10,
                                 choices=settings.LANGUAGES,
                                 default=settings.LANGUAGE_CODE)
+    class Meta:
+        verbose_name = _(u"User's profile")
+        verbose_name_plural = _(u"User's profiles")
+
+    def __unicode__(self):
+        return _("Profile for %s") % self.user
 
 User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
 
