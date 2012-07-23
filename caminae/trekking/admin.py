17687206cccc33d6d33eeed994c138f7f531bad9
Simon Thépot <simon.thepot@gmail.com>
2012-07-23 14:14:23 +0200

    Trekking: basic registration of models in admin

---
+++ caminae/trekking/admin.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+
+from django.contrib import admin
+from modeltranslation.admin import TranslationAdmin
+
+from .models import (
+    Trek, TrekNetwork, Usage, Route, DifficultyLevel, Destination, WebLink
+)
+
+
+
+class TrekAdmin(TranslationAdmin):
+    list_display = ('name', 'departure', 'arrival', )
+    search_fields = ('name', 'departure', 'arrival', )
+
+
+class TrekNetworkAdmin(TranslationAdmin):
+    list_display = ('network',)
+    search_fields = ('network',)
+
+
+class UsageAdmin(TranslationAdmin):
+    list_display = ('usage',)
+    search_fields = ('usage',)
+
+
+class RouteAdmin(TranslationAdmin):
+    list_display = ('route',)
+    search_fields = ('route',)
+
+
+class DifficultyLevelAdmin(TranslationAdmin):
+    list_display = ('difficulty',)
+    search_fields = ('difficulty',)
+
+
+class DestinationAdmin(TranslationAdmin):
+    list_display = ('destination',)
+    search_fields = ('destination',)
+
+
+class WebLinkAdmin(TranslationAdmin):
+    list_display = ('name', 'url', )
+    search_fields = ('name', 'url', )
+
+
+# Register previously defined modeladmins
+trek_admin_to_register = [
+    (Trek, TrekAdmin),
+    (TrekNetwork, TrekNetworkAdmin),
+    (Usage, UsageAdmin),
+    (Route, RouteAdmin),
+    (DifficultyLevel, DifficultyLevelAdmin),
+    (Destination, DestinationAdmin),
+    (WebLink, WebLinkAdmin),
+]
+
+for model, model_admin in trek_admin_to_register:
+    admin.site.register(model, model_admin)
+
