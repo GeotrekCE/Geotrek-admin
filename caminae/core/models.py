c4aaa480343564d74f80025468443bda36e6966b
Simon Thépot <simon.thepot@gmail.com>
2012-07-24 16:16:40 +0200

    Core: Added missing models

--- caminae/core/models.py
+++ caminae/core/models.py
@@ -36,6 +36,21 @@ class Path(StructureRelated):
     max_elevation = models.IntegerField(
             editable=False, default=0, db_column='altitude_maximum', verbose_name=_(u"Maximum elevation"))
 
+
+    path_management = models.ForeignKey('PathManagement',
+            null=True, blank=True, related_name='paths',
+            verbose_name=_("Path management"))
+    datasource_management = models.ForeignKey('DatasourceManagement',
+            null=True, blank=True, related_name='paths',
+            verbose_name=_("Datasource"))
+    challenge_management = models.ForeignKey('ChallengeManagement',
+            null=True, blank=True, related_name='paths',
+            verbose_name=_("Challenge management"))
+    usages_management = models.ManyToManyField('UsageManagement',
+            related_name="paths", verbose_name=_(u"Usages management"))
+    networks_management = models.ManyToManyField('NetworkManagement',
+            related_name="paths", verbose_name=_(u"Networks management"))
+
     def __unicode__(self):
         return self.name
 
@@ -125,4 +140,73 @@ class PathAggregation(models.Model):
         db_table = 'evenements_troncons'
         verbose_name = _(u"Path aggregation")
         verbose_name_plural = _(u"Path aggregations")
+
+
+class DatasourceManagement(models.Model):
+
+    source = models.CharField(verbose_name=_(u"Source"), max_length=50)
+
+    class Meta:
+        db_table = 'gestion_source_donnees'
+        verbose_name = _(u"Datasource management")
+        verbose_name_plural = _(u"Datasources management")
+
+    def __unicode__(self):
+        return self.source
+
+
+class ChallengeManagement(models.Model):
+
+    challenge = models.CharField(verbose_name=_(u"Challenge"), max_length=50)
+
+    class Meta:
+        db_table = 'gestion_enjeux'
+        verbose_name = _(u"Challenge management")
+        verbose_name_plural = _(u"Challenges management")
+
+    def __unicode__(self):
+        return self.challenge
+
+
+class UsageManagement(models.Model):
+
+    usage = models.CharField(verbose_name=_(u"Usage"), max_length=50)
+
+    class Meta:
+        db_table = 'gestion_usages'
+        verbose_name = _(u"Usage management")
+        verbose_name_plural = _(u"Usages management")
+
+    def __unicode__(self):
+        return self.usage
+
+
+class NetworkManagement(models.Model):
+
+    network = models.CharField(verbose_name=_(u"Network"), max_length=50)
+
+    class Meta:
+        db_table = 'gestion_reseau'
+        verbose_name = _(u"Network management")
+        verbose_name_plural = _(u"Networks management")
+
+    def __unicode__(self):
+        return self.network
+
+
+class PathManagement(models.Model):
+
+    name = models.CharField(verbose_name=_(u"Name"), max_length=15)
+    departure = models.CharField(verbose_name=_(u"Name"), max_length=15)
+    arrival = models.CharField(verbose_name=_(u"Arrival"), max_length=15)
+    comments = models.CharField(verbose_name=_(u"Comments"), max_length=200)
+
+    class Meta:
+        db_table = 'gestion_sentier'
+        verbose_name = _(u"Path management")
+        verbose_name_plural = _(u"Paths management")
+
+    def __unicode__(self):
+        return u"%s (%s -> %s)" % (self.name, self.depature, self.arrival)
+
 
