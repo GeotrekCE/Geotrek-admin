c326c8970d3696f7546a48cd3c4eab16e14834fd
Gilles Bassière <gilles.bassiere@makina-corpus.com>
2012-07-26 11:14:51 +0200

    Permissions on mudules (#17): fix decorators.

--- caminae/authent/decorators.py
+++ caminae/authent/decorators.py
@@ -21,18 +21,22 @@ def user_passes_test_or_redirect(f, redirect_to):
         return _wrapped_view
     return decorator
 
+
 def path_manager_required(redirect_to):
     f = lambda u: u.is_authenticated() and u.profile.is_path_manager()
-    return group_required(, redirect_to=redirect_to)
+    return user_passes_test_or_redirect(f, redirect_to=redirect_to)
+
 
 def comm_manager_required(redirect_to):
     f = lambda u: u.is_authenticated() and u.profile.is_comm_manager()
-    return group_required('Référents communication', redirect_to=redirect_to)
+    return user_passes_test_or_redirect(f, redirect_to=redirect_to)
+
 
 def editor_required(redirect_to):
     f = lambda u: u.is_authenticated() and u.profile.is_editor()
-    return group_required('Rédacteurs', redirect_to=redirect_to)
+    return user_passes_test_or_redirect(f, redirect_to=redirect_to)
+
 
 def admininistrator_required(redirect_to):
     f = lambda u: u.is_authenticated() and u.profile.is_administrator()
-    return group_required('Administrateurs', redirect_to=redirect_to)
+    return user_passes_test_or_redirect(f, redirect_to=redirect_to)
