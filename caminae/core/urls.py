4c79d85d9f7e08583968fbf1bceb508554db1a64
Gilles Bassière <gilles.bassiere@makina-corpus.com>
2012-07-26 12:15:08 +0200

    Permissions on modules (#17): demonstrate how to use decorators

--- caminae/core/urls.py
+++ caminae/core/urls.py
@@ -4,6 +4,8 @@ from django.conf.urls import patterns, url
 from .views import PathList, path_list
 from .views import PathDetail, PathCreate, PathUpdate, PathDelete
 
+from caminae.authent.decorators import path_manager_required
+
 
 urlpatterns = patterns('',
     url(r'^path.geojson$', PathList.as_view(), name="layerpath"),
@@ -13,7 +15,7 @@ urlpatterns = patterns('',
     # url(r'^path/edit/(?P<path_id>\d+)/$', path_create_or_edit, name="path_edit"),
 
     url(r'path/(?P<pk>\d+)/$', PathDetail.as_view(), name='path_detail'),
-    url(r'path/add/$', PathCreate.as_view(), name='path_add'),
-    url(r'path/edit/(?P<pk>\d+)/$', PathUpdate.as_view(), name='path_update'),
-    url(r'path/(?P<pk>\d+)/delete/$', PathDelete.as_view(), name='path_delete'),
+    url(r'path/add/$', path_manager_required('core:path_list')(PathCreate.as_view()), name='path_add'),
+    url(r'path/edit/(?P<pk>\d+)/$', path_manager_required('core:path_detail')(PathUpdate.as_view()), name='path_update'),
+    url(r'path/(?P<pk>\d+)/delete/$', path_manager_required('core:path_detail')(PathDelete.as_view()), name='path_delete'),
 )
