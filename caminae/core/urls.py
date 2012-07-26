7e44ee7bfd9036d32680d0981f3c7128102385cf
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2012-07-26 15:10:55 +0200

    Rewrote views import

--- caminae/core/urls.py
+++ caminae/core/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import patterns, url
 
-# from .views import PathList, path_list, path_create_or_edit
-from .views import PathLayer, path_list
-from .views import PathDetail, PathCreate, PathUpdate, PathDelete
-
 from caminae.authent.decorators import path_manager_required
+
+from .views import (PathLayer, PathList, PathDetail, PathCreate, 
+                    PathUpdate, PathDelete)
 
 
 urlpatterns = patterns('',
@@ -12,12 +11,9 @@ urlpatterns = patterns('',
 )
 
 urlpatterns += patterns('',
-    url(r'^path/list/$', path_list, name="path_list"),
-    # url(r'^path/add/$', path_create_or_edit, name="path_add"),
-    # url(r'^path/edit/(?P<path_id>\d+)/$', path_create_or_edit, name="path_edit"),
-
-    url(r'path/(?P<pk>\d+)/$', PathDetail.as_view(), name='path_detail'),
-    url(r'path/add/$', path_manager_required('core:path_list')(PathCreate.as_view()), name='path_add'),
-    url(r'path/edit/(?P<pk>\d+)/$', path_manager_required('core:path_detail')(PathUpdate.as_view()), name='path_update'),
-    url(r'path/(?P<pk>\d+)/delete/$', path_manager_required('core:path_detail')(PathDelete.as_view()), name='path_delete'),
+    url(r'^path/list/$', PathList, name="path_list"),
+    url(r'^path/(?P<pk>\d+)/$', PathDetail.as_view(), name='path_detail'),
+    url(r'^path/add/$', path_manager_required('core:path_list')(PathCreate.as_view()), name='path_add'),
+    url(r'^path/edit/(?P<pk>\d+)/$', path_manager_required('core:path_detail')(PathUpdate.as_view()), name='path_update'),
+    url(r'^path//delete/(?P<pk>\d+)$', path_manager_required('core:path_detail')(PathDelete.as_view()), name='path_delete'),
 )
