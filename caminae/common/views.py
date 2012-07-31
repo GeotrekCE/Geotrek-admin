d519889ec0f16beb809c49b65a4aa39ac2ac3449
Simon Thépot <simon.thepot@gmail.com>
2012-08-01 00:57:06 +0200

    common.views: added various JSON related class (encoder, Response and ListView based)

--- caminae/common/views.py
+++ caminae/common/views.py
@@ -1,1 +1,62 @@
-# Create your views here.
+import json
+
+from django.core.serializers import serialize
+from django.core.serializers.json import DateTimeAwareJSONEncoder
+from django.db.models.query import QuerySet
+from django.http import HttpResponse
+from django.utils.encoding import force_unicode
+from django.utils.functional import Promise
+from django.utils import simplejson
+from django.views.generic.list import ListView
+
+
+class HttpJSONResponse(HttpResponse):
+    def __init__(self, content='', **kwargs):
+        kwargs['content_type'] = 'application/json'
+        super(HttpJSONResponse, self).__init__(content, **kwargs)
+
+
+class DjangoJSONEncoder(DateTimeAwareJSONEncoder):
+    """
+    Taken (slightly modified) from:
+    http://stackoverflow.com/questions/2249792/json-serializing-django-models-with-simplejson
+    """
+    def default(self, obj):
+        # https://docs.djangoproject.com/en/dev/topics/serialization/#id2
+        if isinstance(obj, Promise):
+            return force_unicode(obj)
+        if isinstance(obj, QuerySet):
+            # `default` must return a python serializable
+            # structure, the easiest way is to load the JSON
+            # string produced by `serialize` and return it
+            return simplejson.loads(serialize('json', obj))
+        return super(DjangoJSONEncoder, self).default(obj)
+
+
+class JSONResponseMixin(object):
+    """
+    A mixin that can be used to render a JSON response.
+    """
+    response_class = HttpJSONResponse
+
+    def render_to_response(self, context, **response_kwargs):
+        """
+        Returns a JSON response, transforming 'context' to make the payload.
+        """
+        return self.response_class(
+            self.convert_context_to_json(context),
+            **response_kwargs
+        )
+
+    def convert_context_to_json(self, context):
+        "Convert the context dictionary into a JSON object"
+        return simplejson.dumps(context, cls=DjangoJSONEncoder)
+
+
+class JSONListView(JSONResponseMixin, ListView):
+    """
+    A generic view to serve a model as a layer.
+    """
+    pass
+
+
