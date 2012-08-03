50a886c01e294bb58bd8b9661d85579290e92125
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2012-08-03 13:53:24 +0200

    Switch django-geojson template tags

--- caminae/core/templatetags/maps.py
+++ caminae/core/templatetags/maps.py
@@ -1,11 +1,6 @@
 from django import template
-from django.conf import settings
 from django.template import Context
-from django.utils import simplejson
 from django.contrib.gis.geos import GEOSGeometry
-
-import geojson
-from djgeojson.serializers import Serializer
 
 register = template.Library()
 
@@ -27,18 +22,3 @@ def latlngbounds(obj, fieldname='geom'):
     else:
         extent = list(getattr(obj, fieldname).extent)
     return [[extent[1], extent[0]], [extent[3], extent[2]]]
-
-
-@register.filter
-def geojsonfeature(obj):
-    if obj is None or isinstance(obj, basestring):
-        return 'null'
-    geojsonvalue = ''
-    if isinstance(obj, GEOSGeometry):
-        obj.transform(settings.MAP_SRID)
-        feature = geojson.Feature(geometry=simplejson.loads(obj.geojson))
-        geojsonvalue = geojson.dumps(feature)
-    else:
-        serializer = Serializer()
-        geojsonvalue = serializer.serialize([obj], fields=[], srid=settings.MAP_SRID)
-    return geojsonvalue
