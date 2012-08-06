3c29d788bd2bd57519bf3fb642810c162c3785ab
Simon Thépot <simon.thepot@gmail.com>
2012-08-06 12:42:45 +0000

    Path #107: adding a path work (editing and adding share behaviour)

---
+++ caminae/core/templates/core/add_fragment.js
@@ -0,0 +1,18 @@
+(function go() {
+
+    /*
+    map.setMaxBounds(map_bounds);
+    map.fitBounds(map_bounds);
+    */
+
+
+    // Add reset view control
+    /*
+    map.addControl(new L.Control.ResetView(getBounds));
+    map.addControl(new L.Control.Scale());
+    */
+
+
+
+    map.addLayer(drawnItems);
+})();
