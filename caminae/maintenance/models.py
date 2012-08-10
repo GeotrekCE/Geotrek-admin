2083d9033fd049495bd7b6ad3a48341045c1228a
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2012-08-10 13:30:56 +0200

    Fix fake intervention save (ref #114)

--- caminae/maintenance/models.py
+++ caminae/maintenance/models.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.db import models
+from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.gis.geos import Point, LineString
 
 from caminae.authent.models import StructureRelated
-from caminae.core.models import Path, MapEntityMixin, TopologyMixin
-from caminae.core.factories import PathFactory, TopologyMixinFactory, PathAggregationFactory
+from caminae.core.models import MapEntityMixin, TopologyMixin
+from caminae.core.factories import TopologyMixinFactory
 from caminae.common.models import Organism
 
 
@@ -67,36 +68,21 @@ class Intervention(MapEntityMixin, StructureRelated):
         """
         Initialize the intervention topology from a Point.
         """
-        closest_paths = Path.objects.distance(point).order_by('-distance')
-        if not closest_paths:
-            closest_paths = [PathFactory.create()]
-            # TODO: raise not possible without path !
-
-        # TODO: compute this from point position
-        path = closest_paths[0]
-        distance = 0
-        position = position = 0.5
+        # TODO : compute offset etc.
         fakeline = LineString(Point(point.x, point.y, 0), Point(point.x+0.1, point.y+0.1, 0))
-        
-        # Create topologies objects
-        topology = TopologyMixinFactory.create(offset=distance, geom=fakeline)
-        PathAggregationFactory.create(topo_object=topology,
-                                      path=path,
-                                      start_position=position,
-                                      end_position=position)
+        topology = TopologyMixinFactory.create(geom=fakeline)
         self.topology = topology
         self.save()
 
     @property
     def geom(self):
         if self.topology:
-            if len(self.topology.paths) > 1:
-                pathaggr = self.topology.paths[0]
-                if pathaggr.start_position == pathaggr.end_position:
-                    return self.topology.geom.coords[0]  # return Point
-                else:
-                    return self.topology.geom
-        return Point(0,0) # TODO: None ?
+            if len(list(self.topology.geom.coords)) == 2:
+                fakepoint = Point(*self.topology.geom.coords[0], srid=settings.SRID)  # return Point from fakeline
+                return fakepoint
+            else:
+                return self.topology.geom
+        return Point(0,0,0)
 
     @property
     def name_display(self):
