67f57aed10e3df4da343a86054ed5908b5f9d1b3
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2012-08-14 15:52:23 +0200

    Fake topologies at infrastructure form save (ref #141)

--- caminae/infrastructure/forms.py
+++ caminae/infrastructure/forms.py
@@ -1,7 +1,10 @@
+from django.contrib.gis.geos import Point, LineString
+
 import floppyforms as forms
 
 from caminae.mapentity.forms import MapEntityForm
 from caminae.core.widgets import PointOrMultipathWidget
+from caminae.core.factories import TopologyMixinFactory, PathAggregationFactory
 
 from .models import Infrastructure, Signage
 
@@ -15,14 +18,30 @@ class BaseInfrastructureForm(MapEntityForm):
             'type',)
     geomfields = ('geom',)
 
+    def save(self, commit=True):
+        infrastructure = super(BaseInfrastructureForm, self).save(commit=False)
+        # TODO: this is completely wrong, but we have no topology editor
+        topo_object = TopologyMixinFactory.create()
+        infrastructure.topo_object = topo_object
+        infrastructure.date_insert = topo_object.date_insert
+        infrastructure.date_update = topo_object.date_update
+        infrastructure.delete = topo_object.delete
+        infrastructure.kind = topo_object.kind
+        infrastructure.geom = LineString(Point(0,0,0), Point(1,1,0))
+        infrastructure.offset = topo_object.offset
+        if commit:
+            infrastructure.save()
+        PathAggregationFactory.create(topo_object=infrastructure)
+        return infrastructure
+
 
 class InfrastructureForm(BaseInfrastructureForm):
     class Meta:
         model = Infrastructure
-        exclude = ('deleted',)
+        exclude = ('deleted', 'kind', 'troncons', 'offset')  # TODO: topology editor
 
 
 class SignageForm(BaseInfrastructureForm):
     class Meta:
         model = Signage
-        exclude = ('deleted',)
+        exclude = ('deleted', 'kind', 'troncons', 'offset')
