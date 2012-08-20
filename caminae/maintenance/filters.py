5b4b17dc2fb15face444f63e834ad1f651ad4e99
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2012-08-20 14:49:35 +0200

    BBox filter for interventions (ref #40)

--- caminae/maintenance/filters.py
+++ caminae/maintenance/filters.py
@@ -1,13 +1,23 @@
+from caminae.core.models import TopologyMixin
 from caminae.common.filters import StructureRelatedFilterSet
+from caminae.mapentity.filters import PolygonFilter
+from caminae.mapentity.widgets import GeomWidget
 
 from .models import Intervention, Project
 
 
+class PolygonTopologyFilter(PolygonFilter):
+    def filter(self, qs, value):
+        lookup = self.lookup_type
+        inner_qs = TopologyMixin.objects.filter(**{'geom__%s' % lookup: value})
+        return qs.filter(**{'%s__in' % self.name: inner_qs})
+
+
 class InterventionFilter(StructureRelatedFilterSet):
+    bbox = PolygonTopologyFilter(name='topology', lookup_type='intersects', widget=GeomWidget)
     class Meta(StructureRelatedFilterSet.Meta):
         model = Intervention
         fields = StructureRelatedFilterSet.Meta.fields + ['status']
-        exclude = ('bbox',)  # Intervention has no geom db field
 
 
 class ProjectFilter(StructureRelatedFilterSet):
