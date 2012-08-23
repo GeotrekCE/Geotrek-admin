c7f44731d4131281266a6d1401cdcf333afc7956
Gilles Bassière <gilles.bassiere@makina-corpus.com>
2012-08-23 11:27:57 +0200

    Topologies: move trigger code to a general function for later reuse (trying to apply DRY principle to SQL...)

--- caminae/core/sql/00_utilities.sql
+++ caminae/core/sql/00_utilities.sql
@@ -22,7 +22,6 @@ $$ LANGUAGE plpgsql;
 
 DROP FUNCTION IF EXISTS ft_longueur() CASCADE;
 
-
 -------------------------------------------------------------------------------
 -- Interpolate along : the opposite of ST_LocateAlong
 -------------------------------------------------------------------------------
@@ -35,5 +34,55 @@ BEGIN
            ST_Distance(point, ST_Line_Interpolate_Point(line, ST_Line_Locate_Point(line, point))) AS offset
     INTO tuple;
     RETURN tuple;
+END;
+$$ LANGUAGE plpgsql;
+
+-------------------------------------------------------------------------------
+-- Update geometry of an "evenement"
+-------------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION update_geometry_of_evenement(eid integer) RETURNS void AS $$
+DECLARE
+    egeom geometry;
+    lines_only boolean;
+    t_count integer;
+BEGIN
+    -- See what kind of topology we have
+    SELECT bool_and(et.pk_debut != et.pk_fin), count(*)
+        INTO lines_only, t_count
+        FROM evenements_troncons et
+        WHERE et.evenement = eid;
+
+    IF t_count = 0 THEN
+        -- No more troncons, close this topology
+        UPDATE evenements SET geom = ST_GeomFromText('POINTZ EMPTY', 2154), longueur = 0, supprime = TRUE WHERE id = eid;
+    ELSIF NOT lines_only AND t_count > 1 THEN
+        -- FIXME: This is an invalid case (a multi-point topology or a
+        -- mixed points/lines topology), how to handle it?
+        CONTINUE;
+    ELSIF NOT lines_only AND t_count = 1 THEN
+        -- Special case: the topology describe a point on the path
+        -- Note: We are faking a M-geometry in order to use LocateAlong.
+        -- This is handy because this function includes an offset parameter
+        -- which could be otherwise diffcult to handle.
+        SELECT ST_Force_3DZ(ST_GeometryN(ST_LocateAlong(ST_AddMeasure(t.geom, 0, 1), et.pk_debut, e.decallage), 1))
+            INTO egeom
+            FROM evenements e, evenements_troncons et, troncons t
+            WHERE e.id = eid AND et.evenement = e.id AND et.troncon = t.id;
+        UPDATE evenements SET geom = egeom, longueur = ST_Length(egeom), supprime = FALSE WHERE id = eid;
+    ELSE
+        -- Regular case: the topology describe a line
+        -- Note: We are faking a M-geometry in order to use LocateBetween
+        -- which is better than OffsetCurve because it will not drop the
+        -- Z-index.
+        -- FIXME: If paths are not contiguous, only the first chunk will be
+        -- considered. How to handle these invalid linear topologies?
+        SELECT ST_Force_3DZ(ST_GeometryN(ST_LocateBetween(ST_AddMeasure(ST_LineMerge(ST_Collect(ST_Line_Substring(t.geom, et.pk_debut, et.pk_fin))), 0, 1), 0, 1, e.decallage), 1))
+            INTO egeom
+            FROM evenements e, evenements_troncons et, troncons t
+            WHERE e.id = eid AND et.evenement = e.id AND et.troncon = t.id
+            GROUP BY e.id, e.decallage;
+        UPDATE evenements SET geom = egeom, longueur = ST_Length(egeom), supprime = FALSE WHERE id = eid;
+    END IF;
 END;
 $$ LANGUAGE plpgsql;
--- caminae/core/sql/20_evenements_troncons.sql
+++ caminae/core/sql/20_evenements_troncons.sql
@@ -50,9 +50,6 @@ CREATE OR REPLACE FUNCTION ft_evenements_troncons_geometry() RETURNS trigger AS 
 DECLARE
     eid integer;
     eids integer[];
-    egeom geometry;
-    lines_only boolean;
-    t_count integer;
 BEGIN
     IF TG_OP = 'INSERT' THEN
         eids := array_append(eids, NEW.evenement);
@@ -66,43 +63,7 @@ BEGIN
     END IF;
 
     FOREACH eid IN ARRAY eids LOOP
-        -- See what kind of topology we have
-        SELECT bool_and(et.pk_debut != et.pk_fin), count(*)
-            INTO lines_only, t_count
-            FROM evenements_troncons et
-            WHERE et.evenement = eid;
-
-        IF t_count = 0 THEN
-            -- No more troncons, close this topology
-            UPDATE evenements SET geom = ST_GeomFromText('POINTZ EMPTY', 2154), longueur = 0, supprime = TRUE WHERE id = eid;
-        ELSIF NOT lines_only AND t_count > 1 THEN
-            -- FIXME: This is an invalid case (a multi-point topology or a
-            -- mixed points/lines topology), how to handle it?
-            CONTINUE;
-        ELSIF NOT lines_only AND t_count = 1 THEN
-            -- Special case: the topology describe a point on the path
-            -- Note: We are faking a M-geometry in order to use LocateAlong.
-            -- This is handy because this function includes an offset parameter
-            -- which could be otherwise diffcult to handle.
-            SELECT ST_Force_3DZ(ST_GeometryN(ST_LocateAlong(ST_AddMeasure(t.geom, 0, 1), et.pk_debut, e.decallage), 1))
-                INTO egeom
-                FROM evenements e, evenements_troncons et, troncons t
-                WHERE e.id = eid AND et.evenement = e.id AND et.troncon = t.id;
-            UPDATE evenements SET geom = egeom, longueur = ST_Length(egeom), supprime = FALSE WHERE id = eid;
-        ELSE
-            -- Regular case: the topology describe a line
-            -- Note: We are faking a M-geometry in order to use LocateBetween
-            -- which is better than OffsetCurve because it will not drop the
-            -- Z-index.
-            -- FIXME: If paths are not contiguous, only the first chunk will be
-            -- considered. How to handle these invalid linear topologies?
-            SELECT ST_Force_3DZ(ST_GeometryN(ST_LocateBetween(ST_AddMeasure(ST_LineMerge(ST_Collect(ST_Line_Substring(t.geom, et.pk_debut, et.pk_fin))), 0, 1), 0, 1, e.decallage), 1))
-                INTO egeom
-                FROM evenements e, evenements_troncons et, troncons t
-                WHERE e.id = eid AND et.evenement = e.id AND et.troncon = t.id
-                GROUP BY e.id, e.decallage;
-            UPDATE evenements SET geom = egeom, longueur = ST_Length(egeom), supprime = FALSE WHERE id = eid;
-        END IF;
+        PERFORM update_geometry_of_evenement(eid);
 
         -- TODO: DELETE evenements_troncons ON DELETE OR UPDATE supprime ON evenements (disable this trigger)
         -- TODO: UPDATE evenements ON UPDATE decallage ON evenement
