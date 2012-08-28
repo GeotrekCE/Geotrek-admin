98d3f29b8d9bbd77a869a9cfdc3abe3b984b06bf
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2012-08-28 10:15:33 +0200

    Add Wysiwyg for text areas (ref #32)

--- mapentity/forms.py
+++ mapentity/forms.py
@@ -1,12 +1,16 @@
+from django import forms as django_forms
 from django.utils.translation import ugettext_lazy as _
 
 import floppyforms as forms
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Submit, Div, Button
 from crispy_forms.bootstrap import FormActions
+from tinymce.widgets import TinyMCE
 
 
 class MapEntityForm(forms.ModelForm):
+    formfield_callback = lambda f: MapEntityForm.make_tinymce_widget(f)
+
     pk = forms.Field(required=False, widget=forms.Field.hidden_widget)
     model = forms.Field(required=False, widget=forms.Field.hidden_widget)
 
@@ -20,6 +24,14 @@ class MapEntityForm(forms.ModelForm):
         Submit('save_changes', _('Save changes'), css_class="btn-primary offset1"),
         css_class="form-actions span11",
     )
+
+    @staticmethod
+    def make_tinymce_widget(f):
+        formfield = f.formfield()
+        if formfield and isinstance(formfield.widget, (forms.widgets.Textarea, 
+                                                       django_forms.widgets.Textarea)):
+            formfield.widget = TinyMCE()
+        return formfield
 
     def __init__(self, *args, **kwargs):
         super(MapEntityForm, self).__init__(*args, **kwargs)
