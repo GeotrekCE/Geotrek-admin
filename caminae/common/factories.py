82debbfc953daa1590636c9e6ce7ae981af5f7a2
Simon Thépot <simon.thepot@gmail.com>
2012-08-31 13:48:56 +0000

    Add filetypefactory

--- caminae/common/factories.py
+++ caminae/common/factories.py
@@ -7,3 +7,10 @@ class OrganismFactory(factory.Factory):
     FACTORY_FOR = models.Organism
 
     organism = factory.Sequence(lambda n: u"Organism %s" % n)
+
+
+class FileTypeFactory(factory.Factory):
+    FACTORY_FOR = models.FileType
+
+    type = factory.Sequence(lambda n: u"FileType %s" % n)
+
--- caminae/core/tests/__init__.py
+++ caminae/core/tests/__init__.py
@@ -5,3 +5,4 @@ from .test_factories import *
 from .path_filter import *
 from .graph import *
 from .widgets import *
+from .test_attachment import *
