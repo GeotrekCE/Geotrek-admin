b705cfba1aa8ba04b45717f391c3df130b9cee92
Simon Thépot <simon.thepot@gmail.com>
2012-09-04 12:42:35 +0000

    Filters for path/intervention/projects (some filter missing yet) - (ref #208)

--- caminae/maintenance/filters.py
+++ caminae/maintenance/filters.py
@@ -1,6 +1,9 @@
+from django.forms.widgets import Select
+from django.utils.translation import ugettext_lazy as _
+
 from caminae.core.models import TopologyMixin
 from caminae.common.filters import StructureRelatedFilterSet
-from caminae.mapentity.filters import PolygonFilter, PythonPolygonFilter
+from caminae.mapentity.filters import PolygonFilter, PythonPolygonFilter, YearFilter, YearBetweenFilter
 from caminae.mapentity.widgets import GeomWidget
 
 from .models import Intervention, Project
@@ -17,13 +20,21 @@ class PolygonTopologyFilter(PolygonFilter):
 
 class InterventionFilter(StructureRelatedFilterSet):
     bbox = PolygonTopologyFilter(name='topology', lookup_type='intersects', widget=GeomWidget)
+    year = YearFilter(name='date', widget=Select, label=_(u"Year"))
+
     class Meta(StructureRelatedFilterSet.Meta):
         model = Intervention
-        fields = StructureRelatedFilterSet.Meta.fields + ['status']
+        fields = StructureRelatedFilterSet.Meta.fields + [
+            'status', 'type', 'stake', # user
+        ]
 
 
 class ProjectFilter(StructureRelatedFilterSet):
     bbox = PythonPolygonFilter(name='geom', widget=GeomWidget)
+    in_year = YearBetweenFilter(name=('begin_year', 'end_year'), widget=Select,
+                    label=_(u"Year of activity"))
+
     class Meta(StructureRelatedFilterSet.Meta):
         model = Project
-        fields = StructureRelatedFilterSet.Meta.fields + ['begin_year', 'end_year']
+        fields = StructureRelatedFilterSet.Meta.fields
+
