59f68794957b294bd9ab701b7b3bdc51658ee980
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2012-09-05 16:37:51 +0200

    Add popup ability for adding related objects (ref #53)

--- mapentity/widgets.py
+++ mapentity/widgets.py
@@ -1,4 +1,6 @@
 from django.conf import settings
+from django.template.loader import render_to_string
+from django import forms
 from django.forms import widgets as django_widgets
 
 from caminae.common.utils import wkt_to_geom
@@ -15,3 +17,15 @@ class GeomWidget(django_widgets.HiddenInput):
         if value and not isinstance(value, basestring):
             value.transform(settings.API_SRID)
         return value
+
+
+class SelectMultipleWithPop(forms.SelectMultiple):
+    def __init__(self, *args, **kwargs):
+        self.add_url = kwargs.pop('add_url')
+        super(SelectMultipleWithPop, self).__init__(*args, **kwargs)
+
+    def render(self, name, *args, **kwargs):
+        html = super(SelectMultipleWithPop, self).render(name, *args, **kwargs)
+        context = {'field': name, 'add_url': self.add_url}
+        popupplus = render_to_string("mapentity/popupplus.html", context)
+        return html+popupplus
