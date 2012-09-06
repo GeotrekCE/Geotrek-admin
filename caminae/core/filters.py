65bf789cc7e474b4c81e65db2fd4e607d5d2a9d1
Simon Thépot <simon.thepot@gmail.com>
2012-09-06 13:39:29 +0000

    Use icontains rather than exact for path name and comments filters

--- caminae/core/filters.py
+++ caminae/core/filters.py
@@ -1,4 +1,5 @@
 from django.utils.translation import ugettext_lazy as _
+from django_filters import CharFilter
 
 from .models import Path
 
@@ -7,11 +8,12 @@ from caminae.common.filters import OptionalRangeFilter, StructureRelatedFilterSe
 
 class PathFilter(StructureRelatedFilterSet):
     length = OptionalRangeFilter(label=_('length'))   # TODO: why force ?
+    name = CharFilter(label=_('Name'), lookup_type='icontains')
+    comments = CharFilter(label=_('Comments'), lookup_type='icontains')
 
     class Meta(StructureRelatedFilterSet.Meta):
         model = Path
         fields = StructureRelatedFilterSet.Meta.fields + [
-                    'length', 'name', 'networks', 'comments', 'trail',
+                    'length', 'networks', 'trail',
                 ]
-
 
