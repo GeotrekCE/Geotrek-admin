d42af5fde2ef5ed514cba9713183a9f0bb84d82a
Simon Thépot <simon.thepot@gmail.com>
2012-09-28 09:50:22 +0000

    Fix javascript bug - option.indexing was always true -

--- caminae/mapentity/static/mapentity/mapentity.js
+++ caminae/mapentity/static/mapentity/mapentity.js
@@ -30,7 +30,7 @@ MapEntity.ObjectsLayer = L.GeoJSON.extend({
         };
         
         if (!options) options = {};
-        options.indexing = options.indexing ? options.indexing : true;
+        options.indexing = options.indexing !== undefined ? options.indexing : true;
         options.highlight = options.highlight || typeof(options.objectUrl) != 'undefined';
         this._onEachFeature = options.onEachFeature;
         options.onEachFeature = L.Util.bind(onFeatureParse, this);
