820e1674111e5250809984af83644152f169b20d
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2012-11-05 18:29:24 +0100

    Simpler implementation of smart template inclusion (ref #323)

--- caminae/mapentity/templatetags/mapentity.py
+++ caminae/mapentity/templatetags/mapentity.py
@@ -8,18 +8,16 @@ register = template.Library()
 
 
 class SmartIncludeNode(template.Node):
-    def __init__(self, object, viewname):
-        self.object = object
+    def __init__(self, viewname):
         self.viewname = viewname
 
     def render(self, context):
         apps = [app.split('.')[-1] for app in settings.INSTALLED_APPS]
-        modelname = context[self.object]._meta.object_name.lower()
         viewname = self.viewname
         result = ""
         for module in apps:
             try:
-                t = template.loader.get_template("%(module)s/%(modelname)s_%(module)s_%(viewname)s.html" % locals())
+                t = template.loader.get_template("%(module)s/%(module)s_%(viewname)s_fragment.html" % locals())
                 result += t.render(context)
             except TemplateDoesNotExist:
                 pass
@@ -28,9 +26,9 @@ class SmartIncludeNode(template.Node):
 @register.tag(name="smart_include")
 def do_smart_include(parser, token):
     try:
-        tag_name, object, viewname = token.split_contents()
+        tag_name, viewname = token.split_contents()
     except ValueError:
-        raise template.TemplateSyntaxError("%r tag requires two arguments" % token.contents.split()[0])
+        raise template.TemplateSyntaxError("%r tag requires one argument" % token.contents.split()[0])
     if not (viewname[0] == viewname[-1] and viewname[0] in ('"', "'")):
         raise template.TemplateSyntaxError("%r tag's viewname argument should be in quotes" % tag_name)
-    return SmartIncludeNode(object, viewname[1:-1])
+    return SmartIncludeNode(viewname[1:-1])
