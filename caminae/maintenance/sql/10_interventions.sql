50104f1d4a8c25fb9f39cf2968403bda71ebcf7c
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2013-01-26 22:32:58 +0100

    Keep non topologies up to date

--- caminae/maintenance/sql/10_interventions.sql
+++ caminae/maintenance/sql/10_interventions.sql
@@ -1,5 +1,30 @@
 
 -------------------------------------------------------------------------------
+-- Keep dates up-to-date
+-------------------------------------------------------------------------------
+
+DROP TRIGGER IF EXISTS m_t_intervention_date_insert_tgr ON m_t_intervention;
+CREATE TRIGGER m_t_intervention_date_insert_tgr
+    BEFORE INSERT ON m_t_intervention
+    FOR EACH ROW EXECUTE PROCEDURE ft_date_insert();
+
+DROP TRIGGER IF EXISTS m_t_intervention_date_update_tgr ON m_t_intervention;
+CREATE TRIGGER m_t_intervention_date_update_tgr
+    BEFORE INSERT OR UPDATE ON m_t_intervention
+    FOR EACH ROW EXECUTE PROCEDURE ft_date_update();
+
+
+DROP TRIGGER IF EXISTS m_t_chantier_date_update_tgr ON m_t_chantier;
+CREATE TRIGGER m_t_chantier_date_update_tgr
+    BEFORE INSERT OR UPDATE ON m_t_chantier
+    FOR EACH ROW EXECUTE PROCEDURE ft_date_update();
+
+DROP TRIGGER IF EXISTS m_t_chantier_date_update_tgr ON m_t_chantier;
+CREATE TRIGGER m_t_chantier_date_update_tgr
+    BEFORE INSERT OR UPDATE ON m_t_chantier
+    FOR EACH ROW EXECUTE PROCEDURE ft_date_update();
+
+-------------------------------------------------------------------------------
 -- Delete related interventions when an evenement is deleted
 -------------------------------------------------------------------------------
 
