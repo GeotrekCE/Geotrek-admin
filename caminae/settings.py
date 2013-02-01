ed3b1349f58c7f4015c9cde6db03e4abc18a3719
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2013-02-01 11:50:32 +0100

    Revert "Disable South for tests, will be faster" : requires post migrate refactoring
    This reverts commit 6a12d290081dab40811a92065fcf1c3bf547e34a.

--- caminae/settings.py
+++ caminae/settings.py
@@ -62,9 +62,6 @@ LOCALE_PATHS = (
     os.path.join(PROJECT_ROOT_PATH, 'locale'),
 )
 
-# To disable migrations and use syncdb instead
-SOUTH_TESTS_MIGRATE = False
-
 SITE_ID = 1
 
 # If you set this to False, Django will make some optimizations so as not
