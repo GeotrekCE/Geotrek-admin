03154d05d07d42cb87ae5568461acae6088afefd
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2013-04-15 13:06:22 +0200

    Show colored labels on land layers (ref #386)

--- caminae/mapentity/static/mapentity/mapentity.js
+++ caminae/mapentity/static/mapentity/mapentity.js
@@ -71,6 +71,101 @@ Array.prototype.unique = function() {
 };
 
 
+/**!
+ * @preserve parseColor
+ * Copyright 2011 THEtheChad Elliott
+ * Released under the MIT and GPL licenses.
+ */
+// Parse hex/rgb{a} color syntax.
+// @input string
+// @returns array [r,g,b{,o}]
+parseColor = function(color) {
+
+    var cache
+      , p = parseInt // Use p as a byte saving reference to parseInt
+      , color = color.replace(/\s\s*/g,'') // Remove all spaces
+    ;//var
+    
+    // Checks for 6 digit hex and converts string to integer
+    if (cache = /^#([\da-fA-F]{2})([\da-fA-F]{2})([\da-fA-F]{2})/.exec(color)) 
+        cache = [p(cache[1], 16), p(cache[2], 16), p(cache[3], 16)];
+        
+    // Checks for 3 digit hex and converts string to integer
+    else if (cache = /^#([\da-fA-F])([\da-fA-F])([\da-fA-F])/.exec(color))
+        cache = [p(cache[1], 16) * 17, p(cache[2], 16) * 17, p(cache[3], 16) * 17];
+        
+    // Checks for rgba and converts string to
+    // integer/float using unary + operator to save bytes
+    else if (cache = /^rgba\(([\d]+),([\d]+),([\d]+),([\d]+|[\d]*.[\d]+)\)/.exec(color))
+        cache = [+cache[1], +cache[2], +cache[3], +cache[4]];
+        
+    // Checks for rgb and converts string to
+    // integer/float using unary + operator to save bytes
+    else if (cache = /^rgb\(([\d]+),([\d]+),([\d]+)\)/.exec(color))
+        cache = [+cache[1], +cache[2], +cache[3]];
+        
+    // Otherwise throw an exception to make debugging easier
+    else throw Error(color + ' is not supported by parseColor');
+    
+    // Performs RGBA conversion by default
+    isNaN(cache[3]) && (cache[3] = 1);
+    
+    // Adds or removes 4th value based on rgba support
+    // Support is flipped twice to prevent erros if
+    // it's not defined
+    return cache.slice(0,3 + !!$.support.rgba);
+}
+
+
+/**
+ * Shows a static label in the middle of the Polyline.
+ * It will be hidden on zoom levels below ``LABEL_MIN_ZOOM``.
+ */
+MapEntity.showLineLabel = function (layer, options) {
+    var LABEL_MIN_ZOOM = 6;
+
+    var rgb = parseColor(options.color);
+
+    layer.bindLabel(options.text, {noHide: true, className: options.className});
+
+    var __layerOnAdd = layer.onAdd;
+    layer.onAdd = function (map) {
+        __layerOnAdd.call(layer, map);
+        if (map.getZoom() >= LABEL_MIN_ZOOM) {
+            layer._showLabel();
+        }
+        map.on('zoomend', hideOnZoomOut);
+    };
+
+    var __layerOnRemove = layer.onRemove;
+    layer.onRemove = function () {
+        layer._map.off('zoomend', hideOnZoomOut);
+        layer._hideLabel();
+        __layerOnRemove.call(layer);
+    };
+
+    var __layerShowLabel = layer._showLabel;
+    layer._showLabel = function () {
+        __layerShowLabel.call(layer, {latlng: midLatLng(layer)});
+        layer._label._container.title = options.title;
+        layer._label._container.style.backgroundColor = 'rgba('+rgb.join(',')+ ',0.8)';
+        layer._label._container.style.borderColor = 'rgba('+rgb.join(',')+ ',0.6)';
+    };
+
+    function hideOnZoomOut() {
+        if (layer._map.getZoom() < LABEL_MIN_ZOOM)
+            layer._hideLabel();
+        else
+            layer._showLabel();
+    }
+
+    function midLatLng(line) {
+        var mid = Math.floor(line.getLatLngs().length/2);
+        return L.latLng(line.getLatLngs()[mid]);
+    }
+};
+
+
 MapEntity.Context = new function() {
     var self = this;
 
