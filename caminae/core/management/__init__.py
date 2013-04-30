be473035c3940193575d68b481e0faf541cc1926
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2013-04-30 16:53:29 +0200

    Remove RAISE NOTICE in production

--- caminae/core/management/__init__.py
+++ caminae/core/management/__init__.py
@@ -2,9 +2,12 @@
     http://djangosnippets.org/snippets/2311/
     Ensure South will update our custom SQL during a call to `migrate`.
 """
+import os
+import re
 import logging
 import traceback
 
+from django.db import connection, transaction, models
 from django.conf import settings
 
 from south.signals import post_migrate
@@ -13,13 +16,9 @@ logger = logging.getLogger(__name__)
 
 
 def run_initial_sql(sender, **kwargs):
-    import os
-    import re
-    from django.db import connection, transaction, models
-
     app_label = kwargs.get('app')
     app_dir = os.path.normpath(os.path.join(os.path.dirname(
-                    models.get_app(app_label).__file__), 'sql'))
+                               models.get_app(app_label).__file__), 'sql'))
     if not os.path.exists(app_dir):
         return
 
@@ -35,9 +34,9 @@ def run_initial_sql(sender, **kwargs):
             f = open(sql_file)
             sql = f.read()
             f.close()
-            if not settings.TEST:
-                # TODO: this is the ugliest driver hack ever
-                sql = sql.replace('%', '%%')
+            if not settings.TEST and not settings.DEBUG:
+                # Remove RAISE NOTICE (/!\ only one-liners)
+                sql = re.sub("^.*RAISE NOTICE.*$", "", sql)
             cursor.execute(sql)
         except Exception, e:
             logger.error("Failed to install custom SQL file '%s': %s\n" %
@@ -49,5 +48,3 @@ def run_initial_sql(sender, **kwargs):
             transaction.commit_unless_managed()
 
 post_migrate.connect(run_initial_sql, dispatch_uid="caminae.core.sqlautoload")
-
-
