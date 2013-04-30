e69c77d3be837b0b591ad5aff2ca35d112173398
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2013-04-30 17:41:01 +0200

    Generic stuff should be moved to MapEntity

---
+++ mapentity/context_processors.py
@@ -0,0 +1,9 @@
+from django.conf import settings as settings_  # import the settings file
+
+
+def settings(request):
+    return dict(
+        TITLE=getattr(settings_, 'TITLE'),
+        DEBUG=settings_.DEBUG,
+        VERSION=getattr(settings_, 'VERSION')
+    )
