326a9699dd7cc0245551dacd963f8aec904ad1af
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2013-05-07 16:21:50 +0200

    Load Mapentity submodule templates dir

--- geotrek/settings.py
+++ geotrek/settings.py
@@ -155,7 +155,6 @@ ROOT_URLCONF = 'geotrek.urls'
 WSGI_APPLICATION = 'geotrek.wsgi.application'
 
 TEMPLATE_DIRS = (
-    os.path.join(PROJECT_ROOT_PATH, 'mapentity', 'templates'),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
