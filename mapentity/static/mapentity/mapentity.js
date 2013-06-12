47daee2fe8f0de32d7b877c562b7577b7d1f3a7b
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2013-06-12 18:27:28 +0200

    Remove useless JS function

--- mapentity/static/mapentity/mapentity.js
+++ mapentity/static/mapentity/mapentity.js
@@ -59,17 +59,6 @@ function getURLParameter(name) {
     return paramDecoded;
 }
 
-/**
- * Uniqify arrays
- * http://www.shamasis.net/2009/09/fast-algorithm-to-find-unique-items-in-javascript-array/
- */
-Array.prototype.unique = function() {
-    var o = {}, i, l = this.length, r = [];
-    for(i=0; i<l;i+=1) o[this[i]] = this[i];
-    for(i in o) r.push(o[i]);
-    return r;
-};
-
 
 /**!
  * @preserve parseColor
