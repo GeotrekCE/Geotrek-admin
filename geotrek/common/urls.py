7a5f2d0397f8ecda1c1b0517e844eec9d0e3e9d4
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2013-06-12 12:27:29 +0200

    Fix URL pattern after removing

--- geotrek/common/urls.py
+++ geotrek/common/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, url
 from .views import settings_json
 
 
-urlpatterns += patterns('',
+urlpatterns = patterns('',
     url(r'^api/settings.json', settings_json, name='settings_json'),
 )
