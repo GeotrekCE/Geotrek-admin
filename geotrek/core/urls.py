f95564bb5194207db11e769ea80439cbcb73fba6
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2013-08-14 16:22:00 +0200

    Replace path profile by SVG image, and add it to trek too

--- geotrek/core/urls.py
+++ geotrek/core/urls.py
@@ -4,14 +4,14 @@ from mapentity import registry
 
 from .models import Path, Trail
 from .views import (
-    ElevationProfile,
+    ElevationChart,
     get_graph_json,
 )
 
 
 urlpatterns = patterns('',
     url(r'^api/graph.json$', get_graph_json, name="path_json_graph"),
-    url(r'^api/path/(?P<pk>\d+)/profile.json$', ElevationProfile.as_view(model=Path), name='path_profile'),
+    url(r'^api/path/(?P<pk>\d+)/profile.svg$', ElevationChart.as_view(model=Path), name='path_profile_svg'),
 )
 
 urlpatterns += registry.register(Path)
--- geotrek/trekking/urls.py
+++ geotrek/trekking/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 
 from mapentity import registry
 
-from geotrek.core.views import ElevationProfile
+from geotrek.core.views import ElevationProfile, ElevationChart
 from . import models
 from .views import (
     TrekDocumentPublic, TrekPrint,
@@ -19,6 +19,7 @@ urlpatterns = patterns('',
     url(r'^api/trek/trek-(?P<pk>\d+).gpx$', TrekGPXDetail.as_view(), name="trek_gpx_detail"),
     url(r'^api/trek/trek-(?P<pk>\d+).kml$', TrekKMLDetail.as_view(), name="trek_kml_detail"),
     url(r'^api/trek/(?P<pk>\d+)/profile.json$', ElevationProfile.as_view(model=models.Trek), name="trek_profile"),
+    url(r'^api/trek/(?P<pk>\d+)/profile.svg$', ElevationChart.as_view(model=models.Trek), name='trek_profile_svg'),
     url(r'^api/trek/(?P<pk>\d+)/pois.geojson$', TrekPOIGeoJSON.as_view(), name="trek_poi_geojson"),
     url(r'^popup/add/weblink/', WebLinkCreatePopup.as_view(), name='weblink_add'),
 )
