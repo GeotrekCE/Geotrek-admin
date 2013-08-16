72a4ee13cec58f729f9e9d61446ba777f909e5ad
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2013-08-16 11:51:57 +0200

    Move all postgresql stuff together

--- geotrek/common/utils/postgresql.py
+++ geotrek/common/utils/postgresql.py
@@ -1,8 +1,11 @@
 import re
+import os
 import logging
+import traceback
 from functools import wraps
 
-from django.db import connection
+from django.db import connection, transaction, models
+from django.conf import settings
 
 
 logger = logging.getLogger(__name__)
@@ -48,3 +51,42 @@ def debug_pg_notices(f):
         return r
 
     return wrapped
+
+
+def load_sql_files(app_label):
+    """
+    Look for SQL files in Django app, and load them into database.
+    We remove RAISE NOTICE instructions from SQL outside unit testing
+    since they lead to interpolation errors of '%' character in python.
+    """
+    app_dir = os.path.normpath(os.path.join(os.path.dirname(
+                               models.get_app(app_label).__file__), 'sql'))
+    if not os.path.exists(app_dir):
+        return
+
+    r = re.compile(r'^.*\.sql$')
+    sql_files = [os.path.join(app_dir, f)
+                 for f in os.listdir(app_dir)
+                 if r.match(f) is not None]
+    sql_files.sort()
+    cursor = connection.cursor()
+    for sql_file in sql_files:
+        try:
+            logger.info("Loading initial SQL data from '%s'" % sql_file)
+            f = open(sql_file)
+            sql = f.read()
+            f.close()
+            if not settings.TEST:
+                # Remove RAISE NOTICE (/!\ only one-liners)
+                sql = re.sub(r"\n.*RAISE NOTICE.*\n", "\n", sql)
+                # TODO: this is the ugliest driver hack ever
+                sql = sql.replace('%', '%%')
+            cursor.execute(sql)
+        except Exception as e:
+            logger.error("Failed to install custom SQL file '%s': %s\n" %
+                         (sql_file, e))
+            traceback.print_exc()
+            transaction.rollback_unless_managed()
+            raise
+        else:
+            transaction.commit_unless_managed()
