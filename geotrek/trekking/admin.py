39e1efb45e5245de46b62d3d533e15023b04eb9c
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2013-08-20 01:04:51 +0200

    Migrate existing treks when changing difficulty id (ref #771)

--- geotrek/trekking/admin.py
+++ geotrek/trekking/admin.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from django.db import models
+from django import forms
+from django.db import models, transaction
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
 from django.contrib import admin
 from modeltranslation.admin import TranslationAdmin
 
@@ -9,7 +12,7 @@ from tinymce.widgets import TinyMCE
 from geotrek.authent.admin import TrekkingManagerModelAdmin
 from .models import (
     POIType, Theme, TrekNetwork, Usage, Route, DifficultyLevel, WebLink,
-    WebLinkCategory, InformationDesk
+    WebLinkCategory, InformationDesk, Trek
 )
 
 
@@ -38,10 +41,64 @@ class RouteAdmin(TrekkingManagerModelAdmin, TranslationAdmin):
     search_fields = ('route',)
 
 
+class DifficultyLevelForm(forms.ModelForm):
+
+    def clean_id(self):
+        self.oldid = self.instance.pk if self.instance else None
+        self.newid = self.cleaned_data.get('id')
+
+        exists = len(DifficultyLevel.objects.filter(pk=self.newid)) > 0
+        if self.oldid != self.newid and exists:
+            raise ValidationError(_("Difficulty with id '%s' already exists") % self.newid)
+        return self.newid
+
+
 class DifficultyLevelAdmin(TrekkingManagerModelAdmin, TranslationAdmin):
+    form = DifficultyLevelForm
     list_display = ('id', 'difficulty',)
     search_fields = ('difficulty',)
     fields = ('id', 'difficulty')
+
+    def save_model(self, request, obj, form, change):
+        """
+        Allows to change DifficultyLevel id from Admin form.
+        It will migrate all treks using this difficulty to the new id.
+        """
+        self.oldid = None
+
+        # Nominal case. No migration.
+        if form.oldid is None or form.oldid == form.newid:
+            obj.save()
+            return
+
+        with transaction.commit_on_success():
+            # Migrate Treks
+            migrated = []
+            for t in Trek.objects.filter(difficulty=form.oldid):
+                t.difficulty = None
+                t.save()
+                migrated.append(t)
+            # Apply id change
+            self.oldid = form.oldid
+            obj.save()  # Will create new row in DB
+            old = DifficultyLevel.objects.get(id=self.oldid)
+            old.delete()
+            # Restore
+            for t in migrated:
+                t.difficulty = obj
+                t.save()
+
+    def response_change(self, request, obj):
+        """
+        If id was changed, always returns to the list (prevent 404).
+        Otherwise, behave as usual.
+        """
+        if self.oldid is not None:
+            msg = _('Difficulty id %s was changed to %s successfully.') % (
+                self.oldid, obj.pk)
+            self.message_user(request, msg)
+            return self.response_post_save_change(request, obj)
+        return super(DifficultyLevelAdmin, self).response_change(request, obj)
 
 
 class WebLinkAdmin(TrekkingManagerModelAdmin, TranslationAdmin):
