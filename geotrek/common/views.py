df37865c1f7fa2be0de63317b187139c3de8af1c
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2013-10-02 17:40:50 +0200

    Provide graph path url through JS settings

--- geotrek/common/views.py
+++ geotrek/common/views.py
@@ -1,7 +1,4 @@
-import json
-from django.core.urlresolvers import reverse_lazy
-from django.http import HttpResponse
-from django.views.generic import TemplateView
+from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.core.exceptions import ValidationError
 from django.utils.decorators import method_decorator
@@ -72,6 +69,8 @@ class JSSettings(BaseJSSettings):
             colorspool=settings.LAND_COLORS_POOL,
         )
         dictsettings['version'] = __version__
+        dictsettings['urls'] = {}
+        dictsettings['urls']['path_graph'] = reverse('core:path_json_graph')
         return dictsettings
 
 
