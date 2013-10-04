65038b86181c8a32fc7741ebc19c86024fad370e
Alex Marandon <alex.marandon@makina-corpus.com>
2013-10-04 16:53:08 +0200

    Fix relative import

--- geotrek/settings/base.py
+++ geotrek/settings/base.py
@@ -3,7 +3,7 @@ import sys
 
 from django.contrib.messages import constants as messages
 
-from .. import __version__
+from geotrek import __version__
 from . import PROJECT_ROOT_PATH
 
 gettext_noop = lambda s: s
