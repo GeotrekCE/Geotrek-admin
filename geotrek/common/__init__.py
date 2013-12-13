14dc2ab371a2e2ed023afad3ecd2a9e67430a68c
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2013-12-13 15:48:02 +0100

    Warn on tiling landscape/portrait spatial extent only if map with local projection

--- geotrek/common/__init__.py
+++ geotrek/common/__init__.py
@@ -7,6 +7,7 @@
 """
 from south.signals import post_migrate
 from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
 
 from mapentity.helpers import api_bbox
 
@@ -27,7 +28,17 @@ post_migrate.connect(run_initial_sql, dispatch_uid="geotrek.core.sqlautoload")
 
 
 """
+    TODO: keep until django-leaflet is upgraded to 0.8
+"""
+if settings.LEAFLET_CONFIG['SRID'] != 3857:
+    # Due to bug in Leaflet/Proj4Leaflet ()
+    # landscape extents are not supported.
+    extent = settings.SPATIAL_EXTENT
+    is_landscape = extent[2] - extent[0] > extent[3] - extent[1]
+    if is_landscape:
+        raise ImproperlyConfigured('Landscape spatial_extent not supported (%s).' % (extent,))
+
+"""
     Computed client-side setting.
 """
-
 settings.LEAFLET_CONFIG['SPATIAL_EXTENT'] = api_bbox(settings.SPATIAL_EXTENT, buffer=0.5)
