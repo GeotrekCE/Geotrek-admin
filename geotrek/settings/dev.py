01635ddb2634921d8a278ba9da62e516b8eadfdc
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2014-01-28 16:28:57 +0100

    Django debug toolbar is now smart with DEBUG as True

--- geotrek/settings/dev.py
+++ geotrek/settings/dev.py
@@ -17,28 +17,6 @@ INSTALLED_APPS += (
     'debug_toolbar',
     'django_extensions',
 )
-MIDDLEWARE_CLASSES += (
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-)
-
-def custom_show_toolbar(request):
-    return True  # Always show toolbar
-
-DEBUG_TOOLBAR_CONFIG = {
-    'SHOW_TOOLBAR_CALLBACK': custom_show_toolbar,
-    'INTERCEPT_REDIRECTS': False,
-}
-DEBUG_TOOLBAR_PANELS = (
-    'debug_toolbar.panels.version.VersionDebugPanel',
-    'debug_toolbar.panels.timer.TimerDebugPanel',
-    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
-    'debug_toolbar.panels.headers.HeaderDebugPanel',
-    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
-    'debug_toolbar.panels.template.TemplateDebugPanel',
-    'debug_toolbar.panels.sql.SQLDebugPanel',
-    'debug_toolbar.panels.signals.SignalDebugPanel',
-    'debug_toolbar.panels.logger.LoggingPanel',
-)
 
 #
 # Use Geotrek preprod tiles (uses default extent)
