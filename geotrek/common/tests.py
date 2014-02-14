b6be7be63da387b87acecd82b96c594d0f4ebe76
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2014-02-14 22:15:00 +0100

    Add admin tools extent test

--- geotrek/common/tests.py
+++ geotrek/common/tests.py
@@ -25,14 +25,24 @@ class CommonTest(MapEntityTest):
 class ViewsTest(TestCase):
 
     def setUp(self):
-        user = User.objects.create_user('homer', 'h@s.com', 'dooh')
-        success = self.client.login(username=user.username, password='dooh')
+        self.user = User.objects.create_user('homer', 'h@s.com', 'dooh')
+        success = self.client.login(username=self.user.username, password='dooh')
         self.assertTrue(success)
 
     def test_settings_json(self):
         url = reverse('common:settings_json')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
+
+    def test_admin_check_extents(self):
+        url = reverse('common:check_extents')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+        self.user.is_superuser = True
+        self.user.save()
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
 
 
 class UtilsTest(TestCase):
