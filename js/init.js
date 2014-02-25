84e11c432b518129c130ae2dcc556a7030d54276
Natsu- <romain.garrigues.cs@gmail.com>
2014-02-25 16:39:22 +0100

    Adding OwlCarousel for carousel part

--- js/init.js
+++ js/init.js
@@ -48,13 +48,22 @@ skel.init({
 	}
 });
 
-jQuery(function() {
-	jQuery('#nav > ul').dropotron({ 
-		offsetY: 0,
-		mode: 'fade',
-		speed: 300,
-		alignment: 'center',
-		noOpenerFade: true,
-		detach: false
+$(document).ready(function() {
+
+	jQuery(".owl-carousel").owlCarousel({
+ 	
+		// navigation : true, // Show next and prev buttons
+		slideSpeed : 300,
+		paginationSpeed : 400,
+		singleItem: true,
+		autoPlay: true
+		 
+		// "singleItem:true" is a shortcut for:
+		// items : 1,
+		// itemsDesktop : false,
+		// itemsDesktopSmall : false,
+		// itemsTablet: false,
+		// itemsMobile : false
+			 
 	});
 });
