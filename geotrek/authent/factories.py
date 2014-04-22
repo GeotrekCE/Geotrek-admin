47daf73ae08b36c8ee983f23f867fee6abad2800
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2014-04-22 10:54:56 +0200

    Fix tests with fixtures and registry order

--- geotrek/authent/factories.py
+++ geotrek/authent/factories.py
@@ -13,7 +13,7 @@ class PathManagerFactory(UserFactory):
     @classmethod
     def _prepare(cls, create, **kwargs):
         GROUP_PATH_MANAGER_ID = settings.AUTHENT_GROUPS_MAPPING['PATH_MANAGER']
-        pathmanager, exist = Group.objects.get(pk=GROUP_PATH_MANAGER_ID)
+        pathmanager = Group.objects.get(pk=GROUP_PATH_MANAGER_ID)
         kwargs.setdefault('groups', []).append(pathmanager)
         return super(PathManagerFactory, cls)._prepare(create, **kwargs)
 
@@ -24,7 +24,7 @@ class TrekkingManagerFactory(UserFactory):
     @classmethod
     def _prepare(cls, create, **kwargs):
         GROUP_TREKKING_MANAGER_ID = settings.AUTHENT_GROUPS_MAPPING['TREKKING_MANAGER']
-        pathmanager, exist = Group.objects.get(pk=GROUP_TREKKING_MANAGER_ID)
+        pathmanager = Group.objects.get(pk=GROUP_TREKKING_MANAGER_ID)
         kwargs.setdefault('groups', []).append(pathmanager)
         return super(TrekkingManagerFactory, cls)._prepare(create, **kwargs)
 
--- geotrek/common/tests.py
+++ geotrek/common/tests.py
@@ -2,6 +2,7 @@ import os
 
 import mock
 
+from django.conf import settings
 from django.db import connection
 from django.test import TestCase
 from django.core.urlresolvers import reverse
@@ -17,7 +18,18 @@ from . import check_srid_has_meter_unit
 
 
 class CommonTest(MapEntityTest):
-    fixtures = ['minimal.json', 'basic.json']
+    fixtures = [os.path.join(settings.PROJECT_ROOT_PATH, 'authent', 'fixtures', 'minimal.json'),
+                os.path.join(settings.PROJECT_ROOT_PATH, 'authent', 'fixtures', 'basic.json')]
+
+    def _pre_setup(self):
+        from geotrek.core import urls
+        from geotrek.land import urls
+        from geotrek.maintenance import urls
+        from geotrek.infrastructure import urls
+        from geotrek.trekking import urls
+        from geotrek.tourism import urls
+
+        return super(CommonTest, self)._pre_setup()
 
 
 class StartupCheckTest(TestCase):
