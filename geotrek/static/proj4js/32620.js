1d3e1cf1a41fdf14749ad4b5eb2c07a6056e4542
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2014-05-17 00:09:25 +0200

    Added projection file for EPSG:32620

---
+++ geotrek/static/proj4js/32620.js
@@ -0,0 +1,3 @@
+
+<!-- saved from url=(0065)http://spatialreference.org/ref/epsg/wgs-84-utm-zone-20n/proj4js/ -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"><style type="text/css"></style></head><body><pre style="word-wrap: break-word; white-space: pre-wrap;">Proj4js.defs["EPSG:32620"] = "+proj=utm +zone=20 +ellps=WGS84 +datum=WGS84 +units=m +no_defs";</pre></body></html>
