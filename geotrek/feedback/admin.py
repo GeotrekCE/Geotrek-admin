5f767b9cdc8f2bc338070634fc8571dfdcdaead2
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2014-05-16 23:17:44 +0200

    Add translatable category

--- geotrek/feedback/admin.py
+++ geotrek/feedback/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
+from modeltranslation.admin import TranslationAdmin
 
 from geotrek.feedback import models as feedback_models
 
 
-admin.site.register(feedback_models.ReportCategory)
+admin.site.register(feedback_models.ReportCategory, TranslationAdmin)
---
+++ geotrek/feedback/translation.py
@@ -0,0 +1,9 @@
+from modeltranslation.translator import translator, TranslationOptions
+
+from geotrek.feedback import models as feedback_models
+
+
+class ReportCategoryTO(TranslationOptions):
+    fields = ('category',)
+
+translator.register(feedback_models.ReportCategory, ReportCategoryTO)
