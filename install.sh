4340eee78baca3d380a91cbafcfcb0feb1207ff2
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2014-05-22 16:48:03 +0200

    Bump version

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ exec 1> install.log 2>&1
 
 #------------------------------------------------------------------------------
 
-VERSION=${VERSION:-0.23.0}
+VERSION=${VERSION:-0.23.1}
 dev=false
 tests=false
 prod=false
