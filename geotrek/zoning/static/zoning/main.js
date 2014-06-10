76232b0f6b30c50104595fe1f506ca4ff02cd3b6
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2014-06-10 17:04:22 +0200

    Add RestrictedArea by type (ref #961)

--- geotrek/zoning/static/zoning/main.js
+++ geotrek/zoning/static/zoning/main.js
@@ -4,13 +4,15 @@ $(window).on('entity:map', function (e, data) {
 
     // Add land layers
     var landLayers = [{url: window.SETTINGS.urls.district_layer, name: tr("Districts"), id: 'district'},
-                      {url: window.SETTINGS.urls.city_layer, name: tr("Cities"), id: 'city'},
-                      {url: window.SETTINGS.urls.restrictedarea_layer, name: tr("Restricted areas"), id:'restrictedarea'}];
+                      {url: window.SETTINGS.urls.city_layer, name: tr("Cities"), id: 'city'}];
+
+    landLayers = landLayers.concat(window.SETTINGS.map['restricted_area_types']);
+
     for (var i=0; i<landLayers.length; i++) {
         var landLayer = landLayers[i];
         var layer = new L.ObjectsLayer(null, {
             indexing: false,
-            style: L.Util.extend(window.SETTINGS.map.styles[landLayer.id], { clickable:false })
+            style: L.Util.extend(window.SETTINGS.map.styles[landLayer.id] || {}, { clickable:false })
         });
         layer.load(landLayer.url);
         map.layerscontrol.addOverlay(layer, landLayer.name, tr('Zoning'));
--- geotrek/zoning/tests/test_views.py
+++ geotrek/zoning/tests/test_views.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
 
+from geotrek.zoning.factories import RestrictedAreaTypeFactory
+
 
 class LandLayersViewsTest(TestCase):
 
@@ -9,3 +11,17 @@ class LandLayersViewsTest(TestCase):
             url = reverse('zoning:%s_layer' % layer)
             response = self.client.get(url)
             self.assertEqual(response.status_code, 200)
+
+
+class RestrictedAreaViewsTest(TestCase):
+
+    def test_views_status_is_404_when_type_unknown(self):
+        url = reverse('zoning:restrictedarea_type_layer', kwargs={'type_pk': 1023})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_views_status_is_200_when_type_known(self):
+        t = RestrictedAreaTypeFactory()
+        url = reverse('zoning:restrictedarea_type_layer', kwargs={'type_pk': t.pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
