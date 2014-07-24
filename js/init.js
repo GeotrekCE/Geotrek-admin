e77ce4973ca6fc3f8babc8cd25ae94faa43e67b1
Simon <garric.simon@gmail.com>
2014-07-24 11:59:03 +0200

    Add Scroll to menu
    - Le menu suit la position de l'utilisateur sur le site web en
    soulignant la partie

--- js/init.js
+++ js/init.js
@@ -48,6 +48,126 @@ skel.init({
     }
 });
 
+jQuery.fn.n33_scrolly = function() {            
+    var bh = jQuery('body,html'), t = jQuery(this);
+
+    t.click(function(e) {
+        var h = jQuery(this).attr('href'), target;
+
+        if (h.charAt(0) == '#' && h.length > 1 && (target = jQuery(h)).length > 0)
+        {
+            var pos = Math.max(target.offset().top, 0);
+            e.preventDefault();
+            bh
+                .stop(true, true)
+                .animate({ scrollTop: pos }, 'slow', 'swing');
+        }
+    });
+    
+    return t;
+};
+
+// scrollzer
+jQuery.n33_scrollzer = function(ids, userSettings) {
+
+    var top = jQuery(window), doc = jQuery(document);
+    
+    // Settings
+    var settings = jQuery.extend({
+        activeClassName:    'active',
+        suffix:             '-link',
+        pad:                50,
+        firstHack:          false,
+        lastHack:           false
+    }, userSettings);
+
+    // Variables
+    var k, x, o, l, pos;
+    var lastId, elements = [], links = jQuery();
+
+    // Build elements array
+    for (k in ids)
+    {
+        o = jQuery('#' + ids[k]);
+        l = jQuery('#' + ids[k] + settings.suffix);
+    
+        if (o.length < 1
+        ||  l.length < 1)
+            continue;
+        
+        x = {};
+        x.link = l;
+        x.object = o;
+        elements[ids[k]] = x;
+        links = links.add(l);
+    }
+
+    // Resize event (calculates start/end values for each element)
+    var resizeTimerId, resizeFunc = function() {
+        var x;
+        
+        for (k in elements)
+        {
+            x = elements[k];
+            x.start = Math.ceil(x.object.offset().top) - settings.pad;
+            x.end = x.start + Math.ceil(x.object.innerHeight());
+        }
+        
+        top.trigger('scroll');
+    };
+    
+    top.resize(function() {
+        window.clearTimeout(resizeTimerId);
+        resizeTimerId = window.setTimeout(resizeFunc, 250);
+    });
+
+    // Scroll event (checks to see which element is on the screen and activates its link element)
+    var scrollTimerId, scrollFunc = function() {
+        links.removeClass('scrollzer-locked');
+    };
+
+    top.scroll(function(e) {
+        var i = 0, h, found = false;
+        pos = top.scrollTop();
+
+        window.clearTimeout(scrollTimerId);
+        scrollTimerId = window.setTimeout(scrollFunc, 250);
+        
+        // Step through elements
+        for (k in elements)
+        {
+            if (k != lastId
+            &&  pos >= elements[k].start
+            &&  pos <= elements[k].end)
+            {
+                lastId = k;
+                found = true;
+            }
+            
+            i++;
+        }
+            
+        // If we're using lastHack ...
+        if (settings.lastHack
+        &&  pos + top.height() >= doc.height())
+        {
+            lastId = k;
+            found = true;
+        }
+            
+        // If we found one ...
+        if (found
+        &&  !links.hasClass('scrollzer-locked'))
+        {
+            links.removeClass(settings.activeClassName);
+            elements[lastId].link.addClass(settings.activeClassName);
+        }
+    });
+    
+    // Initial trigger
+    top.trigger('resize');
+};
+
 $(document).ready(function() {
 
     $(".owl-carousel").owlCarousel({
@@ -135,5 +255,49 @@ $(document).ready(function() {
             }
         });
     });
+
+    //jQuery('.scrolly').n33_scrolly();
+
+    // Initialize nav
+    var $nav_a = jQuery('#nav a');
+
+    // Scrollyfy links
+    $nav_a
+        .click(function(e) {
+
+            var t = jQuery(this),
+                href = t.attr('href');
+            
+            if (href[0] != '#')
+                return;
+            
+            e.preventDefault();
+            
+            // Clear active and lock scrollzer until scrolling has stopped
+                $nav_a
+                    .removeClass('active')
+                    .addClass('scrollzer-locked');
+        
+            // Set this link to active
+                t.addClass('active');
+        
+        });
+
+// Initialize scrollzer
+    var ids = [];
+    
+    $nav_a.each(function() {
+        
+        var href = jQuery(this).attr('href');
+        
+        if (href[0] != '#')
+            return;
+    
+        ids.push(href.substring(1));
+    
+    });
+    
+    jQuery.n33_scrollzer(ids, { pad: 200, lastHack: true });
+
 
 });
