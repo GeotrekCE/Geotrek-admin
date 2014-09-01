831bf5c389d4f44a96ce26030ec42e9cfe5d57ec
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2014-09-01 12:20:10 +0200

    Update configuration of tiles

--- .salt/files/demo/config.py
+++ .salt/files/demo/config.py
@@ -2,7 +2,8 @@ from .prod import *
 
 
 LEAFLET_CONFIG['TILES'] = [
-    (gettext_noop('Scan'), 'http://{s}.livembtiles.makina-corpus.net/makina/OSMTopo/{z}/{x}/{y}.png', 'OSM Topo'),
+    (gettext_noop('Scan'), 'http://{s}.livembtiles.makina-corpus.net/makina/OSMTopo/{z}/{x}/{y}.png', '&copy; OSM contributors - Topo by Makina Corpus'),
+    (gettext_noop('OSM'), 'http://{s}.tile.osm.org/{z}/{x}/{y}.png', '&copy; OSM contributors'),
     (gettext_noop('Ortho'), 'https://{s}.tiles.mapbox.com/v3/makina-corpus.i3p1001l/{z}/{x}/{y}.png', '&copy; MapBox Satellite'),
 ]
 LEAFLET_CONFIG['SRID'] = 3857
