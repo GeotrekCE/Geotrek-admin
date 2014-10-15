7e8572393a6ca0b147428d18d9cf3135c8cf1506
Gael UTARD <gael.utard@makina-corpus.com>
2014-10-15 14:16:10 +0200

    Fix extra space in IGN key of PNC settings

--- .salt/files/pnc/config.py
+++ .salt/files/pnc/config.py
@@ -6,10 +6,10 @@ from .prod import *
 # Necessary block of config when maps are not from Geotrek Tilecache :
 LEAFLET_CONFIG['SRID'] = 3857
 LEAFLET_CONFIG['TILES'] = [
-    (gettext_noop('Scan'), 'http://gpp3-wxs.ign.fr/t08ovy851x66gwcumbh5qe9h /geoportail/wmts?LAYER=GEOGRAPHICALGRIDSYSTEMS.MAPS.SCAN-EXPRESS.CLASSIQUE&EXCEPTIONS=image/jpeg&FORMAT=image/jpeg&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}',
+    (gettext_noop('Scan'), 'http://gpp3-wxs.ign.fr/t08ovy851x66gwcumbh5qe9h/geoportail/wmts?LAYER=GEOGRAPHICALGRIDSYSTEMS.MAPS.SCAN-EXPRESS.CLASSIQUE&EXCEPTIONS=image/jpeg&FORMAT=image/jpeg&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}',
      '&copy; IGN - GeoPortail'),
-    (gettext_noop('Ortho'), 'http://gpp3-wxs.ign.fr/t08ovy851x66gwcumbh5qe9h /geoportail/wmts?LAYER=ORTHOIMAGERY.ORTHOPHOTOS&EXCEPTIONS=image/jpeg&FORMAT=image/jpeg&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}',
+    (gettext_noop('Ortho'), 'http://gpp3-wxs.ign.fr/t08ovy851x66gwcumbh5qe9h/geoportail/wmts?LAYER=ORTHOIMAGERY.ORTHOPHOTOS&EXCEPTIONS=image/jpeg&FORMAT=image/jpeg&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}',
      '&copy; IGN - GeoPortail'),
-    (gettext_noop('Cadastre'), 'http://gpp3-wxs.ign.fr/t08ovy851x66gwcumbh5qe9h /geoportail/wmts?LAYER=CADASTRALPARCELS.PARCELS&EXCEPTIONS=image/jpeg&FORMAT=image/png&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}',
+    (gettext_noop('Cadastre'), 'http://gpp3-wxs.ign.fr/t08ovy851x66gwcumbh5qe9h/geoportail/wmts?LAYER=CADASTRALPARCELS.PARCELS&EXCEPTIONS=image/jpeg&FORMAT=image/png&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}',
      '&copy; IGN - GeoPortail'),
 ]
