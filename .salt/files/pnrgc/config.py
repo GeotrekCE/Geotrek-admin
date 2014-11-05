77caef6c4217752bfbeeccc0858aef12fa2d25e8
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2014-11-05 16:51:57 +0100

    PNRGC - Cadastre as overlay

--- .salt/files/pnrgc/config.py
+++ .salt/files/pnrgc/config.py
@@ -18,6 +18,9 @@ LEAFLET_CONFIG['TILES'] = [
      '&copy; IGN - GeoPortail'),
     (gettext_noop('Ortho'), 'http://gpp3-wxs.ign.fr/ilbpmecqb9rugpmbx4ojtt98/geoportail/wmts?LAYER=ORTHOIMAGERY.ORTHOPHOTOS&EXCEPTIONS=image/jpeg&FORMAT=image/jpeg&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}',
      '&copy; IGN - GeoPortail'),
-    (gettext_noop('Cadastre'), 'http://gpp3-wxs.ign.fr/ilbpmecqb9rugpmbx4ojtt98/geoportail/wmts?LAYER=CADASTRALPARCELS.PARCELS&EXCEPTIONS=image/jpeg&FORMAT=image/png&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}',
+]
+
+LEAFLET_CONFIG['OVERLAYS'] = [
+    (gettext_noop('Cadastre'), 'http://gpp3-wxs.ign.fr/ilbpmecqb9rugpmbx4ojtt98/geoportail/wmts?LAYER=CADASTRALPARCELS.PARCELS&STYLE=bdparcellaire&EXCEPTIONS=image/jpeg&FORMAT=image/png&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}',
      '&copy; IGN - GeoPortail'),
 ]
