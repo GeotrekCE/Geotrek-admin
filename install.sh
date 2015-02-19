f9a707cdbbeb29b92e0e7491e2a01cb63442af9d
Gael UTARD <gael.utard@kisis.fr>
2015-02-19 11:10:48 +0100

    Back to development

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ exec 1> install.log 2>&1
 
 #------------------------------------------------------------------------------
 
-VERSION=${VERSION:-0.30.0}
+VERSION=${VERSION:-0.30.1.dev0}
 dev=false
 tests=false
 prod=false
