15108c1477b31bb2a5c1b832aa292c6de4b22569
Gael UTARD <gael.utard@kisis.fr>
2015-03-11 16:29:08 +0100

    Light version for PNR

--- .salt/files/pnr/config.py
+++ .salt/files/pnr/config.py
@@ -13,3 +13,11 @@ LEAFLET_CONFIG['TILES'] = [
     (gettext_noop('Cadastre'), 'http://gpp3-wxs.ign.fr/hqsgqtv0l9d8a2k2ene94fdq/geoportail/wmts?LAYER=CADASTRALPARCELS.PARCELS&EXCEPTIONS=image/jpeg&FORMAT=image/png&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}',
      '&copy; IGN - GeoPortail'),
 ]
+
+TREKKING_TOPOLOGY_ENABLED = False
+TRAIL_MODEL_ENABLED = False
+_INSTALLED_APPS = list(INSTALLED_APPS)
+_INSTALLED_APPS.remove('geotrek.land')
+_INSTALLED_APPS.remove('geotrek.maintenance')
+_INSTALLED_APPS.remove('geotrek.infrastructure')
+INSTALLED_APPS = _INSTALLED_APPS
