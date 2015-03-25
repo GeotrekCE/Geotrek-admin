66b9ebde50794dcb08ae8e077caa2a7fd5ed7b84
Gael UTARD <gael.utard@kisis.fr>
2015-03-25 11:53:08 +0100

    Preparing release 0.33.0

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ exec 1> install.log 2>&1
 
 #------------------------------------------------------------------------------
 
-VERSION=${VERSION:-0.32.3.dev0}
+VERSION=${VERSION:-0.33.0}
 dev=false
 tests=false
 prod=false
