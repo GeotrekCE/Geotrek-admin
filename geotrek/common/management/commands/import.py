d1cf9204dc5089abbb24f7f8d68e565fc65bf390
Gael UTARD <gael.utard@kisis.fr>
2015-04-24 15:24:08 +0200

    Add TourInSoftParser

--- geotrek/common/management/commands/import.py
+++ geotrek/common/management/commands/import.py
@@ -1,8 +1,10 @@
 import importlib
 
+from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
+from django.utils import translation
 
-from geotrek.common.parsers import FatalImportError
+from geotrek.common.parsers import ImportError
 
 
 class Command(BaseCommand):
@@ -10,11 +12,13 @@ class Command(BaseCommand):
     args = '<parser> <shapefile>'
 
     def handle(self, *args, **options):
+        translation.activate(settings.LANGUAGE_CODE)
+
         verbosity = int(options.get('verbosity', '1'))
 
         # Validate arguments
-        if len(args) != 2:
-            raise CommandError("Parser and/or filename missing")
+        if len(args) < 1:
+            raise CommandError("Parser module path missing")
 
         try:
             module_name, class_name = args[0].rsplit('.', 1)
@@ -23,6 +27,9 @@ class Command(BaseCommand):
         except:
             raise CommandError("Failed to import parser class '{0}'".format(args[0]))
 
+        if not hasattr(Parser, 'filename') and len(args) < 2:
+            raise CommandError("File path missing")
+
         def progress_cb(progress):
             if verbosity >= 2:
                 self.stdout.write("{progress:02d}%".format(progress=int(100 * progress)))
@@ -30,8 +37,8 @@ class Command(BaseCommand):
         parser = Parser(progress_cb=progress_cb)
 
         try:
-            parser.parse(args[1])
-        except FatalImportError as e:
+            parser.parse(args[1] if len(args) >= 2 else None)
+        except ImportError as e:
             raise CommandError(e)
 
         if verbosity >= 1:
