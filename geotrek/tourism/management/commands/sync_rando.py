4f00ba0e25f5622712bc86027af8cc3c74adaf49
Gael UTARD <gael.utard@kisis.fr>
2015-04-29 22:33:06 +0200

    Sync resized pictures instead of original ones

--- geotrek/tourism/management/commands/sync_rando.py
+++ geotrek/tourism/management/commands/sync_rando.py
@@ -12,25 +12,25 @@ class Command(BaseCommand):
     def sync_content(self, lang, content):
         self.sync_pdf(lang, content)
 
-        for picture in content.pictures:
-            self.sync_media_file(picture.attachment_file)
+        for picture, resized in content.resized_pictures:
+            self.sync_media_file(resized)
 
     def sync_event(self, lang, event):
         self.sync_pdf(lang, event)
 
-        for picture in event.pictures:
-            self.sync_media_file(picture.attachment_file)
+        for picture, resized in event.resized_pictures:
+            self.sync_media_file(resized)
 
     def sync_tourism(self, lang):
         self.sync_geojson(lang, TouristicContentViewSet, 'touristiccontents')
         self.sync_geojson(lang, TouristicEventViewSet, 'touristicevents')
 
-        contents = tourism_models.TouristicContent.objects.existing()
+        contents = tourism_models.TouristicContent.objects.existing().order_by('pk')
         contents = contents.filter(**{'published_{lang}'.format(lang=lang): True})
         for content in contents:
             self.sync_content(lang, content)
 
-        events = tourism_models.TouristicEvent.objects.existing()
+        events = tourism_models.TouristicEvent.objects.existing().order_by('pk')
         events = events.filter(**{'published_{lang}'.format(lang=lang): True})
         for event in events:
             self.sync_event(lang, event)
@@ -38,11 +38,11 @@ class Command(BaseCommand):
     def sync(self):
         super(Command, self).sync()
 
-        self.sync_static_file('tourism/touristicevent.svg')
-        self.sync_pictograms(tourism_models.InformationDeskType)
-        self.sync_pictograms(tourism_models.TouristicContentCategory)
-        self.sync_pictograms(tourism_models.TouristicContentType)
-        self.sync_pictograms(tourism_models.TouristicEventType)
+        self.sync_static_file('**', 'tourism/touristicevent.svg')
+        self.sync_pictograms('**', tourism_models.InformationDeskType)
+        self.sync_pictograms('**', tourism_models.TouristicContentCategory)
+        self.sync_pictograms('**', tourism_models.TouristicContentType)
+        self.sync_pictograms('**', tourism_models.TouristicEventType)
 
         for lang in settings.MODELTRANSLATION_LANGUAGES:
             self.sync_tourism(lang)
