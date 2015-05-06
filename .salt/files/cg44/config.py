32aae743a5e0c73dbe31239d407f11a8c4e2f6b0
Gael UTARD <gael.utard@kisis.fr>
2015-05-06 19:29:51 +0200

    Fix name of ortho layer for CG44

--- .salt/files/cg44/config.py
+++ .salt/files/cg44/config.py
@@ -13,7 +13,7 @@ LEAFLET_CONFIG['TILES'] = [
     # *** ORTHO ***
     # * DNS admin-rando.loire-atlantique.fr *
     # * DNS geotrek-cg44.makina-corpus.net *
-    (gettext_noop('Scan'), 'http://gpp3-wxs.ign.fr/hqsgqtv0l9d8a2k2ene94fdq/geoportail/wmts?LAYER=ORTHOIMAGERY.ORTHOPHOTOS&EXCEPTIONS=image/jpeg&FORMAT=image/jpeg&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}', '&copy; IGN - GeoPortail'),
+    (gettext_noop('Ortho'), 'http://gpp3-wxs.ign.fr/hqsgqtv0l9d8a2k2ene94fdq/geoportail/wmts?LAYER=ORTHOIMAGERY.ORTHOPHOTOS&EXCEPTIONS=image/jpeg&FORMAT=image/jpeg&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}', '&copy; IGN - GeoPortail'),
     # (gettext_noop('Scan'), 'http://gpp3-wxs.ign.fr/158os34s66xqcvdwzyivp1m0/geoportail/wmts?LAYER=ORTHOIMAGERY.ORTHOPHOTOS&EXCEPTIONS=image/jpeg&FORMAT=image/jpeg&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}', '&copy; IGN - GeoPortail', '&copy; IGN - GeoPortail'),
     # * Ortho vuduciel CG44 (problème avec les tuiles png transparentes pour le découpage) *
     # (gettext_noop('Ortho'), 'http://{s}.tiles.cg44.makina-corpus.net/ortho-2012/{z}/{x}/{y}.jpg', {'attribution': 'Source: Département de Loire-Atlantique', 'tms': True}),
