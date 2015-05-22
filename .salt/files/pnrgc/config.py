279eda78a4c43713e64c8c631b74cbc205bb10b5
Gael UTARD <gael.utard@kisis.fr>
2015-05-22 11:45:27 +0200

    Cleanup HTML when pasting to TinyMCE for PNRGC

--- .salt/files/pnrgc/config.py
+++ .salt/files/pnrgc/config.py
@@ -24,3 +24,25 @@ LEAFLET_CONFIG['OVERLAYS'] = [
     (gettext_noop('Cadastre'), 'http://gpp3-wxs.ign.fr/ilbpmecqb9rugpmbx4ojtt98/geoportail/wmts?LAYER=CADASTRALPARCELS.PARCELS&STYLE=bdparcellaire&EXCEPTIONS=image/jpeg&FORMAT=image/png&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}',
      '&copy; IGN - GeoPortail'),
 ]
+
+TINYMCE_DEFAULT_CONFIG = {
+    'theme': 'advanced',
+    'theme_advanced_buttons1': 'bold,italic,forecolor,separator,bullist,numlist,link,media,separator,undo,redo,separator,cleanup,code',
+    'theme_advanced_buttons2': '',
+    'theme_advanced_buttons3': '',
+    'theme_advanced_statusbar_location': 'bottom',
+    'theme_advanced_toolbar_location': 'top',
+    'theme_advanced_toolbar_align': 'center',
+    'theme_advanced_resizing': True,
+    'theme_advanced_resize_horizontal': False,
+    'forced_root_block': False,
+    'plugins': 'media,paste',
+    'paste_auto_cleanup_on_paste': True,
+    'width': '95%',
+    'resize': "both",
+    'valid_elements': ('@[id|class|style|title|dir<ltr?rtl|lang|xml::lang],'
+                       'a[rel|rev|charset|hreflang|tabindex|accesskey|type|name|href|target|title|class],'
+                       'img[longdesc|usemap|src|border|alt=|title|hspace|vspace|width|height|align],'
+                       'p,em/i,strong/b,div[align],br,ul,li,ol,span[style],'
+                       'iframe[src|frameborder=0|alt|title|width|height|align|name]'),
+}
