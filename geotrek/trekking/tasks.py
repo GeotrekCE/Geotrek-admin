5093ce5106bb68bd4841c8cb9d03f274709f6531
Laurent Saint-Félix <anaethelion@gmail.com>
2015-08-04 16:05:36 +0200

    Add custom directory in settings ini for sync rando output from celery task

--- geotrek/trekking/tasks.py
+++ geotrek/trekking/tasks.py
@@ -1,19 +1,31 @@
+import os
 from django.core.management import call_command
+from django.conf import settings
 from celery import shared_task, current_task
+
 
 @shared_task(name='geotrek.trekking.sync-rando')
 def launch_sync_rando(*args, **kwargs):
+    if not os.path.exists(settings.SYNC_RANDO_ROOT):
+        os.mkdir(settings.SYNC_RANDO_ROOT)
     print('Sync rando started')
     try:
-        current_task.update_state()
+        current_task.update_state(
+            state='PROGRESS',
+            meta={
+                'name': current_task.name
+            }
+        )
         call_command(
             'sync_rando',
-            '/tmp/geotest',
+            settings.SYNC_RANDO_ROOT,
             url=kwargs.get('url'),
             verbosity='2'
         )
-    except:
-        pass
+    except Exception as e:
+        raise
     print('Sync rando ended')
 
-    return 'Sync rando ended'
+    return {
+        'name': current_task.name
+    }
