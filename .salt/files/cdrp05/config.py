5e740d2b75fde1853683225812358cc1d2ae7c2b
Gael UTARD <gael.utard@kisis.fr>
2015-11-03 16:37:43 +0100

    CDRP05 - Scan 25 by default

--- .salt/files/cdrp05/config.py
+++ .salt/files/cdrp05/config.py
@@ -3,8 +3,8 @@ from .prod import *
 
 LEAFLET_CONFIG['SRID'] = 3857
 LEAFLET_CONFIG['TILES'] = [
-    (gettext_noop('Scan Express'), 'http://gpp3-wxs.ign.fr/5l7whtiz6iuh9i76vq1wbln5/geoportail/wmts?LAYER=GEOGRAPHICALGRIDSYSTEMS.MAPS.SCAN-EXPRESS.CLASSIQUE&EXCEPTIONS=image/jpeg&FORMAT=image/jpeg&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}', '&copy; IGN - GeoPortail'),
     (gettext_noop('Scan'), 'http://gpp3-wxs.ign.fr/5l7whtiz6iuh9i76vq1wbln5/geoportail/wmts?LAYER=GEOGRAPHICALGRIDSYSTEMS.MAPS&EXCEPTIONS=image/jpeg&FORMAT=image/jpeg&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}', '&copy; IGN - GeoPortail'),
+    (gettext_noop('Scan Express'), 'http://gpp3-wxs.ign.fr/5l7whtiz6iuh9i76vq1wbln5/geoportail/wmts?LAYER=GEOGRAPHICALGRIDSYSTEMS.MAPS.SCAN-EXPRESS.CLASSIQUE&EXCEPTIONS=image/jpeg&FORMAT=image/jpeg&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}', '&copy; IGN - GeoPortail'),
     (gettext_noop('Ortho'), 'http://gpp3-wxs.ign.fr/5l7whtiz6iuh9i76vq1wbln5/geoportail/wmts?LAYER=ORTHOIMAGERY.ORTHOPHOTOS&EXCEPTIONS=image/jpeg&FORMAT=image/jpeg&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}', '&copy; IGN - GeoPortail'),
 ]
 
