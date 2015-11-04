2b9d3a776781697eab1d5e57af6b6ea1a12b28cc
J-E Castagnede <jean-etienne.castagnede@makina-corpus.com>
2015-11-04 17:31:21 +0100

    fix custom settings

--- geotrek/settings/tests.py
+++ geotrek/settings/tests.py
@@ -17,9 +17,3 @@ SOUTH_TESTS_MIGRATE = False
 MAPENTITY_CONFIG['MAPENTITY_WEASYPRINT'] = False
 
 MAILALERTSUBJECT = "Acknowledgment of feedback email"
-
-DATABASES['default']['NAME'] = "geotrekdb"
-DATABASES['default']['USER'] = "postgres"
-DATABASES['default']['PASSWORD'] = "postgres"
-DATABASES['default']['HOST'] = "10.0.3.92"
-DATABASES['default']['PORT'] = 5432
