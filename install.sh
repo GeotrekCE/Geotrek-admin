bbe70ba2efd479a81c75e2977e53d3b256591672
Gaël UTARD <gael.utard@makina-corpus.com>
2016-01-28 23:41:42 +0100

    Preparing release 2.8.0

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ exec 1> install.log 2>&1
 
 #------------------------------------------------------------------------------
 
-STABLE_VERSION=${STABLE_VERSION:-2.7.2}
+STABLE_VERSION=${STABLE_VERSION:-2.8.0}
 dev=false
 tests=false
 prod=false
