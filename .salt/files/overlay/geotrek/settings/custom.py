25b42a268496aa1a340383437f3e287f851f8ebd
J-E Castagnede <jean-etienne.castagnede@makina-corpus.com>
2016-04-27 12:36:29 +0200

    fix leaflet layers

--- .salt/files/overlay/geotrek/settings/custom.py
+++ .salt/files/overlay/geotrek/settings/custom.py
@@ -6,10 +6,13 @@ LEAFLET_CONFIG['SRID'] = 3857
 LEAFLET_CONFIG['TILES'] = [
     (gettext_noop('Scan'), 'http://gpp3-wxs.ign.fr/u1v3m6passq1fujg84dme9ga/geoportail/wmts?LAYER=GEOGRAPHICALGRIDSYSTEMS.MAPS.SCAN-EXPRESS.CLASSIQUE&EXCEPTIONS=image/jpeg&FORMAT=image/jpeg&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}',
      '&copy; IGN - GeoPortail'),
+    (gettext_noop('Scan Express'), 'http://gpp3-wxs.ign.fr/u1v3m6passq1fujg84dme9ga/geoportail/wmts?LAYER=GEOGRAPHICALGRIDSYSTEMS.MAPS.SCAN-EXPRESS.STANDARD&EXCEPTIONS=image/jpeg&FORMAT=image/jpeg&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}',
+     '&copy; IGN - GeoPortail'),
     (gettext_noop('Ortho'), 'http://gpp3-wxs.ign.fr/u1v3m6passq1fujg84dme9ga/geoportail/wmts?LAYER=ORTHOIMAGERY.ORTHOPHOTOS&EXCEPTIONS=image/jpeg&FORMAT=image/jpeg&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}',
      '&copy; IGN - GeoPortail'),
-    (gettext_noop('Cadastre'), 'http://gpp3-wxs.ign.fr/u1v3m6passq1fujg84dme9ga/geoportail/wmts?LAYER=CADASTRALPARCELS.PARCELS&EXCEPTIONS=image/jpeg&FORMAT=image/png&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}',
-     '&copy; IGN - GeoPortail'),
+    (gettext_noop('OSM'), 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', '(c) OpenStreetMap Contributors'),
+    #(gettext_noop('Cadastre'), 'http://gpp3-wxs.ign.fr/u1v3m6passq1fujg84dme9ga/geoportail/wmts?LAYER=CADASTRALPARCELS.PARCELS&EXCEPTIONS=image/jpeg&FORMAT=image/png&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}',
+    # '&copy; IGN - GeoPortail'),
 ]
 
 SPLIT_TREKS_CATEGORIES_BY_PRACTICE = True
