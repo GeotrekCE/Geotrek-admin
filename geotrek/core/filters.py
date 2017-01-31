52c02ba5be6afd74ff001058ae31039328140710
J-E Castagnede <j.e.castagnede@gmail.com>
2017-01-31 12:00:37 +0100

    fix alias name

--- geotrek/core/filters.py
+++ geotrek/core/filters.py
@@ -7,7 +7,7 @@ from .models import Topology, Path, Trail
 from geotrek.common.filters import OptionalRangeFilter, StructureRelatedFilterSet
 from geotrek.infrastructure.filters import InfrastructureFilterSet, SignageFilterSet
 from geotrek.maintenance.filters import InterventionFilterSet, ProjectFilterSet
-from geotrek.maintenance import models as maintenace_models
+from geotrek.maintenance import models as maintenance_models
 
 
 class TopologyFilter(ModelChoiceFilter):
@@ -58,13 +58,13 @@ class TopologyFilter(ModelChoiceFilter):
             return qs.filter(pk__in=[path.pk for path in set(paths)])
 
         # TODO: This is (amazingly) ugly in terms of OOP. Should refactor overlapping()
-        elif issubclass(qs.model, maintenace_models.Intervention):
+        elif issubclass(qs.model, maintenance_models.Intervention):
             return qs.filter(topology__in=[topo.pk for topo in overlapping])
-        elif issubclass(qs.model, maintenace_models.Project):
+        elif issubclass(qs.model, maintenance_models.Project):
             # Find all interventions overlapping those edges
-            interventions = self._topology_filter(maintenace_models.Intervention.objects.existing()
-                                                                   .select_related('project')
-                                                                   .filter(project__in=qs),
+            interventions = self._topology_filter(maintenance_models.Intervention.objects.existing()
+                                                  .select_related('project')
+                                                  .filter(project__in=qs),
                                                   edges)
             # Return only the projects concerned by the interventions
             projects = []
