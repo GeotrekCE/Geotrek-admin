fd410c8ea13f9539c11bac16e35ddfc4512d7a51
J-E Castagnede <j.e.castagnede@gmail.com>
2017-02-24 11:21:35 +0100

    fix urls

--- geotrek/api/v2/urls.py
+++ geotrek/api/v2/urls.py
@@ -5,9 +5,9 @@ from geotrek.api.v2 import views as api_views
 
 router = routers.SimpleRouter()
 router.register(r'touristiccontent', api_views.TouristicContentViewSet)
-router.register(r'trek', api_views.TrekViewSet)
-#router.register(r'roaming', api_views.RoamingViewSet)
-router.register(r'poi', api_views.POIViewSet)
+router.register(r'trek', api_views.TrekViewSet, base_name='trek')
+router.register(r'roaming', api_views.RoamingViewSet, base_name='roaming')
+router.register(r'poi', api_views.POIViewSet, base_name='poi')
 
 urlpatterns = [
     #url(r'^forgot-password/$', ForgotPasswordFormView.as_view()),
