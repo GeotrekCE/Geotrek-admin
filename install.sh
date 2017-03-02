9baf619f1ffc5cee50b1e5e01485e849f6bd26ee
J-E Castagnede <j.e.castagnede@gmail.com>
2017-03-02 17:12:34 +0100

    fix travis

--- install.sh
+++ install.sh
@@ -261,7 +261,7 @@ function screamshotter_system_dependencies {
         ln -sf $libpath/*phantomjs*/bin/phantomjs $binpath/phantomjs
         echo_step "PhantomJS installed..."
         echo_step "Downloading CasperJS..."
-        wget --quiet https://codeload.github.com/casperjs/casperjs/zip/1.1.4 -O casperjs.zip
+        wget --quiet https://github.com/n1k0/casperjs/archive/1.1-beta3.zip -O casperjs.zip
         if [ ! $? -eq 0 ]; then exit_error 9 "Failed to download casperjs"; fi
         echo_step "CasperJS downloaded..."
         rm -rf $libpath/*casperjs*/
