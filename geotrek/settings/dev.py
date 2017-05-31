780b1a12b8f7183ccfabae5c0b956c980e8471f7
J-E Castagnede <j.e.castagnede@gmail.com>
2017-05-31 17:29:03 +0200

    fix debugtoolbar

--- geotrek/settings/dev.py
+++ geotrek/settings/dev.py
@@ -6,20 +6,23 @@ from .default import *
 
 DEBUG = True
 TEMPLATES[1]['OPTIONS']['debug'] = True
+DEBUG_TOOLBAR = False
 
 #
 # Developper additions
 # ..........................
 
 INSTALLED_APPS = (
-    # 'debug_toolbar',
     'django_extensions',
 ) + INSTALLED_APPS
 
 INTERNAL_IPS = (
     '127.0.0.1',  # localhost default
-    '10.0.3.1',  # lxc default
 )
+
+ALLOWED_HOSTS = [
+    '*',
+]
 
 #
 # Use some default tiles
@@ -37,3 +40,9 @@ LOGGING['loggers']['geotrek']['level'] = 'DEBUG'
 LOGGING['loggers']['']['level'] = 'DEBUG'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+try:
+    from .local import *
+    # set local settings for dev
+except ImportError:
+    pass
--- geotrek/urls.py
+++ geotrek/urls.py
@@ -40,3 +40,12 @@ urlpatterns = patterns(
 
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static.static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG and settings.DEBUG_TOOLBAR:
+    try:
+        import debug_toolbar
+        urlpatterns = [
+            url(r'^__debug__/', include(debug_toolbar.urls)),
+        ] + urlpatterns
+    except ImportError:
+        pass
