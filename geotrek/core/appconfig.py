3a37a219ee696c758d6b61c11de2019498ae8a52
J-E Castagnede <j.e.castagnede@gmail.com>
2017-06-16 10:58:58 +0200

    fix signals with django 1.8

---
+++ geotrek/core/appconfig.py
@@ -0,0 +1,15 @@
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+from django.db.models.signals import post_migrate, pre_migrate
+
+from geotrek.common.utils.postgresql import pm_callback, check_srid_has_meter_unit
+
+
+class GeotrekConfig(AppConfig):
+    def __init__(self, *args, **kwargs):
+        pre_migrate.connect(check_srid_has_meter_unit, sender=self, dispatch_uid='geotrek.core.checksrid')
+        super(GeotrekConfig, self).__init__(*args, **kwargs)
+
+    def ready(self):
+        post_migrate.connect(pm_callback, sender=self, dispatch_uid='geotrek.core.movetoschemas')
