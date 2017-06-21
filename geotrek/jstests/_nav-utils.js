b7775f4c870b822d5fb94a795a02f23a9b4b789c
J-E Castagnede <j.e.castagnede@gmail.com>
2017-06-21 18:01:15 +0200

    fix mapentity version

--- geotrek/jstests/_nav-utils.js
+++ geotrek/jstests/_nav-utils.js
@@ -6,7 +6,7 @@ module.exports = (function() {
 
     function setUp() {
         casper.options.viewportSize = {width: 1280, height: 768};
-        casper.options.waitTimeout = 100000;
+        casper.options.waitTimeout = 20000;
         casper.userAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.97 Safari/537.11 FrontendTest');
 
         casper.on('remote.message', function(msg) {
