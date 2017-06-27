593a98374f78554c0633deecad913c754e2d3001
J-E Castagnede <j.e.castagnede@gmail.com>
2017-06-27 17:33:37 +0200

    factorize serializer specification

---
+++ geotrek/api/v2/pagination.py
@@ -0,0 +1,9 @@
+from __future__ import unicode_literals
+
+from rest_framework.pagination import PageNumberPagination
+
+
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 100
+    page_size_query_param = 'page_size'
+    max_page_size = 1000
---
+++ geotrek/api/v2/viewsets.py
@@ -0,0 +1,50 @@
+from __future__ import unicode_literals
+
+from django_filters.rest_framework.backends import DjangoFilterBackend
+from rest_framework import viewsets
+from rest_framework_extensions.mixins import DetailSerializerMixin
+from rest_framework_gis.filters import InBBOXFilter, DistanceToPointFilter
+
+from geotrek.api.v2 import pagination as api_pagination, serializers as api_serializers
+
+
+class GeotrekViewset(DetailSerializerMixin, viewsets.ReadOnlyModelViewSet):
+    filter_backends = (DjangoFilterBackend, InBBOXFilter, DistanceToPointFilter)
+    distance_filter_field = 'geom'
+    pagination_class = api_pagination.StandardResultsSetPagination
+
+    def get_serializer_class(self):
+        base_serializer_class = super(GeotrekViewset, self).get_serializer_class()
+        format_output = self.request.query_params.get('format', 'json')
+        dimension = self.request.query_params.get('dim', '2')
+        final_class = None
+
+        if format_output == 'geojson':
+            if dimension == '3':
+                class GeneratedGeo3DSerializer(api_serializers.Base3DSerializer,
+                                               api_serializers.BaseGeoJSONSerializer,
+                                               base_serializer_class):
+                    class Meta(api_serializers.BaseGeoJSONSerializer.Meta,
+                               base_serializer_class.Meta):
+                        pass
+                final_class = GeneratedGeo3DSerializer
+
+            else:
+                class GeneratedGeoSerializer(api_serializers.BaseGeoJSONSerializer,
+                                            base_serializer_class):
+                    class Meta(api_serializers.BaseGeoJSONSerializer.Meta,
+                                            base_serializer_class.Meta):
+                        pass
+                final_class = GeneratedGeoSerializer
+        else:
+            if dimension == '3':
+                class Generated3DSerializer(api_serializers.Base3DSerializer,
+                                           base_serializer_class):
+                    class Meta(base_serializer_class.Meta):
+                        pass
+                final_class = Generated3DSerializer
+
+            else:
+                final_class = base_serializer_class
+
+        return final_class
