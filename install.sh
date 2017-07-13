31ea7e2cde7ed01a3bf60d88123dbbfcbcd7f6b8
J-E Castagnede <j.e.castagnede@gmail.com>
2017-07-13 11:28:15 +0200

    preparing 2.15.0

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ exec 1> install.log 2>&1
 
 #------------------------------------------------------------------------------
 
-STABLE_VERSION=${STABLE_VERSION:-2.14.3}
+STABLE_VERSION=${STABLE_VERSION:-2.15.0}
 dev=false
 tests=false
 prod=false
