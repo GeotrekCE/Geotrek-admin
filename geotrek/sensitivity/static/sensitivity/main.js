ddb722b3fb0c29192dfcd504cf55f5f99d4680eb
Gaël UTARD <gael.utard@makina-corpus.com>
2017-11-29 14:52:18 +0100

    Add sensitive area layer to other modules

---
+++ geotrek/sensitivity/static/sensitivity/main.js
@@ -0,0 +1,15 @@
+$(window).on('entity:map', function (e, data) {
+
+    if (data.modelname != 'sensitivearea') {
+        var map = data.map;
+
+        // Show sensitivity layer in application maps
+        var layer = new L.ObjectsLayer(null, {
+            modelname: 'sensitivearea',
+            style: L.Util.extend(window.SETTINGS.map.styles['sensitivearea'] || {}, {clickable:false}),
+        });
+        var url = window.SETTINGS.urls['sensitivearea_layer'];
+        layer.load(url);
+        map.layerscontrol.addOverlay(layer, tr('sensitivearea'), tr('Sensitivity'));
+    }
+});
