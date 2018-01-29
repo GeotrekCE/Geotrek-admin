50f98e4d5976965abca306433f2e540b708f3e7d
J-E Castagnede <j.e.castagnede@gmail.com>
2018-01-29 14:15:24 +0100

    delete useless test (tested by django_celery_results)

--- geotrek/common/tests/test_tasks.py
+++ geotrek/common/tests/test_tasks.py
@@ -23,14 +23,3 @@ class TasksTest(TestCase):
             class_name='haricot',
             module_name='toto'
         )
-
-    def test_import_return(self):
-        filename = os.path.join(os.path.dirname(__file__), 'data', 'organism.xls')
-        task = import_datas.delay('AttachmentParser', filename, 'geotrek.common.tests.test_parsers')
-
-        self.assertEqual(task.status, 'SUCCESS')
-        self.assertEqual(task.result['parser'], 'AttachmentParser')
-        self.assertEqual(task.result['filename'], 'organism.xls')
-        self.assertEqual(task.result['current'], 100)
-        self.assertEqual(task.result['total'], 100)
-        self.assertEqual(task.result['name'], 'geotrek.common.import-file')
