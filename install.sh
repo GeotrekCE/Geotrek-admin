9272aa3bad729020b7fc15a97f7e00ff378d68ef
Gaël UTARD <gael.utard@makina-corpus.com>
2018-02-07 17:57:02 +0100

    Preparing release 2.17.2

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ exec 1> install.log 2>&1
 
 #------------------------------------------------------------------------------
 
-STABLE_VERSION=${STABLE_VERSION:-2.17.1}
+STABLE_VERSION=${STABLE_VERSION:-2.17.2}
 dev=false
 tests=false
 prod=false
