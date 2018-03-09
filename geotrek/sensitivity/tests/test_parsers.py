02c8f2b436fd52f99b6482d67e2d21177753c975
Gaël UTARD <gael.utard@makina-corpus.com>
2018-03-09 17:25:22 +0100

    Test species import parser

--- geotrek/sensitivity/tests/test_parsers.py
+++ geotrek/sensitivity/tests/test_parsers.py
@@ -1,5 +1,6 @@
 # -*- encoding: utf-8 -*-
 
+import os
 import mock
 import requests
 
@@ -7,6 +8,7 @@ from django.core.management import call_command
 from django.test import TestCase
 
 from geotrek.sensitivity.models import SportPractice, Species, SensitiveArea
+from geotrek.sensitivity.factories import SpeciesFactory
 
 
 class BiodivParserTests(TestCase):
@@ -68,6 +70,7 @@ class BiodivParserTests(TestCase):
         species = Species.objects.get()
         area = SensitiveArea.objects.get()
         self.assertEqual(practice.name, "Land")
+        self.assertEqual(practice.name_fr, "Terrestre")
         self.assertEqual(species.name, u"Black grouse")
         self.assertEqual(species.name_fr, u"Tétras lyre")
         self.assertTrue(species.period01)
@@ -78,3 +81,23 @@ class BiodivParserTests(TestCase):
         self.assertEqual(area.description, "Blahblah")
         self.assertEqual(area.description_fr, "Blabla")
         self.assertEqual(area.eid, '1')
+
+
+class SpeciesSensitiveAreaShapeParserTest(TestCase):
+    def test_good_data(self):
+        filename = os.path.join(os.path.dirname(__file__), 'data', 'species.shp')
+        species = SpeciesFactory(name=u"Aigle royal")
+        call_command('import', 'geotrek.sensitivity.parsers.SpeciesSensitiveAreaShapeParser', filename, verbosity=0)
+        area = SensitiveArea.objects.get()
+        self.assertEqual(area.species, species)
+        self.assertEqual(area.contact, u"Contact")
+        self.assertEqual(area.description, u"Test UTF8 éêè")
+        self.assertEqual(
+            area.geom.wkt,
+            'POLYGON ((929315.3613368584774435 6483309.4435053952038288, '
+            '929200.3539448172086850 6483204.0200626906007528, 928404.8861498644109815 6482494.8078117696568370, '
+            '928194.0392644553212449 6482082.6979902880266309, 927925.6886830255389214 6481210.5586006408557296, '
+            '927676.5060002692043781 6481287.2301953341811895, 927772.3454936370253563 6481498.0770807461813092, '
+            '927887.3528856782941148 6481900.6029528900980949, 928184.4553151186555624 6482600.2312544705346227, '
+            '928625.3169846105156466 6483520.2903908034786582, 929162.0181474700802937 6483664.0496308589354157, '
+            '929315.3613368584774435 6483309.4435053952038288))')
