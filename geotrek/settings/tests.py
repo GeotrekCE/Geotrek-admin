11e524471cc3c24084c4f00f5fdfd826fc927864
Timothee <timothee.de-montety@makina-corpus.com>
2018-03-29 11:34:24 +0200

    remove notmigrations now None

--- geotrek/settings/tests.py
+++ geotrek/settings/tests.py
@@ -30,7 +30,7 @@ class DisableMigrations(object):
         return True
 
     def __getitem__(self, item):
-        return 'notmigrations'
+        return None
 
 
 MIGRATION_MODULES = DisableMigrations()
