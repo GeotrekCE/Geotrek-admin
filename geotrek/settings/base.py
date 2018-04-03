e13aa962eb1865ae0e5afe6a43c3bf7a531d8022
LePetitTim <timothee.de_montety@isen-lille.fr>
2018-04-03 11:31:19 +0200

    Add comment for Password_Hashers

--- geotrek/settings/base.py
+++ geotrek/settings/base.py
@@ -595,6 +595,7 @@ SWAGGER_SETTINGS = {
 
 SENSITIVITY_DEFAULT_RADIUS = 100  # meters
 SENSITIVE_AREA_INTERSECTION_MARGIN = 500  # meters (always used)
+# Used for extern authent
 PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
     'django.contrib.auth.hashers.UnsaltedMD5PasswordHasher',
