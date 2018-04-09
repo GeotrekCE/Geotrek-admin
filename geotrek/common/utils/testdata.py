76248b967f5971dc873c763684c9340fe334ddde
Timothee <timothee.de-montety@makina-corpus.com>
2018-04-09 12:15:54 +0200

    Add B64 decoding

--- geotrek/common/utils/testdata.py
+++ geotrek/common/utils/testdata.py
@@ -1,6 +1,7 @@
 from django.core.files.uploadedfile import SimpleUploadedFile
 
 import factory
+import base64
 
 
 # Produce a small red dot
@@ -8,7 +9,7 @@ IMG_FILE = 'iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38G
 
 
 def get_dummy_img():
-    return IMG_FILE.decode('base64')
+    return base64.b64decode(IMG_FILE)
 
 
 def get_dummy_uploaded_image(name='dummy_img.png'):
