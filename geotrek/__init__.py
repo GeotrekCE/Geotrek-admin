6e1398ebe062e1791ed4260e942cce08f23ae334
Timothee <timothee.de-montety@makina-corpus.com>
2018-04-09 11:05:21 +0200

    Change conf (will be remove)

--- geotrek/__init__.py
+++ geotrek/__init__.py
@@ -1,7 +1,4 @@
-from __future__ import absolute_import
-
-pkg_resources = __import__('pkg_resources')
-distribution = pkg_resources.get_distribution('geotrek')
+import os
 
 #: Module version, as defined in PEP-0396.
-__version__ = distribution.version
+__version__ = open("VERSION").read().strip()
