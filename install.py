ec4ada28da8b8c5106c1d507f11ca16c3dcf5d1d
Timothee <timothee.de-montety@makina-corpus.com>
2018-04-09 12:20:42 +0200

    Create requirements link to the settings.

---
+++ install.py
@@ -0,0 +1,20 @@
+import invoke
+import sys
+
+invoke.run("sudo apt-get install libgeos-c1v5 libproj9")
+invoke.run("sudo apt-get install -y -q --no-upgrade libjson0 gdal-bin libgdal-dev libssl-dev binutils libproj-dev")
+invoke.run("sudo apt-get install -y -qq language-pack-en-base language-pack-fr-base", hide=True, warn=True)
+invoke.run("sudo locale-gen fr_FR.UTF-8",hide=True, warn=True)
+
+
+if sys.argv[1] == "true":
+    invoke.run("sudo bin/pip install -r requirements.txt", hide=True, warn=True)
+    invoke.run("sudo bin/pip install -r requirements_dev.txt", hide=True, warn=True)
+elif sys.argv[2] == "true":
+    invoke.run("sudo bin/pip install -r requirements.txt", hide=True, warn=True)
+    invoke.run("sudo bin/pip install -r requirements_tests.txt", hide=True, warn=True)
+else:
+    print("coucou")
+    invoke.run("sudo bin/pip install -r requirements.txt", warn=True)
+
+invoke.run("bin/pip install -r editable.txt -e", warn=True)
