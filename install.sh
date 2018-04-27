69d41c6573b856ba602a73adc2e9dc8c834d0958
Gaël UTARD <gael.utard@makina-corpus.com>
2018-04-27 14:10:46 +0200

    Preparing release 2.18.3

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ cd "$(dirname "$0")"
 
 #------------------------------------------------------------------------------
 
-STABLE_VERSION=${STABLE_VERSION:-2.18.2}
+STABLE_VERSION=${STABLE_VERSION:-2.18.3}
 dev=false
 tests=false
 prod=false
