a5e8ac27542ba02003eb9210a8eda5cb6c89543f
J-E Castagnede <j.e.castagnede@gmail.com>
2018-05-02 11:07:36 +0200

    back to development

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ cd "$(dirname "$0")"
 
 #------------------------------------------------------------------------------
 
-STABLE_VERSION=${STABLE_VERSION:-2.18.3}
+STABLE_VERSION=${STABLE_VERSION:-2.18.4}
 dev=false
 tests=false
 prod=false
