90231af44c5c5942e1b44fc7a3c5c0d0ba26fe7c
J-E Castagnede <j.e.castagnede@gmail.com>
2018-05-11 15:32:11 +0200

    fix docker push

--- docker/docker_push.sh
+++ docker/docker_push.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
 docker login -u "$DOCKER_LOGIN" -p "$DOCKER_PASSWORD";
+
 if [[ $ACTION == test ]];
 then
-      docker push geotrekce/admin:$GEOTREK_VERSION;
+  docker tag geotrek $GEOTREK_VERSION;
+  docker push geotrekce/admin:$GEOTREK_VERSION;
+  docker tag geotrek geotrekce/admin:latest;
+  docker push geotrekce/admin:latest;
+
+  if [[ $GEOTREK_VERSION != *.dev* ]];
+    then
+      docker tag geotrek geotrekce/admin:latest;
+      docker push geotrekce/admin:latest;
+    fi
 fi
+if [[ $ACTION == test ]]; then docker tag geotrek $GEOTREK_VERSION; fi
