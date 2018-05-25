851111f52ab47025aca54f4ed217c40670bce434
J-E Castagnede <j.e.castagnede@gmail.com>
2018-05-25 08:43:56 +0200

    Update docker_push.sh
    Fix without ACTION

--- docker/docker_push.sh
+++ docker/docker_push.sh
@@ -2,15 +2,11 @@
 
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_LOGIN" --password-stdin
 
-if [[ $ACTION == deploy ]];
+docker tag geotrek geotrekce/admin:$GEOTREK_VERSION;
+docker push geotrekce/admin:$GEOTREK_VERSION;
+
+if [[ $GEOTREK_VERSION != *.dev* ]];
 then
-  docker tag geotrek geotrekce/admin:$GEOTREK_VERSION;
-  docker push geotrekce/admin:$GEOTREK_VERSION;
-
-  if [[ $GEOTREK_VERSION != *.dev* ]];
-    then
-      docker tag geotrek geotrekce/admin:latest;
-      docker push geotrekce/admin:latest;
-    fi
+     docker tag geotrek geotrekce/admin:latest;
+     docker push geotrekce/admin:latest;
 fi
-
