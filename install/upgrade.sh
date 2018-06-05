31161076dbefd0187a36a8ec7956f8a46b7ec473
Timothee <timothee.de-montety@makina-corpus.com>
2018-06-05 16:24:50 +0200

    Fix Upgrade pg_dump/restor

--- install/upgrade.sh
+++ install/upgrade.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-export GEOTREK_VERSION=$(docker-compose run web cat VERSION)
+export GEOTREK_VERSION=$(docker-compose run web cat VERSION | tr -d '\r')
+echo $GEOTREK_VERSION
 source .env
 
 sudo systemctl stop geotrek
 
 export PGPASSWORD=$(echo $POSTGRES_PASSWORD)
-pg_dump -Fc --no-acl --no-owner -h 127.0.0.1 -U $POSTGRES_USER $POSTGRES_DB > geotrek_$GEOTREK_VERSION.dump
+docker-compose run web bash -c "PGPASSWORD=$POSTGRES_PASSWORD pg_dump -Fc --no-acl --no-owner -h postgres -w -U $POSTGRES_USER $POSTGRES_DB > /app/src/var/geotrek_$GEOTREK_VERSION.dump"
+
 docker tag geotrekce/admin:latest geotrekce/admin:$GEOTREK_VERSION
 
 if docker-compose run web update.sh; then
@@ -16,6 +18,6 @@ else
     docker tag geotrekce/admin:$GEOTREK_VERSION geotrekce/admin:latest
     docker pull geotrekce/admin:$GEOTREK_VERSION
 fi
-pg_dump -Fc --no-acl --no-owner -h 127.0.0.1 -U $POSTGRES_USER $POSTGRES_DB > geotrek_$GEOTREK_VERSION.dump
+docker-compose run web bash -c "pg_restore -Fc --dbname=$POSTGRES_DB /app/src/var/geotrek_$GEOTREK_VERSION.dump"
 
 sudo systemctl start geotrek
