16cb69470354c38a372d2b647d940b4c9656f059
Timothee <timothee.de-montety@makina-corpus.com>
2018-06-06 11:15:55 +0200

    Update upgrade.sh

--- install/upgrade.sh
+++ install/upgrade.sh
@@ -9,15 +9,13 @@ sudo systemctl stop geotrek
 export PGPASSWORD=$(echo $POSTGRES_PASSWORD)
 docker-compose run web bash -c "PGPASSWORD=$POSTGRES_PASSWORD pg_dump -Fc --no-acl --no-owner -h postgres -w -U $POSTGRES_USER $POSTGRES_DB > /app/src/var/geotrek_$GEOTREK_VERSION.dump"
 
+docker pull geotrekce/admin:latest
 docker tag geotrekce/admin:latest geotrekce/admin:$GEOTREK_VERSION
 
-if docker-compose run web update.sh; then
-# Restore
+if ! docker-compose run web update.sh; then
     docker rmi geotrekce/admin:latest
-else
     docker tag geotrekce/admin:$GEOTREK_VERSION geotrekce/admin:latest
-    docker pull geotrekce/admin:$GEOTREK_VERSION
+    docker-compose run web bash -c "PGPASSWORD=$POSTGRES_PASSWORD pg_restore -c -Fc --no-owner -h postgres -w -U $POSTGRES_USER --dbname=$POSTGRES_DB /app/src/var/geotrek_$GEOTREK_VERSION.dump"
 fi
-docker-compose run web bash -c "PGPASSWORD=$POSTGRES_PASSWORD pg_restore -Fc --no-owner -h postgres -w -U $POSTGRES_USER --dbname=$POSTGRES_DB /app/src/var/geotrek_$GEOTREK_VERSION.dump"
 
 sudo systemctl start geotrek
