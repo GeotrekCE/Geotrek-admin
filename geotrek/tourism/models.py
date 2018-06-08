299b66de3c2527aa40fab8a8a72e91458d53dfb4
Timothee <timothee.de-montety@makina-corpus.com>
2018-06-08 15:16:07 +0200

    Fix import with modeltranslation

--- geotrek/tourism/models.py
+++ geotrek/tourism/models.py
@@ -13,10 +13,6 @@ from easy_thumbnails.files import get_thumbnailer
 from mapentity.registry import registry
 from mapentity.models import MapEntityMixin
 from mapentity.serializers import plain_text, smart_plain_text
-if 'modeltranslation' in settings.INSTALLED_APPS:
-    from modeltranslation.manager import MultilingualManager
-else:
-    from django.db.models import Manager as MultilingualManager
 
 from geotrek.authent.models import StructureRelated
 from geotrek.core.models import Topology
@@ -29,6 +25,10 @@ from geotrek.common.utils import intersecting
 
 from extended_choices import Choices
 
+if 'modeltranslation' in settings.INSTALLED_APPS:
+    from modeltranslation.manager import MultilingualManager
+else:
+    from django.db.models import Manager as MultilingualManager
 
 logger = logging.getLogger(__name__)
 
