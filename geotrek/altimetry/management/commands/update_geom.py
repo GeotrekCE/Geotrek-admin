a99088c2bbebd6b300da0729c61300a30f65c9c5
Timothee <timothee.de-montety@makina-corpus.com>
2018-07-16 11:21:18 +0200

    Change name function

---
+++ geotrek/altimetry/management/commands/update_geom.py
@@ -0,0 +1,27 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.db import connection
+from django.conf import settings
+from optparse import make_option
+import os.path
+from subprocess import call
+import tempfile
+
+
+class Command(BaseCommand):
+    help = 'Reload geometries to update altimetry.\n'
+
+    def handle(self, *args, **options):
+        cur = connection.cursor()
+        sql = 'SELECT * FROM raster_columns WHERE r_table_name = \'mnt\''
+        cur.execute(sql)
+        dem_exists = cur.rowcount != 0
+        cur.close()
+        if not dem_exists:
+            raise CommandError("There is no DEM, the command won't do anything")
+        self.stdout.write('Everything looks fine, we can start reload geometries\n')
+        cur = connection.cursor()
+        sql = 'SELECT update_evenement_geom_when_troncon_changes_nt();'
+        cur.execute(sql)
+        cur.close()
+        self.stdout.write('Geometries reloaded\n')
+        return
