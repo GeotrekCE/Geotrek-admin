8234c2acf13eb471cfd0d82dc8993e40199031a2
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2018-07-20 15:24:27 +0200

    Remove duplicate test

--- geotrek/common/tests/test_filters.py
+++ geotrek/common/tests/test_filters.py
@@ -17,10 +17,3 @@ class ProjectYearsFilterTest(TestCase):
         self.assertIn(p, filter.qs)
         self.assertEqual(len(filter.qs), 3)
         # We get all project if it's a wrong filter
-
-    def test_filter_year_with_string(self):
-        filter = ProjectFilterSet(data={'in_year': 'toto'})
-        p = ProjectFactory.create(begin_year=1200, end_year=1300)
-        self.assertIn(p, filter.qs)
-        self.assertEqual(len(filter.qs), 3)
-        # We get all project if it's a wrong filter
