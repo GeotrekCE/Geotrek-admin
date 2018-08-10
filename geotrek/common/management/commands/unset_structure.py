15127fcf4617daa79efd23b775639518c98dc5c1
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2018-08-10 17:51:57 +0200

    Fix Command for ManytoMany fields

--- geotrek/common/management/commands/unset_structure.py
+++ geotrek/common/management/commands/unset_structure.py
@@ -4,6 +4,7 @@ from django.apps import apps
 from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
 from django.utils import translation
+from django.db.models import ManyToManyField
 
 
 class Command(BaseCommand):
@@ -13,6 +14,13 @@ class Command(BaseCommand):
         parser.add_argument('--all', action='store_true', help="Manage all models")
         parser.add_argument('--list', action='store_true', help="Show available models to manage")
         parser.add_argument('model', nargs='*', help="List of choices to manage")
+
+    def get_new_fk(self, old_fk, RelatedModel, related_fields):
+        if old_fk and old_fk.structure:
+            kwargs = {field.name: getattr(old_fk, field.name) for field in related_fields}
+            kwargs['structure'] = None
+            return RelatedModel.objects.get(**kwargs)
+        return old_fk
 
     def get_all_items(self):
         self.items = defaultdict(list)
@@ -30,7 +38,7 @@ class Command(BaseCommand):
                         continue
                     if 'structure' not in [f.name for f in RelatedModel._meta.get_fields()]:
                         continue
-                    self.items[RelatedModel].append((Model, field.name))
+                    self.items[RelatedModel].append((Model, field.name, isinstance(field, ManyToManyField)))
 
     def handle_related_model(self, RelatedModel, subitems):
         if self.options['verbosity'] > 0:
@@ -46,20 +54,20 @@ class Command(BaseCommand):
                 self.stdout.write(u"  Create {}".format(new_obj))
 
         # Update foreign keys
-        for Model, fk_name in subitems:
+        for Model, fk_name, m2m in subitems:
             if self.options['verbosity'] > 0:
                 self.stdout.write("  Handle field {} of model {}".format(fk_name, Model._meta.model_name))
             related_fields = [field for field in RelatedModel._meta.get_fields() if not field.auto_created]
             for obj in Model.objects.all():
                 old_fk = getattr(obj, fk_name)
-                if old_fk and old_fk.structure:
-                    kwargs = {field.name: getattr(old_fk, field.name) for field in related_fields}
-                    kwargs['structure'] = None
-                    new_fk = RelatedModel.objects.get(**kwargs)
-                    setattr(obj, fk_name, new_fk)
-                    obj.save()
-                    if self.options['verbosity'] > 0:
-                        self.stdout.write(u"    Update {}".format(obj))
+                if m2m:
+                    new_fk = [self.get_new_fk(value, RelatedModel, related_fields) for value in old_fk.all()]
+                else:
+                    new_fk = self.get_new_fk(old_fk, RelatedModel, related_fields)
+                setattr(obj, fk_name, new_fk)
+                obj.save()
+                if self.options['verbosity'] > 0:
+                    self.stdout.write(u"    Update {}".format(obj))
 
         # Remove related objects with structure!=None
         related_objs = RelatedModel.objects.exclude(structure=None)
