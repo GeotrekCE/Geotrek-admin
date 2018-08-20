b3395b7bc8fb6af51f2008de0cb56a25940a074a
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2018-08-20 14:34:35 +0200

    Create function remove duplicate path (without topologies)

---
+++ geotrek/core/management/commands/remove_duplicate_path.py
@@ -0,0 +1,41 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.db import connection
+import itertools
+
+from geotrek.core.models import Path
+
+
+class Command(BaseCommand):
+    help = 'Remove all duplicate path (same geom).\n'
+    help += 'Do not remove path with topology.'
+
+    def handle(self, *args, **options):
+        verbosity = options['verbosity']
+        cursor = connection.cursor()
+        sqlquery = """
+        SELECT DISTINCT t1.id, t2.id FROM l_t_troncon t1
+         JOIN l_t_troncon t2 ON t1.id != t2.id AND ST_EQUALS(t1.geom, t2.geom) ORDER BY t1.id"""
+        cursor.execute(sqlquery)
+        list_topologies = cursor.fetchall()
+        list_topologies = [sorted(duplicate) for duplicate in list_topologies]
+        list_topologies.sort()
+        list_topologies = list(k for k, _ in itertools.groupby(list_topologies))
+        to_delete = []
+        for path1_pk, path2_pk in list_topologies:
+            path1 = Path.objects.get(pk=path1_pk)
+            path2 = Path.objects.get(pk=path2_pk)
+            if path1.topology_set.exists() and path2.topology_set.exists():
+                continue
+            elif path1.topology_set.exists():
+                if path2 not in to_delete:
+                    to_delete.append(path2)
+            elif path2.topology_set.exists():
+                if path1 not in to_delete:
+                    to_delete.append(path1)
+            else:
+                if path2 not in to_delete:
+                    to_delete.append(path2)
+        if verbosity > 0:
+            self.stdout.write("Delete duplicate paths: %s" % [str(path.name) for path in to_delete])
+        for path in to_delete:
+            path.delete()
