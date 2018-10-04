a09d2bb92344fcf094804972a2ca2a6f263066c3
J-E Castagnede <j.e.castagnede@gmail.com>
2018-10-04 12:36:18 +0200

    fix tests

--- geotrek/core/tests/test_commands.py
+++ geotrek/core/tests/test_commands.py
@@ -7,35 +7,22 @@ from geotrek.trekking.factories import POIFactory
 
 
 class RemoveDuplicatePathTest(TestCase):
-
-    def test_remove_no_duplicate_path(self):
-        """
-        This test check that we remove 1 of the duplicate path and keep ones with topologies.
-
-                poi3 (only on p4)
-        +-------o------+                    p5 is reversed.
-        p3/p4/p5
-
-        +-------o------+        +--------+
-        p1/p2   poi1/poi2       p6
-
-        We get at the end p1, p2, p4, p6.
-        """
+    def setUp(self):
         geom_1 = LineString((0, 0), (1, 0), (2, 0))
-        p1 = Path.objects.create(name='First Path', geom=geom_1)
-        p2 = Path.objects.create(name='Second Path', geom=geom_1)
+        self.p1 = Path.objects.create(name='First Path', geom=geom_1)
+        self.p2 = Path.objects.create(name='Second Path', geom=geom_1)
 
         geom_2 = LineString((0, 2), (1, 2), (2, 2))
-        p3 = Path.objects.create(name='Third Path', geom=geom_2)
-        p4 = Path.objects.create(name='Fourth Path', geom=geom_2)
+        self.p3 = Path.objects.create(name='Third Path', geom=geom_2)
+        self.p4 = Path.objects.create(name='Fourth Path', geom=geom_2)
 
         geom_3 = LineString((2, 2), (1, 2), (0, 2))
-        p5 = Path.objects.create(name='Fifth Path', geom=geom_3)
+        self.p5 = Path.objects.create(name='Fifth Path', geom=geom_3)
 
         geom_4 = LineString((4, 0), (6, 0))
 
-        p6 = Path.objects.create(name='Sixth Path', geom=geom_4)
-        p7 = Path.objects.create(name='Seventh Path', geom=geom_4)
+        self.p6 = Path.objects.create(name='Sixth Path', geom=geom_4)
+        self.p7 = Path.objects.create(name='Seventh Path', geom=geom_4)
 
         geom_5 = LineString((0, 6), (1, 6), (2, 6))
 
@@ -49,8 +36,20 @@ class RemoveDuplicatePathTest(TestCase):
         poi3 = POIFactory.create(name='POI3', no_path=True)
         poi3.add_path(p4, start=0.5, end=0.5)
 
-        self.assertEquals(Path.objects.count(), 9)
+    def test_remove_duplicate_path(self):
+        """
+        This test check that we remove 1 of the duplicate path and keep ones with topologies.
 
+                poi3 (only on p4)
+        +-------o------+                    p5 is reversed.
+        p3/p4/p5
+
+        +-------o------+        +--------+
+        p1/p2   poi1/poi2       p6
+
+        We get at the end p1, p3, p5, p6.
+        """
         call_command('remove_duplicate_paths', verbosity=0)
+
         self.assertEquals(Path.objects.count(), 5)
-        self.assertItemsEqual((p1, p2, p4, p6, p7), Path.objects.all())
+        self.assertItemsEqual((self.p1, self.p3, self.p5, self.p6), Path.objects.all())
