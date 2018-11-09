724dbae4e6e37a4716b3f1bbc54e76fc058eafe5
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2018-11-09 10:19:00 +0000

    Change migration add permissions

--- geotrek/authent/migrations/0002_auto_20181107_1620.py
+++ geotrek/authent/migrations/0002_auto_20181107_1620.py
@@ -8,6 +8,7 @@ from django.core.management import call_command
 def add_permissions(apps, schema_editor):
     call_command('update_geotrek_permissions', verbosity=0)
     UserModel = apps.get_model('auth', 'User')
+    GroupModel = apps.get_model('auth', 'Group')
     PermissionModel = apps.get_model('auth', 'Permission')
     permissions = ['path', 'service', 'poi', 'touristicevent', 'touristiccontent', 'project',
                    'trek', 'intervention', 'signage', 'workmanagementedge',
@@ -16,8 +17,15 @@ def add_permissions(apps, schema_editor):
                    ]
     for user in UserModel.objects.all():
         for perm in permissions:
-            user.user_permissions.add(PermissionModel.objects.get(
-                codename='change_geom_%s' % perm))
+            if user.user_permissions.filter(codename='change_%s' % perm).exists():
+                user.user_permissions.add(PermissionModel.objects.get(
+                    codename='change_geom_%s' % perm))
+
+    for group in GroupModel.objects.all():
+        for perm in permissions:
+            if group.permissions.filter(codename='change_%s' % perm).exists():
+                group.permissions.add(PermissionModel.objects.get(
+                    codename='change_geom_%s' % perm))
 
 
 class Migration(migrations.Migration):
