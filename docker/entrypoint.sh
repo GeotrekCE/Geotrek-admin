bea5fa9725f12d0927e998168611fc4027cb173a
J-E Castagnede <j.e.castagnede@gmail.com>
2018-11-22 14:44:56 +0100

    revert bad change

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -11,17 +11,17 @@ mkdir -p /app/src/var/static \
          /app/src/var/conf/extra_locale
 
 # if not custom.py present, create it
-if [[ ! -f /app/src/var/conf/custom.py ]]; then
+if [ ! -f /app/src/var/conf/custom.py ]; then
     cp /app/src/geotrek/settings/custom.py.dist /app/src/var/conf/custom.py
 fi
-if [[ ! -f /app/src/geotrek/settings/custom.py ]]; then
+if [ ! -f /app/src/geotrek/settings/custom.py ]; then
     ln -s /app/src/var/conf/custom.py /app/src/geotrek/settings/custom.py
 fi
 # if not parsers.py present, create it
-if [[ ! -f /app/src/var/conf/parsers.py ]]; then
+if [ ! -f /app/src/var/conf/parsers.py ]; then
     touch /app/src/var/conf/parsers.py
 fi
-if [[ ! -f /app/src/bulkimport/parsers.py ]]; then
+if [ ! -f /app/src/bulkimport/parsers.py ]; then
     ln -s /app/src/var/conf/parsers.py /app/src/bulkimport/parsers.py
 fi
 
