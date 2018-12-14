5fc58af3ff2defad2da4d1596d2be714077771f4
J-E Castagnede <j.e.castagnede@gmail.com>
2018-12-14 19:22:36 +0100

    fix meta print

--- geotrek/common/middleware.py
+++ geotrek/common/middleware.py
@@ -72,7 +72,6 @@ except socket.error:
 
 class FixedAutoLoginMiddleware(AutoLoginMiddleware):
     def process_request(self, request):
-        print(request.META)
         if "HTTP_X_FORWARDED_FOR" in request.META:
             request.META["HTTP_X_PROXY_REMOTE_ADDR"] = request.META["REMOTE_ADDR"]
             parts = request.META["HTTP_X_FORWARDED_FOR"].split(",", 1)
