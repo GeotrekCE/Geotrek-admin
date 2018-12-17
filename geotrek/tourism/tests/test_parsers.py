ec26d21b160aded74c188efc7d726e7339ca4f9a
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2018-12-17 09:14:41 +0000

    Fix tests for translation apidae

--- geotrek/tourism/tests/test_parsers.py
+++ geotrek/tourism/tests/test_parsers.py
@@ -229,9 +229,9 @@ class ParserTests(TranslationResetMixin, TestCase):
         self.assertEqual(round(content.geom.x), 1000157)
         self.assertEqual(round(content.geom.y), 6413576)
         self.assertEqual(content.practical_info[:39], "<b>Ouverture:</b><br>Du 01/05 au 31/10.")
-        self.assertTrue("<br><b>Capacité totale:</b><br>10<br>" in content.practical_info)
-        self.assertTrue("><br><b>Services:</b><br>Test, Test2, Test3, Test4<br>" in content.practical_info)
-        self.assertIn("<b>Tarifs:</b><br>A partir de 30 € par personne<br>", content.practical_info)
+        self.assertTrue("<br><b>Capacité totale:</b><br>10<br>" in content.practical_info_fr)
+        self.assertTrue("><br><b>Services:</b><br>Test, Test2, Test3, Test4<br>" in content.practical_info_fr)
+        self.assertIn("<b>Tarifs:</b><br>A partir de 30 € par personne<br>", content.practical_info_fr)
         self.assertIn("<b>Accès:</b><br>TestFr<br>", content.practical_info)
         self.assertTrue("><br><b>Services:</b><br>Test EN, Test2 EN, Test3 EN, Test4 EN<br>"
                         in content.practical_info_en)
@@ -293,14 +293,14 @@ class ParserTests(TranslationResetMixin, TestCase):
         self.assertEqual(event.website, "http://www.LeGrandTim.fr")
         self.assertEqual(round(event.geom.x), 922920)
         self.assertEqual(round(event.geom.y), 6357103)
-        self.assertEqual(event.practical_info[:38], "<b>Ouverture:</b><br>Mardi 6 août 2019")
+        self.assertEqual(event.practical_info_fr[:38], "<b>Ouverture:</b><br>Mardi 6 août 2019")
         self.assertIn("><br><b>Services:</b><br>Le plus grand des services, Un autre grand service<br>",
-                      event.practical_info)
-        self.assertIn("<b>Ouverture:</b><br>Mardi 6 août 2019 de 9h à midi.<br>", event.practical_info)
-        self.assertIn("<b>Langues Parlées:</b><br>Français<br>", event.practical_info)
-        self.assertIn("<b>Accès:</b><br>TestFr<br>", event.practical_info)
+                      event.practical_info_fr)
+        self.assertIn("<b>Ouverture:</b><br>Mardi 6 août 2019 de 9h à midi.<br>", event.practical_info_fr)
+        self.assertIn("<b>Langues Parlées:</b><br>Français<br>", event.practical_info_fr)
+        self.assertIn("<b>Accès:</b><br>TestFr<br>", event.practical_info_fr)
         self.assertIn("><br><b>Services:</b><br>Le plus grand des services, Un autre grand service<br>",
-                      event.practical_info_en)
+                      event.practical_info_fr)
         self.assertIn("><br><b>Services:</b><br>Le plus grand des services, Un autre grand service<br>",
                       event.practical_info_fr)
         self.assertTrue(event.published)
