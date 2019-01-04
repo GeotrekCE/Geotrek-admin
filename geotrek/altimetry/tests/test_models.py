f7b875bb3d4b313e9c1e22297918d33e67633104
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2019-01-04 15:50:31 +0000

    Add test trek_dtail_pdf language none

---
+++ geotrek/altimetry/tests/test_models.py
@@ -0,0 +1,17 @@
+import os
+
+from django.test import TestCase
+from django.conf import settings
+
+from geotrek.trekking.factories import TrekFactory
+from geotrek.trekking.models import Trek
+
+
+class AltimetryMixinTest(TestCase):
+    def test_get_elevation_chart_none(self):
+        trek = TrekFactory.create(no_path=True)
+        trek.get_elevation_chart_path()
+        basefolder = os.path.join(settings.MEDIA_ROOT, 'profiles')
+        print(Trek._meta.model_name, trek.pk, 'en')
+        self.assertTrue(os.path.exists(os.path.join(basefolder,
+                                                    '%s-%s-%s.png' % (Trek._meta.model_name, '1', 'en'))))
