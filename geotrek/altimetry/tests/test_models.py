fcd523105e9f158f423018d45b05527435a41fb0
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2019-01-07 14:40:57 +0000

    Change test model elevation chart

--- geotrek/altimetry/tests/test_models.py
+++ geotrek/altimetry/tests/test_models.py
@@ -2,6 +2,7 @@ import os
 
 from django.test import TestCase
 from django.conf import settings
+from django.utils.translation import get_language
 
 from geotrek.trekking.factories import TrekFactory
 from geotrek.trekking.models import Trek
@@ -9,9 +10,12 @@ from geotrek.trekking.models import Trek
 
 class AltimetryMixinTest(TestCase):
     def test_get_elevation_chart_none(self):
-        trek = TrekFactory.create(no_path=True)
+        trek = TrekFactory.create(no_path=True, published=True)
+        response = self.client.get('/media/profiles/trek-%s.png' % trek.pk)
+        self.assertEqual(response.status_code, 200)
+        # In PDF
         trek.get_elevation_chart_path()
         basefolder = os.path.join(settings.MEDIA_ROOT, 'profiles')
         self.assertTrue(os.listdir(basefolder))
         directory = os.listdir(basefolder)
-        self.assertIn('%s-%s-%s.png' % (Trek._meta.model_name, '1', 'en'), directory)
+        self.assertIn('%s-%s-%s.png' % (Trek._meta.model_name, str(trek.pk), get_language()), directory)
