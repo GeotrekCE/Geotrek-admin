35c93923626123cbca1c74a48f48eb06e44abf11
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2019-01-14 13:51:30 +0000

    Fix sql a_v_infrastructure

--- geotrek/infrastructure/sql/00_cleanup.sql
+++ geotrek/infrastructure/sql/00_cleanup.sql
@@ -1,3 +1,4 @@
 -- Cleanup public schema if installed before release 0.28
 DROP VIEW IF EXISTS public.a_v_amenagement;
+DROP VIEW IF EXISTS public.a_v_infrastructure;
 DROP VIEW IF EXISTS public.a_v_equipement;
