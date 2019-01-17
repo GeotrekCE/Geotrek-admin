6d21f720bf396c60e2b31488932a40e647cbdde5
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2019-01-17 09:10:06 +0000

    Add blade form, formset

--- geotrek/signage/urls.py
+++ geotrek/signage/urls.py
@@ -4,10 +4,12 @@ from mapentity.registry import registry
 
 from . import models
 from geotrek.trekking.views import TrekSignageViewSet
-
+from geotrek.signage.views import BladeUpdate
 
 urlpatterns = registry.register(models.Signage)
 urlpatterns += [
     url(r'^api/(?P<lang>\w\w)/treks/(?P<pk>\d+)/signages\.geojson$',
         TrekSignageViewSet.as_view({'get': 'list'}), name="trek_signage_geojson"),
+    url(r'signage/blades/(?P<pk>\d+)/$', BladeUpdate.as_view(),
+        name="blade_edit")
 ]
