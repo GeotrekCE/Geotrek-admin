1e62f2fddf96e263198cd8612ddbb3ed2e3c0a73
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2019-02-18 10:55:04 +0000

    Add test mobile settings

---
+++ geotrek/api/tests/test_mobile_settings.py
@@ -0,0 +1,144 @@
+
+from __future__ import unicode_literals
+
+import json
+
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from django.test.testcases import TestCase
+
+from geotrek.common import factories as common_factories
+from geotrek.common.models import Theme
+from geotrek.trekking import factories as trekking_factories
+from geotrek.trekking.models import TrekNetwork, Route, Practice, Accessibility, DifficultyLevel
+from geotrek.tourism import factories as tourism_factories
+from geotrek.tourism.models import InformationDesk
+
+SETTINGS_STRUCTURE = sorted([
+    'informationdesk', 'network', 'route', 'practice', 'accessibility', 'difficulty', 'theme'
+])
+
+
+class SettingsMobileTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.administrator = User.objects.create(username="administrator", is_superuser=True,
+                                                is_staff=True, is_active=True)
+        cls.administrator.set_password('administrator')
+        cls.administrator.save()
+        cls.administrator.refresh_from_db()
+
+    def get_settings(self, params=None):
+        return self.client.get(reverse('apimobile:settings'), params, HTTP_ACCEPT_LANGUAGE='fr')
+
+    def test_settings_no_permission(self):
+        response = self.get_settings()
+        #  test response code
+        self.assertEqual(response.status_code, 401)
+
+    def test_settings_structure(self):
+        self.client.login(username="administrator", password="administrator")
+
+        response = self.get_settings()
+        #  test response code
+        self.assertEqual(response.status_code, 200)
+
+        json_response = json.loads(response.content.decode('utf-8'))
+        self.assertEqual(sorted(json_response.keys()),
+                         SETTINGS_STRUCTURE)
+
+    def test_settings_information_desk(self):
+        self.client.login(username="administrator", password="administrator")
+        informationdesk = tourism_factories.InformationDeskFactory()
+        tourism_factories.InformationDeskFactory()
+        response = self.get_settings()
+        #  test response code
+        self.assertEqual(response.status_code, 200)
+
+        json_response = json.loads(response.content.decode('utf-8'))
+
+        self.assertEqual(len(json_response.get('informationdesk')), InformationDesk.objects.count())
+        self.assertEqual(json_response.get('informationdesk')[0].get('email'), informationdesk.email)
+        self.assertEqual(json_response.get('informationdesk')[0].get('description'), informationdesk.description)
+
+    def test_settings_network(self):
+        self.client.login(username="administrator", password="administrator")
+        network = trekking_factories.TrekNetworkFactory()
+        trekking_factories.TrekNetworkFactory()
+        response = self.get_settings()
+        #  test response code
+        self.assertEqual(response.status_code, 200)
+
+        json_response = json.loads(response.content.decode('utf-8'))
+
+        self.assertEqual(len(json_response.get('network')), TrekNetwork.objects.count())
+        self.assertEqual(json_response.get('network')[0].get('label'), network.network)
+
+    def test_settings_route(self):
+        self.client.login(username="administrator", password="administrator")
+        route = trekking_factories.RouteFactory()
+        trekking_factories.RouteFactory()
+        response = self.get_settings()
+        #  test response code
+        self.assertEqual(response.status_code, 200)
+
+        json_response = json.loads(response.content.decode('utf-8'))
+
+        self.assertEqual(len(json_response.get('route')), Route.objects.count())
+        self.assertEqual(json_response.get('route')[0].get('label'), route.route)
+
+    def test_settings_practice(self):
+        self.client.login(username="administrator", password="administrator")
+        practice = trekking_factories.PracticeFactory()
+        trekking_factories.PracticeFactory()
+        response = self.get_settings()
+        #  test response code
+        self.assertEqual(response.status_code, 200)
+
+        json_response = json.loads(response.content.decode('utf-8'))
+
+        self.assertEqual(len(json_response.get('practice')), Practice.objects.count())
+        self.assertEqual(json_response.get('practice')[0].get('label'), practice.name)
+        self.assertIn(str(practice.pictogram), json_response.get('practice')[0].get('pictogram'))
+
+    def test_settings_accessibility(self):
+        self.client.login(username="administrator", password="administrator")
+        accessibility = trekking_factories.AccessibilityFactory()
+        trekking_factories.AccessibilityFactory()
+        response = self.get_settings()
+        #  test response code
+        self.assertEqual(response.status_code, 200)
+
+        json_response = json.loads(response.content.decode('utf-8'))
+
+        self.assertEqual(len(json_response.get('accessibility')), Accessibility.objects.count())
+        self.assertEqual(json_response.get('accessibility')[0].get('label'), accessibility.name)
+        self.assertIn(str(accessibility.pictogram), json_response.get('accessibility')[0].get('pictogram'))
+
+    def test_settings_difficulty(self):
+        self.client.login(username="administrator", password="administrator")
+        difficulty = trekking_factories.DifficultyLevelFactory()
+        trekking_factories.DifficultyLevelFactory()
+        response = self.get_settings()
+        #  test response code
+        self.assertEqual(response.status_code, 200)
+
+        json_response = json.loads(response.content.decode('utf-8'))
+
+        self.assertEqual(len(json_response.get('difficulty')), DifficultyLevel.objects.count())
+        self.assertEqual(json_response.get('difficulty')[0].get('label'), difficulty.difficulty)
+        self.assertIn(str(difficulty.pictogram), json_response.get('difficulty')[0].get('pictogram'))
+
+    def test_settings_theme(self):
+        self.client.login(username="administrator", password="administrator")
+        theme = common_factories.ThemeFactory()
+        common_factories.ThemeFactory()
+        response = self.get_settings()
+        #  test response code
+        self.assertEqual(response.status_code, 200)
+
+        json_response = json.loads(response.content.decode('utf-8'))
+
+        self.assertEqual(len(json_response.get('theme')), Theme.objects.count())
+        self.assertEqual(json_response.get('theme')[0].get('label'), theme.label)
+        self.assertIn(str(theme.pictogram), json_response.get('theme')[0].get('pictogram'))
