2c7a40679e6202446a2e1076e19832589abf9ef9
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2019-02-19 12:59:22 +0000

    Add test mobile flatpage

---
+++ geotrek/api/tests/test_mobile_flatpage.py
@@ -0,0 +1,51 @@
+from __future__ import unicode_literals
+
+import json
+
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from django.test.testcases import TestCase
+
+from geotrek.flatpages.factories import FlatPageFactory
+from geotrek.flatpages.models import FlatPage
+
+FLATPAGE_DETAIL_PROPERTIES_JSON_STRUCTURE = sorted([
+    'id', 'title', 'content'
+])
+
+
+class FlatPageAdministratorTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.flatpage = FlatPageFactory.create()
+        FlatPageFactory.create()
+        cls.administrator = User.objects.create(username="administrator", is_superuser=True,
+                                                is_staff=True, is_active=True)
+        cls.administrator.set_password('administrator')
+        cls.administrator.save()
+        cls.administrator.refresh_from_db()
+
+    def get_flatpage_list(self, params=None):
+        return self.client.get(reverse('apimobile:flatpage-list'), params, HTTP_ACCEPT_LANGUAGE='fr')
+
+    def get_flatpage_detail(self, id_flatpage, params=None):
+        return self.client.get(reverse('apimobile:flatpage-detail', args=(id_flatpage,)),
+                               params, HTTP_ACCEPT_LANGUAGE='fr')
+
+    def test_flatpage_list_administrator(self):
+        self.client.login(username="administrator", password="administrator")
+        response = self.get_flatpage_list()
+        self.assertEqual(response.status_code, 200)
+        json_response = json.loads(response.content.decode('utf-8'))
+        self.assertEqual(len(json_response), 2)
+        self.assertEqual(json_response[0].get('title'), FlatPage.objects.first().title)
+
+    def test_flatpage_detail_administrator(self):
+        self.client.login(username="administrator", password="administrator")
+        response = self.get_flatpage_detail(self.flatpage.pk)
+        self.assertEqual(response.status_code, 200)
+        json_response = json.loads(response.content.decode('utf-8'))
+        self.assertEqual(sorted(json_response.keys()),
+                         FLATPAGE_DETAIL_PROPERTIES_JSON_STRUCTURE)
+        self.assertEqual(json_response.get('content'), self.flatpage.content)
+        self.assertEqual(json_response.get('title'), self.flatpage.title)
