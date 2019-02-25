cae0b063b20e72360040d587a70bb2c75034af82
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2019-02-25 16:06:45 +0100

    Show only one city

--- geotrek/api/mobile/serializers/trekking.py
+++ geotrek/api/mobile/serializers/trekking.py
@@ -1,14 +1,14 @@
 from __future__ import unicode_literals
 
 from django.conf import settings
-from drf_dynamic_fields import DynamicFieldsMixin
 from rest_framework import serializers
 from rest_framework_gis import serializers as geo_serializers
 
 from geotrek.common import models as common_models
+from geotrek.zoning.models import City
 
 
-class AttachmentSerializer(DynamicFieldsMixin, serializers.ModelSerializer):
+class AttachmentSerializer(serializers.ModelSerializer):
     url = serializers.SerializerMethodField(read_only=True)
 
     def get_url(self, obj):
@@ -26,7 +26,7 @@ class AttachmentSerializer(DynamicFieldsMixin, serializers.ModelSerializer):
 if 'geotrek.trekking' in settings.INSTALLED_APPS:
     from geotrek.trekking import models as trekking_models
 
-    class POIListSerializer(DynamicFieldsMixin, serializers.ModelSerializer):
+    class POIListSerializer(serializers.ModelSerializer):
         pictures = AttachmentSerializer(many=True, )
         thumbnail = serializers.ReadOnlyField(source='serializable_thumbnail_mobile')
         geometry = geo_serializers.GeometryField(read_only=True, precision=7, source='geom2d_transformed')
@@ -37,7 +37,7 @@ if 'geotrek.trekking' in settings.INSTALLED_APPS:
                 'id', 'pictures', 'name', 'description', 'thumbnail', 'type', 'geometry',
             )
 
-    class TrekDetailSerializer(DynamicFieldsMixin, serializers.ModelSerializer):
+    class TrekDetailSerializer(serializers.ModelSerializer):
         thumbnail = serializers.ReadOnlyField(source='serializable_thumbnail_mobile')
 
         geometry = geo_serializers.GeometryField(read_only=True, precision=7, source='geom2d_transformed')
@@ -65,14 +65,23 @@ if 'geotrek.trekking' in settings.INSTALLED_APPS:
             )
             auto_bbox = True
 
-    class TrekListSerializer(DynamicFieldsMixin, serializers.ModelSerializer):
+    class TrekListSerializer(serializers.ModelSerializer):
         thumbnail = serializers.ReadOnlyField(source='serializable_thumbnail_mobile')
         length = serializers.SerializerMethodField(read_only=True)
         geometry = geo_serializers.GeometryField(read_only=True, precision=7, source='start_point', )
         cities = serializers.SerializerMethodField(read_only=True)
 
         def get_cities(self, obj):
-            return [city.code for city in obj.cities]
+            qs = City.objects
+            if hasattr(qs, 'existing'):
+                qs = qs.existing()
+            if obj.start_point:
+                city = qs.filter(geom__covers=(obj.start_point, 0)).first()
+            else:
+                return None
+            if city:
+                return city.code
+            return None
 
         def get_length(self, obj):
             return round(obj.length_2d_m, 1)
