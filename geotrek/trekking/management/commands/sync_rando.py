91bba084eacef55c7d9c008987feb6776f98a5f4
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2019-03-07 15:03:55 +0000

    Remove possibility of same directory for mobile and rando

--- geotrek/trekking/management/commands/sync_rando.py
+++ geotrek/trekking/management/commands/sync_rando.py
@@ -740,7 +740,7 @@ class Command(BaseCommand):
         if not os.path.exists(self.dst_root):
             return
         existing = set([os.path.basename(p) for p in os.listdir(self.dst_root)])
-        remaining = existing - set(('api', 'media', 'meta', 'static', 'zip', 'mobile'))
+        remaining = existing - set(('api', 'media', 'meta', 'static', 'zip'))
         if remaining:
             raise CommandError(u"Destination directory contains extra data")
 
