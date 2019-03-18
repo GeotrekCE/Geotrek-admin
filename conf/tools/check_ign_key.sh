e56ba4b219c4dc14ed43f7bae91ad5551ad261c9
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2019-03-18 13:19:21 +0000

    Add without gpp3

--- conf/tools/check_ign_key.sh
+++ conf/tools/check_ign_key.sh
@@ -7,12 +7,16 @@ layers_png="CADASTRALPARCELS.PARCELS"
 
 for layer in $layers_jpeg; do
     echo '------------------------------------------------------------'
-    echo $layer
-    curl -s -I --header "Referer: $referer" "http://gpp3-wxs.ign.fr/$key/geoportail/wmts?LAYER=$layer&EXCEPTIONS=text/xml&FORMAT=image/jpeg&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX=12&TILEROW=1478&TILECOL=2112" | head -n 1
+    echo $layer '(gpp3)'
+    curl -s -I --header "Referer: $referer" "gpp3-wxs.ign.fr/$key/geoportail/wmts?LAYER=$layer&EXCEPTIONS=text/xml&FORMAT=image/jpeg&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX=12&TILEROW=1478&TILECOL=2112" | head -n 1
+    echo $layer '(without gpp3)'
+    curl -s -I --header "Referer: $referer" "wxs.ign.fr/$key/wmts?LAYER=$layer&EXCEPTIONS=text/xml&FORMAT=image/jpeg&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX=12&TILEROW=1478&TILECOL=2112" | head -n 1
 done;
 
 for layer in $layers_png; do
     echo '------------------------------------------------------------'
-    echo $layer
-    curl -s -I --header "Referer: $referer" "http://gpp3-wxs.ign.fr/$key/geoportail/wmts?LAYER=$layer&EXCEPTIONS=text/xml&FORMAT=image/png&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX=12&TILEROW=1478&TILECOL=2112" | head -n 1
+    echo $layer '(gpp3)'
+    curl -s -I --header "Referer: $referer" "gpp3-wxs.ign.fr/$key/geoportail/wmts?LAYER=$layer&EXCEPTIONS=text/xml&FORMAT=image/png&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX=12&TILEROW=1478&TILECOL=2112" | head -n 1
+    echo $layer '(without gpp3)'
+    curl -s -I --header "Referer: $referer" "wxs.ign.fr/$key/wmts?LAYER=$layer&EXCEPTIONS=text/xml&FORMAT=image/png&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX=12&TILEROW=1478&TILECOL=2112" | head -n 1
 done;
