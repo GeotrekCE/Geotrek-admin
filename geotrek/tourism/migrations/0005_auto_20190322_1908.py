4cd75fafbad79239af7e1f25958f6954f47233f7
Gaël UTARD <gael.utard@makina-corpus.com>
2019-03-29 17:25:38 +0100

    Fix tourism migrations

--- geotrek/tourism/migrations/0005_auto_20190322_1908.py
+++ geotrek/tourism/migrations/0005_auto_20190322_1908.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('tourism', '0003_auto_20190306_1417'),
+        ('tourism', '0004_auto_20190328_1339'),
     ]
 
     operations = [
