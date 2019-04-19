9f8449b185423f8f4c1f9cbb71870a531f3c1dfc
Gaël UTARD <gael.utard@makina-corpus.com>
2019-04-19 15:24:51 +0200

    Remove gpp3 from check_ign_key script

--- conf/tools/check_ign_key.sh
+++ conf/tools/check_ign_key.sh
@@ -2,21 +2,27 @@
 
 referer=$1
 key=$2
+
+if test -z "$referer" -o -z "$key"; then
+    echo Usage: $0 REFERER KEY
+    exit 1
+fi
+
 layers_jpeg="GEOGRAPHICALGRIDSYSTEMS.MAPS GEOGRAPHICALGRIDSYSTEMS.MAPS.SCAN-EXPRESS.CLASSIQUE GEOGRAPHICALGRIDSYSTEMS.MAPS.SCAN-EXPRESS.STANDARD ORTHOIMAGERY.ORTHOPHOTOS"
 layers_png="CADASTRALPARCELS.PARCELS"
 
+echo '------------------------------------------------------------'
+echo GetCapabilities
+curl -s -I --header "Referer: $referer" "http://wxs.ign.fr/$key/wmts?SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetCapabilities" | head -n 1
+
 for layer in $layers_jpeg; do
     echo '------------------------------------------------------------'
-    echo $layer '(gpp3)'
-    curl -s -I --header "Referer: $referer" "https://gpp3-wxs.ign.fr/$key/geoportail/wmts?LAYER=$layer&EXCEPTIONS=text/xml&FORMAT=image/jpeg&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX=12&TILEROW=1478&TILECOL=2112" | head -n 1
-    echo $layer '(without gpp3)'
+    echo $layer
     curl -s -I --header "Referer: $referer" "https://wxs.ign.fr/$key/wmts?LAYER=$layer&EXCEPTIONS=text/xml&FORMAT=image/jpeg&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX=12&TILEROW=1478&TILECOL=2112" | head -n 1
 done;
 
 for layer in $layers_png; do
     echo '------------------------------------------------------------'
-    echo $layer '(gpp3)'
-    curl -s -I --header "Referer: $referer" "https://gpp3-wxs.ign.fr/$key/geoportail/wmts?LAYER=$layer&EXCEPTIONS=text/xml&FORMAT=image/png&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX=12&TILEROW=1478&TILECOL=2112" | head -n 1
-    echo $layer '(without gpp3)'
+    echo $layer
     curl -s -I --header "Referer: $referer" "https://wxs.ign.fr/$key/wmts?LAYER=$layer&EXCEPTIONS=text/xml&FORMAT=image/png&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX=12&TILEROW=1478&TILECOL=2112" | head -n 1
 done;
