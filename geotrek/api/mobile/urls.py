1e04152b69f88f6512920db8ccdd9ba2f0201517
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2019-05-06 12:11:00 +0200

    Fix api mobile only with geotrek flatpages trekking tourism

--- geotrek/api/mobile/urls.py
+++ geotrek/api/mobile/urls.py
@@ -5,16 +5,13 @@ from django.conf.urls import url, include
 from rest_framework import routers
 
 
-if 'geotrek.flatpages' and 'geotrek.trekking' and 'geotrek.tourism':
+if 'geotrek.flatpages' and 'geotrek.trekking' and 'geotrek.tourism' in settings.INSTALLED_APPS:
     from geotrek.api.mobile import views as api_mobile
     router = routers.DefaultRouter()
+    router.register(r'flatpages', api_mobile.FlatPageViewSet, base_name='flatpage')
+    router.register(r'treks', api_mobile.TrekViewSet, base_name='treks')
     urlpatterns = [
         url(r'^$', api_mobile.SwaggerSchemaView.as_view(), name="schema"),
         url(r'^', include(router.urls)),
     ]
-    urlpatterns += url(r'^settings/$', api_mobile.SettingsView.as_view(), name='settings'),
-
-if 'geotrek.flatpages' in settings.INSTALLED_APPS:
-    router.register(r'flatpages', api_mobile.FlatPageViewSet, base_name='flatpage')
-if 'geotrek.trekking' in settings.INSTALLED_APPS:
-    router.register(r'treks', api_mobile.TrekViewSet, base_name='treks')
+    urlpatterns += url(r'^settings/$', api_mobile.SettingsView.as_view(), name='settings')
