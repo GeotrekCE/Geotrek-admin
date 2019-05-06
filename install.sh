fde8e731e93ff0a4ace480072bdb603bc6ec3abd
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2019-05-06 14:04:00 +0200

    Preparing release 2.27.1

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ cd "$(dirname "$0")"
 
 #------------------------------------------------------------------------------
 
-STABLE_VERSION=${STABLE_VERSION:-2.27.0}
+STABLE_VERSION=${STABLE_VERSION:-2.27.1}
 dev=false
 tests=false
 prod=false
