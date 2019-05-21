ba40a68641a4f5ad68a1603be95a11bca2c71eff
Gaël UTARD <gael.utard@makina-corpus.com>
2019-05-21 14:48:59 +0200

    Add default values for POSTGRES_HOST and PGPORT

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 cd /app/src
 
 mkdir -p /app/src/var/static \
@@ -30,7 +31,7 @@ fi
 chown django:django -R ./var
 
 # wait for postgres
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -p "$PGPORT" -d "$POSTGRES_DB" -c '\q'; do
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "${POSTGRES_HOST:-postgres}" -U "$POSTGRES_USER" -p "${PGPORT:-5432}" -d "$POSTGRES_DB" -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
