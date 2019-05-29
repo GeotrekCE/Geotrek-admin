809445a933478bb80d116e7e18bb078eabe1cea6
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2019-05-29 14:02:45 +0200

    Add test tasks exceptions

--- geotrek/common/tests/test_tasks.py
+++ geotrek/common/tests/test_tasks.py
@@ -1,19 +1,18 @@
 # -*- encoding: utf-8 -*-
 
 from django.test import TestCase
-from geotrek.common.tasks import import_datas
-from geotrek.common.models import FileType
+from geotrek.common.tasks import import_datas, import_datas_from_web
+from geotrek.common.models import Organism
+from geotrek.common.parsers import ExcelParser, GlobalImportError
+
+
+class OrganismParser(ExcelParser):
+    model = Organism
+    fields = {'organism': 'nOm'}
 
 
 class TasksTest(TestCase):
-    def setUp(self):
-        self.filetype = FileType.objects.create(type=u"Photographie")
-
-    def test_import_exceptions(self):
-        self.assertRaises(
-            ImportError, import_datas, filename='bombadil', name='haricot', module='toto')
-
-    def test_import_message_exception(self):
+    def test_import_datas_message_exception(self):
         self.assertRaisesMessage(
             ImportError,
             "Failed to import parser class 'haricot' from module 'toto'",
@@ -21,4 +20,33 @@ class TasksTest(TestCase):
             filename='bombadil',
             name='haricot',
             module='toto'
+        )
+
+    def test_import_datas_from_web_message_exception(self):
+        self.assertRaisesMessage(
+            ImportError,
+            "Failed to import parser class 'haricot' from module 'toto'",
+            import_datas_from_web,
+            filename='bombadil',
+            name='haricot',
+            module='toto'
+        )
+
+    def test_import_datas_from_web_other_exception(self):
+        self.assertRaisesMessage(
+            GlobalImportError,
+            'Filename is required',
+            import_datas_from_web,
+            name='OrganismParser',
+            module='geotrek.common.tests.test_tasks'
+        )
+
+    def test_import_datas_other_exception(self):
+        self.assertRaisesMessage(
+            GlobalImportError,
+            'File does not exists at: bombadil',
+            import_datas,
+            filename='bombadil',
+            name='OrganismParser',
+            module='geotrek.common.tests.test_tasks'
         )
