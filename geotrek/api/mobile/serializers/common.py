1b63d9c0a2ad6a385fc8a2e34d1b70345860e9b0
Emmanuelle Helly <emmanuelle.helly@makina-corpus.com>
2019-06-26 09:05:05 +0000

    Change mobile_color field name to bg_color

--- geotrek/api/mobile/serializers/common.py
+++ geotrek/api/mobile/serializers/common.py
@@ -102,6 +102,7 @@ if 'geotrek.trekking' in settings.INSTALLED_APPS:
 
     class PracticeSerializer(serializers.ModelSerializer):
         pictogram = serializers.SerializerMethodField(read_only=True)
+        color = serializers.ReadOnlyField(source='bg_color')
 
         def get_pictogram(self, obj):
             file_name, file_extension = os.path.splitext(str(obj.pictogram.url))
