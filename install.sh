fc5625694a7a32e5a5bd558a4cfe1fc597a1a74c
Gaël UTARD <gael.utard@makina-corpus.com>
2019-07-01 18:48:40 +0200

    Preparing release 2.27.9

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ cd "$(dirname "$0")"
 
 #------------------------------------------------------------------------------
 
-STABLE_VERSION=${STABLE_VERSION:-2.27.8}
+STABLE_VERSION=${STABLE_VERSION:-2.27.9}
 dev=false
 tests=false
 prod=false
