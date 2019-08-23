69c4f5e439e2894777bf1915cfa7abea25197a02
Gaël UTARD <gael.utard@makina-corpus.com>
2019-08-23 11:47:32 +0200

    Debug jstests

--- geotrek/jstests/nav-auth.js
+++ geotrek/jstests/nav-auth.js
@@ -6,23 +6,35 @@ casper.test.begin('Login from home page', function(test) {
 
     casper.start(utils.baseurl + '/', function () {
         test.assertUrlMatch('/login/?next=/', 'Redirects to login page.');
+        casper.echo('#4');
         test.assertExists('form', 'Form present.');
+        casper.echo('#5');
     });
+    casper.echo('#1');
 
     casper.then(function () {
+        casper.echo('#6');
         casper.fill('form', {
             'username':    'admin',
             'password':    'admin',
         }, true);
-        casper.click("button[type='submit']");
+        casper.echo('#7');
+        casper.click('button[type="submit"]');
+        casper.echo('#8');
     });
+    casper.echo('#2');
 
     casper.then(function () {
+        casper.echo('#9');
         test.assertUrlMatch('/', 'Logged-in and redirected.');
+        casper.echo('#10');
         utils.saveCookies();
+        casper.echo('#11');
     });
+    casper.echo('#3');
 
     casper.run(function done() {
+        casper.echo('#12');
         test.done();
     });
 });
