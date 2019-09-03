eb3f32d28cdbcb6281c1f078291f44dd9922f0c9
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2019-09-03 15:10:16 +0200

    Add test sync_mobile states without celery

--- geotrek/api/tests/test_views.py
+++ geotrek/api/tests/test_views.py
@@ -21,3 +21,30 @@ class SyncMobileViewTest(TestCase):
         self.client.login(username='homer', password='doooh')
         response = self.client.get(reverse('apimobile:sync_mobiles_view'))
         self.assertRedirects(response, '/login/?next=/api/mobile/commands/syncview')
+
+    def test_post_sync_mobile_superuser(self):
+        """
+        test if sync can be launched by superuser post
+        """
+        self.client.login(username='admin', password='super')
+        response = self.client.post(reverse('apimobile:sync_mobiles'), data={})
+        self.assertRedirects(response, '/api/mobile/commands/syncview')
+
+    def test_post_sync_mobile_simpleuser(self):
+        """
+        test if sync can be launched by simple user post
+        """
+        self.client.login(username='homer', password='doooh')
+        response = self.client.post(reverse('apimobile:sync_mobiles'), data={})
+        self.assertRedirects(response, '/login/?next=/api/mobile/commands/sync')
+
+    def test_get_sync_mobile_states_superuser(self):
+        self.client.login(username='admin', password='super')
+        response = self.client.post(reverse('apimobile:sync_mobiles_state'), data={})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, '[]')
+
+    def test_get_sync_mobile_states_simpleuser(self):
+        self.client.login(username='homer', password='doooh')
+        response = self.client.post(reverse('apimobile:sync_mobiles_state'), data={})
+        self.assertRedirects(response, '/login/?next=/api/mobile/commands/statesync/')
