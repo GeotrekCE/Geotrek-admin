446412dd174be07d000a17cce5fb81bf41315f4d
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2019-09-19 11:58:28 +0200

    Preparing release 2.29.6

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ cd "$(dirname "$0")"
 
 #------------------------------------------------------------------------------
 
-STABLE_VERSION=${STABLE_VERSION:-2.29.5}
+STABLE_VERSION=${STABLE_VERSION:-2.29.6}
 dev=false
 tests=false
 prod=false
