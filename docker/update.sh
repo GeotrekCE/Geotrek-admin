9a2ed9928c677a815a3aa219dd7f5a47f089ef46
Gaël UTARD <gael.utard@makina-corpus.com>
2019-12-10 13:46:31 +0100

    Compile only extra locales in entrypoint.
    Other locales are compiled in docker image

--- docker/update.sh
+++ docker/update.sh
@@ -3,7 +3,7 @@
 cd /app/src
 
 ./manage.py migrate --noinput
-./manage.py compilemessages
+pushd var/conf/extra_locale; ../../../manage.py compilemessages; popd
 ./manage.py collectstatic --clear --noinput --verbosity=0
 ./manage.py sync_translation_fields --noinput
 ./manage.py update_translation_fields
