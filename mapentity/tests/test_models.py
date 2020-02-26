f310cb74f844423fb691a3ce953e2b84866df68a
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2020-02-26 18:44:09 +0100

    Add logentry test model

--- mapentity/tests/test_models.py
+++ mapentity/tests/test_models.py
@@ -1,6 +1,11 @@
 from django.conf import settings
 from django.test import TestCase
+from django.utils import timezone
+from django.utils.encoding import force_text
+from django.utils.formats import localize
 
+from mapentity.models import LogEntry, ADDITION
+from mapentity.factories import UserFactory
 from geotrek.core.factories import PathFactory
 
 import os
@@ -12,3 +17,19 @@ class ModelsTest(TestCase):
         with open(os.path.join(settings.MEDIA_ROOT, 'maps', 'path-%s.png' % path.pk), mode='w') as f:
             path.delete()
         self.assertFalse(os.path.exists(os.path.join(settings.MEDIA_ROOT, 'maps', 'path-%s.png' % path.pk)))
+
+
+class LogEntryTest(TestCase):
+    def test_logentry(self):
+        user = UserFactory.create()
+        object = PathFactory.create()
+        logentry = LogEntry.objects.log_action(
+            user_id=user.pk,
+            content_type_id=object.get_content_type_id(),
+            object_id=object.pk,
+            object_repr=force_text(object),
+            action_flag=ADDITION
+        )
+        self.assertEqual('Added', logentry.action_flag_display)
+        self.assertIn('{0}'.format(localize(logentry.action_time)), logentry.action_time_display)
+        self.assertIn(object.name, logentry.object_display)
