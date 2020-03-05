85d40741eaa222e257eac8bac7348ccc96106468
Gaël UTARD <gael.utard@makina-corpus.com>
2020-03-05 11:04:14 +0100

    Release 2.32.8

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ cd "$(dirname "$0")"
 
 #------------------------------------------------------------------------------
 
-STABLE_VERSION=${STABLE_VERSION:-2.32.7}
+STABLE_VERSION=${STABLE_VERSION:-2.32.8}
 dev=false
 tests=false
 prod=false
