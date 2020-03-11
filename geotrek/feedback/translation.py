113cc2d99637d4f5bc5b7dc29a46af4945d36f07
Emmanuelle Helly <emmanuelle.helly@makina-corpus.com>
2020-03-11 16:56:14 +0100

    Add models for problem magnitude and activity involved
    And add translatable fields for both.

--- geotrek/feedback/translation.py
+++ geotrek/feedback/translation.py
@@ -7,4 +7,14 @@ class ReportCategoryTO(TranslationOptions):
     fields = ('category',)
 
 
+class ReportActivityTO(TranslationOptions):
+    fields = ('activity',)
+
+
+class ReportProblemMagnitudeTO(TranslationOptions):
+    fields = ('magnitude',)
+
+
 translator.register(feedback_models.ReportCategory, ReportCategoryTO)
+translator.register(feedback_models.ReportActivity, ReportActivityTO)
+translator.register(feedback_models.ReportProblemMagnitude, ReportProblemMagnitudeTO)
