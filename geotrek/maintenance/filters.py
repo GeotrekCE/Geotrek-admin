9ca1009307e8196a6df94eadfe1ecde514df95dd
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2020-03-20 15:57:29 +0100

    Remove useless import

--- geotrek/maintenance/filters.py
+++ geotrek/maintenance/filters.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.contrib.contenttypes.models import ContentType
 from django.utils.translation import ugettext_lazy as _
 from django_filters import ChoiceFilter
 
