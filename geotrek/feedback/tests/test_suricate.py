113e8a4029f419f20b6337c006001e9a29992781
Emmanuelle Helly <emmanuelle.helly@makina-corpus.com>
2020-03-25 11:45:15 +0100

    Raise exception if status code on suricate is not 200 or 201

--- geotrek/feedback/tests/test_suricate.py
+++ geotrek/feedback/tests/test_suricate.py
@@ -14,8 +14,8 @@ class SuricateAPITest(TestCase):
     @mock.patch('geotrek.feedback.models.post_report_to_suricate')
     def test_save_report_post_to_suricate(self, mock_post_report_to_suricate):
         """Test post to suricate on save Report
-Helper `post_report_to_suricate` function is mock
-"""
+        Helper `post_report_to_suricate` function is mock
+        """
         # Create a report
         report = ReportFactory()
 
@@ -25,8 +25,8 @@ Helper `post_report_to_suricate` function is mock
     @mock.patch('geotrek.feedback.helpers.requests.post')
     def test_post_request_to_suricate(self, mock_post):
         """Test post request itself
-Request post is mock
-"""
+        Request post is mock
+        """
         # Create a report without saving it
         report = ReportFactory.build()
 
@@ -47,3 +47,27 @@ Request post is mock
         # Call the function with the report
         result = post_report_to_suricate(report)
         self.assertEqual(result, None)
+
+    @override_settings(SEND_REPORT_TO_SURICATE=True)
+    @mock.patch('geotrek.feedback.helpers.requests.post')
+    def test_post_request_to_suricate_fails(self, mock_post):
+        """Test post request itself but fails
+        Request post is mock
+        """
+        # Define a mock response
+        mock_response = mock.Mock()
+        expected_dict = {
+            "code_ok": "false",
+            "error": {
+                "code": "400",
+                "message": "Erreur inconnue."
+            }
+        }
+        mock_response.json.return_value = expected_dict
+        mock_response.status_code = 400
+
+        # Define response for the fake API
+        mock_post.return_value = mock_response
+
+        # Create a report, should raise an excemption
+        self.assertRaises(Exception, ReportFactory())
