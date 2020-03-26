0c86b23627149b4ceed0b34956a27f67a66f0014
Emmanuelle Helly <emmanuelle.helly@makina-corpus.com>
2020-03-26 16:22:30 +0100

    Remove unused suricate report settings

--- geotrek/feedback/tests/test_suricate.py
+++ geotrek/feedback/tests/test_suricate.py
@@ -6,11 +6,20 @@ from django.test.utils import override_settings
 from geotrek.feedback.factories import ReportFactory
 from geotrek.feedback.helpers import post_report_to_suricate
 
+SURICATE_REPORT_SETTINGS = {
+    'URL': 'http://suricate.example.com',
+    'ID_ORIGIN': 'geotrek',
+    'PRIVATE_KEY_CLIENT_SERVER': '',
+    'PRIVATE_KEY_SERVER_CLIENT': '',
+    'ID_USER': 'XXX123',
+}
+
 
 class SuricateAPITest(TestCase):
     """Test Suricate API"""
 
     @override_settings(SURICATE_REPORT_ENABLED=True)
+    @override_settings(SURICATE_REPORT_SETTINGS=SURICATE_REPORT_SETTINGS)
     @mock.patch('geotrek.feedback.models.post_report_to_suricate')
     def test_save_report_post_to_suricate(self, mock_post_report_to_suricate):
         """Test post to suricate on save Report
@@ -49,6 +58,7 @@ class SuricateAPITest(TestCase):
         self.assertEqual(result, None)
 
     @override_settings(SURICATE_REPORT_ENABLED=True)
+    @override_settings(SURICATE_REPORT_SETTINGS=SURICATE_REPORT_SETTINGS)
     @mock.patch('geotrek.feedback.helpers.requests.post')
     def test_post_request_to_suricate_fails(self, mock_post):
         """Test post request itself but fails
