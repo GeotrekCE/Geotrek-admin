d59f9bcdb3ade3e55d4253f07b3fb70dfcddfa56
Gaël UTARD <gael.utard@makina-corpus.com>
2020-04-04 14:44:43 +0200

    Show an explicit error message if the geotrek command is not run as root

--- manage.py
+++ manage.py
@@ -10,6 +10,9 @@ if __name__ == "__main__":
     from django.core.management import execute_from_command_line
 
     if sys.argv[0].endswith('geotrek'):
+        if os.getuid() != 0:
+            print("ERROR! This command should be run as root")
+            sys.exit(1)
         user = pwd.getpwnam('geotrek')
         os.setgid(user.pw_gid)
         os.setuid(user.pw_uid)
