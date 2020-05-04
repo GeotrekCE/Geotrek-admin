7d07175f531634fd641d471ea1f83a7a9724cd92
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2020-05-04 15:20:07 +0200

    Fix migrations with same number

--- geotrek/signage/migrations/0015_auto_20200429_1208.py
+++ geotrek/signage/migrations/0015_auto_20200429_1208.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('signage', '0013_auto_20200423_1255'),
+        ('signage', '0014_auto_20200428_1459'),
     ]
 
     operations = [
