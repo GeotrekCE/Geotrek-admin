2ed5c550a72bef41cb4e7a5454ed9fda6897e589
Emmanuelle Helly <emmanuelle.helly@makina-corpus.com>
2020-05-28 14:49:18 +0200

    Update column liste

--- geotrek/feedback/views.py
+++ geotrek/feedback/views.py
@@ -27,8 +27,8 @@ class ReportList(mapentity_views.MapEntityList):
     model = feedback_models.Report
     filterform = ReportFilterSet
     columns = [
-        'id', 'email', 'category',
-        'problem_magnitude', 'status', 'date_update',
+        'id', 'email', 'activity', 'category',
+        'status', 'date_update',
     ]
 
 
