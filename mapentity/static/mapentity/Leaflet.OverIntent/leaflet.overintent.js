be39c23a37fc82782081ab48ece5699f7ad335b7
Gaël UTARD <gael.utard@makina-corpus.com>
2020-07-03 17:06:58 +0200

    Upgrade Leaflet.OverIntent to 1.0.0

--- mapentity/static/mapentity/Leaflet.OverIntent/leaflet.overintent.js
+++ mapentity/static/mapentity/Leaflet.OverIntent/leaflet.overintent.js
@@ -1,24 +1,47 @@
-L.OverIntentInitHook = function () {
-    var duration = 500,
-        timer = null;
+(function (factory, window) {
+  if (typeof define === 'function' && define.amd) {
+    define(['leaflet'], function (L) {
+      factory(L);
+    });
+  } else if (typeof module === 'object' && module.exports) {
+    module.exports = function (L) {
+      if (L === undefined && typeof window !== 'undefined') {
+        L = require('leaflet');
+      }
+      factory(L);
+      return L;
+    };
+  } else if (typeof window !== 'undefined' && window.L) {
+    factory(window.L);
+  }
+}(function overIntentFactory(L) {
+  L.OverIntentInitHook = function () {
+    var timer = null;
 
     this.on('mouseover', function (e) {
-        if (timer !== null) return;
+      var duration;
+      if (timer !== null) return;
 
-        timer = setTimeout(L.Util.bind(function () {
-            this.fire('mouseintent', {latlng: e.latlng, layer: e.layer});
-            timer = null;
-        }, this), duration);
+      duration = this.options.intentDuration || 300;
+
+      timer = setTimeout(L.Util.bind(function () {
+        this.fire('mouseintent', {
+          latlng: e.latlng,
+          layer: e.layer
+        });
+        timer = null;
+      }, this), duration);
     });
 
     this.on('mouseout', function (e) {
-        if (timer !== null) {
-            clearTimeout(timer);
-            timer = null;
-        }
+      if (timer !== null) {
+        clearTimeout(timer);
+        timer = null;
+      }
     });
-};
+  };
 
-L.Marker.addInitHook(L.OverIntentInitHook);
-L.Path.addInitHook(L.OverIntentInitHook);
-L.FeatureGroup.addInitHook(L.OverIntentInitHook);
+  L.Marker.addInitHook(L.OverIntentInitHook);
+  L.Path.addInitHook(L.OverIntentInitHook);
+  L.FeatureGroup.addInitHook(L.OverIntentInitHook);
+}, window));
