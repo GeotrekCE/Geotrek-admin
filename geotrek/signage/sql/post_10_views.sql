7237c46e845cb93922008203689f49ed771d5e69
Gaël UTARD <gael.utard@makina-corpus.com>
2020-08-19 16:24:13 +0200

    Make sure each postgis view has an id field
    This is required by QGis

--- geotrek/signage/sql/post_10_views.sql
+++ geotrek/signage/sql/post_10_views.sql
@@ -1,5 +1,5 @@
 CREATE VIEW {# geotrek.signage #}.v_signages AS (
-	SELECT e.geom, t.*
+	SELECT e.geom, e.id, t.*
 	FROM signage_signage AS t, signage_signagetype AS b, core_topology AS e
 	WHERE t.topo_object_id = e.id AND t.type_id = b.id
 	AND e.deleted = FALSE
