632a38cda52c0ea3d9ce1975be187b1c23db111f
Gaël UTARD <gael.utard@makina-corpus.com>
2020-08-21 13:30:19 +0200

    Really add an id field to each SQL view

--- geotrek/core/sql/post_60_views.sql
+++ geotrek/core/sql/post_60_views.sql
@@ -1,5 +1,5 @@
 CREATE VIEW {# geotrek.core #}.v_trails AS (
-    SELECT core_topology.geom, core_trail.*
+    SELECT core_topology.geom, core_topology.id, core_trail.*
     FROM core_trail, core_topology
     WHERE core_trail.topo_object_id = core_topology.id
     AND core_topology.deleted = FALSE
