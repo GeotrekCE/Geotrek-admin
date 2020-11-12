00dc9e8887d5476aad587f8c77e3a6a89ddab8c4
Emmanuelle Helly <emmanuelle.helly@makina-corpus.com>
2020-11-12 14:30:46 +0100

    Add rdt theme in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,4 @@
+import sphinx_rtd_theme
 # Geotrek documentation build configuration file, created by
 # sphinx-quickstart on Wed May 15 09:50:19 2013.
 #
@@ -21,7 +22,10 @@
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.todo']
+extensions = [
+    'sphinx.ext.todo',
+    'sphinx_rtd_theme',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -87,7 +91,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
