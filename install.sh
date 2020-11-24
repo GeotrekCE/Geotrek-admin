14e8bf091e133c60e3a918972f0ed8b12491ba1a
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2020-11-24 18:25:02 +0100

    Add install lsb release

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-if lsb_release -d | grep 'Ubuntu 18.04' > /dev/null; then
-	echo "Found Ubuntu 18.04"
+if lsb_release -d | grep 'Ubuntu 18.04' || lsb_release -d | grep 'Ubuntu 20.04' > /dev/null; then
+	echo "Either, Ubuntu 18.04, 20.04 found"
 else
-	echo "ERROR ! Ubuntu 18.04 not found."
+	echo "ERROR ! Neither Ubuntu 18.04, Ubuntu 20.04 found."
 	exit 1
 fi
 
@@ -20,7 +20,7 @@ fi
 
 sudo apt update
 sudo apt install -y $postgis wget software-properties-common
-echo "deb https://packages.geotrek.fr/ubuntu bionic main" | sudo tee /etc/apt/sources.list.d/geotrek.list
+echo "deb https://packages.geotrek.fr/ubuntu $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/geotrek.list
 wget -O- "https://packages.geotrek.fr/geotrek.gpg.key" | sudo apt-key add -
 sudo apt update
 sudo apt install -y geotrek-admin
