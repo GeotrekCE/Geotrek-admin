0c6e2281f802344238a70c2c403b346b704ad6c7
Gaël UTARD <gael.utard@makina-corpus.com>
2020-11-30 11:55:58 +0100

    Add Site model

--- geotrek/outdoor/models.py
+++ geotrek/outdoor/models.py
@@ -1,3 +1,22 @@
-from django.db import models
+from django.conf import settings
+from django.contrib.gis.db import models
+from django.utils.translation import gettext_lazy as _
+from geotrek.authent.models import StructureRelated
+from geotrek.common.mixins import NoDeleteMixin, TimeStampedModelMixin, AddPropertyMixin
+from mapentity.models import MapEntityMixin
 
-# Create your models here.
+
+class Site(AddPropertyMixin, MapEntityMixin, StructureRelated,
+           TimeStampedModelMixin, NoDeleteMixin):
+    geom = models.GeometryField(verbose_name=_("Location"), srid=settings.SRID)
+    name = models.CharField(verbose_name=_("Name"), max_length=128)
+    description = models.TextField(verbose_name=_("Description"), blank=True)
+    eid = models.CharField(verbose_name=_("External id"), max_length=1024, blank=True, null=True)
+
+    class Meta:
+        verbose_name = _("Site")
+        verbose_name_plural = _("Sites")
+        ordering = ('name', )
+
+    def __str__(self):
+        return self.name
