1cb9bea75633c63060cc700b320a997ec38ab38b
Gaël UTARD <gael.utard@makina-corpus.com>
2020-12-04 10:22:09 +0100

    Common tests for outdoor

---
+++ geotrek/outdoor/tests/test_functional.py
@@ -0,0 +1,30 @@
+from geotrek.common.tests import CommonTest
+from geotrek.outdoor.models import Site
+from geotrek.outdoor.factories import SiteFactory, OutdoorManagerFactory
+from geotrek.authent.factories import StructureFactory
+from django.utils.translation import gettext as _
+
+
+class SiteViewsTests(CommonTest):
+    model = Site
+    modelfactory = SiteFactory
+    userfactory = OutdoorManagerFactory
+    expected_json_geom = {'type': 'Point', 'coordinates': [-1.3630812, -5.9838563]}
+
+    def get_expected_json_attrs(self):
+        return {
+            'name': 'Site',
+            'description': 'Blah',
+        }
+
+    def get_bad_data(self):
+        return {
+            'geom': 'doh!'
+        }, _('Invalid geometry value.')
+
+    def get_good_data(self):
+        return {
+            'structure': StructureFactory.create().pk,
+            'name': 'test',
+            'geom': '{"type": "Point", "coordinates":[0, 0]}',
+        }
