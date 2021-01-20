64a1723acb7297b7874802702f9e7d9a520813d3
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2021-01-20 16:50:57 +0100

    Change sql zoning

--- geotrek/zoning/sql/post_20_views.sql
+++ geotrek/zoning/sql/post_20_views.sql
@@ -1,20 +1,18 @@
 CREATE VIEW {# geotrek.zoning #}.v_cities AS (
 	SELECT e.*, l.code, l.name
-	FROM core_topology AS e, zoning_cityedge AS f, zoning_city AS l
-	WHERE f.topo_object_id = e.id AND f.city_id = l.code
-	AND deleted = FALSE
+	FROM core_topology AS e, zoning_city AS l
+	WHERE deleted = FALSE
 );
 
 CREATE VIEW {# geotrek.zoning #}.v_districts AS (
 	SELECT e.*, l.name
-	FROM core_topology AS e, zoning_districtedge AS f, zoning_district AS l
-	WHERE f.topo_object_id = e.id AND f.district_id = l.id
-	AND deleted = FALSE
+	FROM core_topology AS e, zoning_district AS l
+	WHERE deleted = FALSE
 );
 
 CREATE VIEW {# geotrek.zoning #}.v_restrictedareas AS (
 	SELECT e.*, l.id AS zonage_id, l.name, b.name AS type
-	FROM core_topology AS e, zoning_restrictedareaedge AS f, zoning_restrictedareatype AS b, zoning_restrictedarea AS l
-	WHERE f.topo_object_id = e.id AND f.restricted_area_id = l.id AND l.area_type_id = b.id
+	FROM core_topology AS e, zoning_restrictedareatype AS b, zoning_restrictedarea AS l
+	WHERE l.area_type_id = b.id
 	AND deleted = FALSE
 );
