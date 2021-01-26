f22711b87db99522844b9d16d51f06ecc0fabf54
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2021-01-26 18:11:06 +0100

    Fix uniquify mixin

--- geotrek/zoning/mixins.py
+++ geotrek/zoning/mixins.py
@@ -1,6 +1,6 @@
 from django.utils.translation import gettext_lazy as _
 from .models import RestrictedArea, District, City
-from geotrek.common.utils import intersecting
+from geotrek.common.utils import intersecting, uniquify
 
 
 class ZoningPropertiesMixin:
@@ -12,30 +12,30 @@ class ZoningPropertiesMixin:
 
     @property
     def areas(self):
-        return intersecting(RestrictedArea, self.zoning_property, distance=0)
+        return uniquify(intersecting(RestrictedArea, self.zoning_property, distance=0))
 
     @property
     def districts(self):
-        return intersecting(District, self.zoning_property, distance=0)
+        return uniquify(intersecting(District, self.zoning_property, distance=0))
 
     @property
     def cities(self):
-        return intersecting(City, self.zoning_property, distance=0)
+        return uniquify(intersecting(City, self.zoning_property, distance=0))
 
     @property
     def published_areas(self):
         if not hasattr(self, 'published'):
             return self.areas
-        return self.areas.filter(published=True)
+        return [area for area in self.areas if area.published]
 
     @property
     def published_districts(self):
         if not hasattr(self, 'published'):
             return self.districts
-        return self.districts.filter(published=True)
+        return [district for district in self.districts if district.published]
 
     @property
     def published_cities(self):
         if not hasattr(self, 'published'):
             return self.cities
-        return self.cities.filter(published=True)
+        return [city for city in self.cities if city.published]
