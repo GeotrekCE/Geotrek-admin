05e898abad58d57bd755ba2e479d610897f2d080
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2021-02-01 15:02:31 +0100

    Add test topologyfilter

--- geotrek/core/tests/test_filters.py
+++ geotrek/core/tests/test_filters.py
@@ -5,6 +5,7 @@ from django.test import TestCase
 
 from geotrek.land.tests.test_filters import LandFiltersTest
 
+from geotrek.core.factories import PathFactory, TrailFactory
 from geotrek.core.filters import PathFilterSet, TopologyFilter
 from geotrek.core.models import Topology
 
@@ -25,3 +26,19 @@ class TopologyFilterTest(TestCase):
 
         with self.assertRaises(NotImplementedError):
             topology.value_to_edges('Value')
+
+
+@skipIf(not settings.TREKKING_TOPOLOGY_ENABLED, 'Test with dynamic segmentation only')
+class TopologyFilterTrailTest(TestCase):
+    def setUp(self):
+        self.path = PathFactory()
+        self.trail = TrailFactory(paths=[(self.path, 0, 1)])
+
+    def test_trail_filters(self):
+        PathFactory()
+        qs = PathFilterSet().qs
+        self.assertEqual(qs.count(), 2)
+
+        data = {'trail': self.trail}
+        qs = PathFilterSet(data=data).qs
+        self.assertEqual(qs.count(), 1)
