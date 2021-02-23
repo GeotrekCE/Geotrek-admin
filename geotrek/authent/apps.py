c578f268a37b95d4ca7b29b008acadc8225fbab6
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2021-02-23 10:29:57 +0100

    Change pk=1 by default_structure_pk

--- geotrek/authent/apps.py
+++ geotrek/authent/apps.py
@@ -1,4 +1,5 @@
 from django.apps import AppConfig
+from django.conf import settings
 from django.utils.translation import gettext_lazy as _
 
 from django.db.models.signals import post_migrate
@@ -7,8 +8,8 @@ from django.db.models.signals import post_migrate
 def create_default_structure(sender, **kwargs):
     from geotrek.authent.models import Structure
     structure, created = Structure.objects.get_or_create(name=_('My structure'))
-    if created and not Structure.objects.filter(pk=1).exists():
-        structure.pk = 1
+    if created and not Structure.objects.filter(pk=settings.DEFAULT_STRUCTURE_PK).exists():
+        structure.pk = settings.DEFAULT_STRUCTURE_PK
         structure.save()
 
 
