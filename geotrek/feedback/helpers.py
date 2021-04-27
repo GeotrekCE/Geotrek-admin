cd0c31632e29b1fd3a20dcd76d768024ec65a5bc
Célia Prat <celia.prat@makina-corpus.com>
2021-04-27 12:40:09 +0200

    :construction: First draft for Suricate interace

--- geotrek/feedback/helpers.py
+++ geotrek/feedback/helpers.py
@@ -1,48 +1,168 @@
 import logging
 from hashlib import md5
-import requests
+from urllib import urlencode
 
-from django.template.loader import render_to_string
+import requests
 from django.conf import settings
 from django.core.mail import mail_managers
+from django.template.loader import render_to_string
 from django.utils.translation import gettext_lazy as _
 
 logger = logging.getLogger(__name__)
 
 
-def send_report_managers(report, template_name='feedback/report_email.html'):
-    subject = _('Feedback from {email}').format(email=report.email)
-    message = render_to_string(template_name, {'report': report})
+def send_report_managers(report, template_name="feedback/report_email.html"):
+    subject = _("Feedback from {email}").format(email=report.email)
+    message = render_to_string(template_name, {"report": report})
     mail_managers(subject, message, fail_silently=False)
 
 
+class SuricateManager:
+
+    URL = settings.SURICATE_REPORT_SETTINGS["URL"]
+    ID_ORIGIN = settings.SURICATE_REPORT_SETTINGS["ID_ORIGIN"]
+    PRIVATE_KEY_CLIENT_SERVER = settings.SURICATE_REPORT_SETTINGS[
+        "PRIVATE_KEY_CLIENT_SERVER"
+    ]
+    CHECK_ORIGIN = (
+        f"&check={md5((PRIVATE_KEY_CLIENT_SERVER + ID_ORIGIN).encode()).hexdigest()}"
+    )
+    USE_AUTH = "AUTH" in settings.SURICATE_REPORT_SETTINGS.keys()
+    AUTH = (None, settings.SURICATE_REPORT_SETTINGS)[USE_AUTH]
+
+    def get_from_suricate(self, endpoint, web_service="wsgestion/", url_params={}):
+        # Build ever-present URL parameter
+        origin_param = f"?id_origin={self.ID_ORIGIN}"
+        # Add specific URL parameters
+        extra_url_params = urlencode(url_params)
+        # Include alert ID in check when needed
+        if "uid_alerte" in url_params:
+            id_alert = url_params["uid_alerte"]
+            check = f"&check={md5((self.PRIVATE_KEY_CLIENT_SERVER + id_alert + self.ID_ORIGIN).encode()).hexdigest()}"
+        else:
+            check = self.CHECK_ORIGIN
+        # If HTTP Auth required, add to request
+        if self.USE_AUTH:
+            print(self.URL + web_service + endpoint + self.CHECK_ORIGIN)
+            response = requests.get(
+                f"{self.URL}{web_service}{endpoint}{origin_param}{extra_url_params}{check}",
+                auth=self.AUTH,
+            )
+            print(response)
+        else:
+            response = requests.get(
+                f"{self.URL}{web_service}{endpoint}{origin_param}{extra_url_params}{check}",
+            )
+            print(response)
+        # Handle response
+        # todo
+        if response.status_code not in [200, 201]:
+            print(response.status_code)
+            raise Exception("Failed to get Suricate API")
+        return response
+
+    def post_to_suricate(self, endpoint, params, web_service="wsgestion/"):
+        # If HTTP Auth required, add to request
+        if self.USE_AUTH:
+            response = requests.post(
+                f"{self.URL}{web_service}{endpoint}",
+                params,
+                auth=self.AUTH,
+            )
+        else:
+            response = requests.post(f"{self.URL}{web_service}{endpoint}", params)
+        # Handle response
+        # todo
+        if response.status_code not in [200, 201]:
+            raise Exception("Failed to post on Suricate API")
+        return response
+
+    def post_report(self, report):
+        CHECK = md5(
+            (self.PRIVATE_KEY_CLIENT_SERVER + report.email).encode()
+        ).hexdigest()
+        """Send report to Suricate Rest API"""
+        params = {
+            "id_origin": self.ID_ORIGIN,
+            "id_user": report.email,
+            "lat": report.geom.y,
+            "long": report.geom.x,
+            "report": report.comment,
+            "activite": report.activity.suricate_id,
+            "nature_prb": report.category.suricate_id,
+            "ampleur_prb": report.problem_magnitude.suricate_id,
+            "check": CHECK,
+            "os": "linux",
+            "version": settings.VERSION,
+        }
+
+        self.post_to_suricate("wsSendReport", params, "wsstandard")
+
+    def get_activities(self):
+        """Get activities list from Suricate Rest API"""
+        return self.get_from_suricate("wsGetActivities")
+
+    def get_statuses(self):
+        """Get statuses list from Suricate Rest API"""
+        return self.get_from_suricate("wsGetStatusList")
+
+    def get_alerts(self):
+        """Get reports list from Suricate Rest API"""
+        return self.get_from_suricate("wsGetAlerts")
+
+    def lock_alert(self, id_alert):
+        """Lock report on Suricate Rest API"""
+        return self.get_from_suricate(
+            "wsLockAlert", extra_url_params={"uid_alerte": id_alert}
+        )
+
+    def unlock_alert(self, id_alert):
+        """Unlock report on Suricate Rest API"""
+        return self.get_from_suricate(
+            "wsUnlockAlert", url_params={"uid_alerte": id_alert}
+        )
+
+    def post_message(self, message, id_alert):
+        """Send message to sentinel through Suricate Rest API"""
+        check = md5(
+            (self.PRIVATE_KEY_CLIENT_SERVER + self.ID_ORIGIN + id_alert).encode()
+        ).hexdigest()
+        params = {
+            "id_origin": self.ID_ORIGIN,
+            "uid_alerte": id_alert,
+            "message": message,
+            "check": check,
+        }
+
+        self.post_to_suricate("wsSendMessageSentinelle", params)
+
+    def update_status(self, id_alert, new_status, message):
+        """Update status for given report on Suricate Rest API"""
+        check = md5(
+            (self.PRIVATE_KEY_CLIENT_SERVER + self.ID_ORIGIN + id_alert).encode()
+        ).hexdigest()
+
+        params = {
+            "id_origin": self.ID_ORIGIN,
+            "uid_alerte": id_alert,
+            "statut": new_status,
+            "txt_changestatut": message,
+            "check": check,
+        }
+        self.post_to_suricate("wsUpdateStatus", params)
+
+    def update_gps(self, id_alert, gps_lat, gps_long):
+        """Update report GPS coordinates on Suricate Rest API"""
+        self.get_from_suricate(
+            "wsUpdateStatus",
+            url_params={
+                "uid_alerte": id_alert,
+                "gpslatitude": gps_lat,
+                "gpslongitude": gps_long,
+            },
+        )
+
+
 def post_report_to_suricate(report):
-    """Send report to Suricate Rest API"""
-
-    URL = settings.SURICATE_REPORT_SETTINGS['URL']
-    ID_ORIGIN = settings.SURICATE_REPORT_SETTINGS['ID_ORIGIN']
-    PRIVATE_KEY_CLIENT_SERVER = settings.SURICATE_REPORT_SETTINGS['PRIVATE_KEY_CLIENT_SERVER']
-    CHECK = md5((PRIVATE_KEY_CLIENT_SERVER + report.email).encode()).hexdigest()
-
-    params = {
-        'id_origin': ID_ORIGIN,
-        'id_user': report.email,
-        'lat': report.geom.y,
-        'long': report.geom.x,
-        'report': report.comment,
-        'activite': report.activity.suricate_id,
-        'nature_prb': report.category.suricate_id,
-        'ampleur_prb': report.problem_magnitude.suricate_id,
-        'check': CHECK,
-        'os': 'linux',
-        'version': settings.VERSION,
-    }
-
-    # If HTTP Auth required, add to request
-    if 'AUTH' in settings.SURICATE_REPORT_SETTINGS.keys():
-        response = requests.post(URL + 'wsSendReport', params, auth=settings.SURICATE_REPORT_SETTINGS['AUTH'])
-    else:
-        response = requests.post(URL + 'wsSendReport', params)
-
-    if response.status_code not in [200, 201]:
-        raise Exception("Failed to post on Suricate API")
+    """ For retrocompatibility """
+    SuricateManager().post_report(report)
