be344af809f3b4ae72017ed12cbe9efc9150b02f
Célia Prat <celia.prat@makina-corpus.com>
2021-04-28 15:52:36 +0200

    :construction: Implement activities parsing

--- geotrek/feedback/parsers.py
+++ geotrek/feedback/parsers.py
@@ -1,6 +1,8 @@
-from geotrek.feedback.models import ReportStatus
-from .helpers import SuricateRequestManager
 import logging
+
+from geotrek.feedback.models import ReportActivity, ReportStatus
+
+from .helpers import SuricateRequestManager
 
 logger = logging.getLogger(__name__)
 
@@ -8,13 +10,20 @@ logger = logging.getLogger(__name__)
 class SuricateParser(SuricateRequestManager):
     def get_activities(self):
         """Get activities list from Suricate Rest API"""
-        return self.get_from_suricate("wsGetActivities")
+        data = self.get_from_suricate("wsGetActivities")
+        for activity in data["activites"]:
+            obj, created = ReportActivity.objects.get_or_create(
+                suricate_id=activity["id"], defaults={"label": activity["libelle"]}
+            )
+            if created:
+                logger.info(
+                    f"Created new activity - id: {activity['id']}, label: {activity['libelle']}"
+                )
 
     def get_statuses(self):
         """Get statuses list from Suricate Rest API"""
         data = self.get_from_suricate("wsGetStatusList")
-        for status in data['statuts']:
-            print(status)
+        for status in data["statuts"]:
             obj, created = ReportStatus.objects.get_or_create(
                 id=status["id"], defaults={"label": status["libelle"]}
             )
