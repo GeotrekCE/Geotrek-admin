1a60962d8d8353ec9917cdeed4638875d7f63947
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2021-05-19 09:38:31 +0200

    Move functions lookup

--- geotrek/common/lookups.py
+++ geotrek/common/lookups.py
@@ -1,6 +1,12 @@
 from django.contrib.gis.db.models.fields import BaseSpatialField
 
-from geotrek.common.utils.functions import IsEmpty as IsEmptyFunc
+from django.contrib.gis.db.models import BooleanField
+from django.db.models.lookups import Transform
+
+
+class IsEmptyFunc(Transform):
+    function = 'ST_ISEMPTY'
+    output_field = BooleanField()
 
 
 @BaseSpatialField.register_lookup
--- geotrek/common/utils/functions.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.gis.db.models import BooleanField
-from django.db.models.lookups import Transform
-
-
-class IsEmpty(Transform):
-    function = 'ST_ISEMPTY'
-    output_field = BooleanField()
