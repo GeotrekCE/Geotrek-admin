64796bef190d417b98bf6502dda194b4645dd3b2
Chatewgne <chatewgne@gmail.com>
2021-06-24 16:26:44 +0200

    Order Treks by localized names in APIv2

--- geotrek/api/v2/views/trekking.py
+++ geotrek/api/v2/views/trekking.py
@@ -3,6 +3,7 @@ from django.db.models import F, Q
 from django.db.models.aggregates import Count
 from django.http import Http404
 from django.shortcuts import get_object_or_404
+from django.utils.translation import activate
 
 from rest_framework.response import Response
 
@@ -15,28 +16,31 @@ from geotrek.trekking import models as trekking_models
 class TrekViewSet(api_viewsets.GeotrekGeometricViewset):
     filter_backends = api_viewsets.GeotrekGeometricViewset.filter_backends + (api_filters.GeotrekTrekQueryParamsFilter,)
     serializer_class = api_serializers.TrekSerializer
-    queryset = trekking_models.Trek.objects.existing() \
-        .select_related('topo_object') \
-        .prefetch_related('topo_object__aggregations', 'accessibilities', 'attachments') \
-        .annotate(geom3d_transformed=Transform(F('geom_3d'), settings.API_SRID),
-                  length_2d_m=Length('geom'),
-                  length_3d_m=Length3D('geom_3d')) \
-        .order_by('pk')  # Required for reliable pagination
+
+    def get_queryset(self):
+        activate(self.request.GET.get('language'))
+        return trekking_models.Trek.objects.existing() \
+            .select_related('topo_object') \
+            .prefetch_related('topo_object__aggregations', 'accessibilities', 'attachments') \
+            .annotate(geom3d_transformed=Transform(F('geom_3d'), settings.API_SRID),
+                      length_2d_m=Length('geom'),
+                      length_3d_m=Length3D('geom_3d')) \
+            .order_by("name")  # Required for reliable pagination
 
     def retrieve(self, request, pk=None, format=None):
         # Return detail view even for unpublished treks that are childrens of other published treks
         qs_filtered = self.filter_published_lang_retrieve(request, self.queryset)
         try:
             trek = qs_filtered.get(pk=pk)
-        except self.queryset.model.DoesNotExist:
-            raise Http404('No %s matches the given query.' % self.queryset.model._meta.object_name)
+        except self.get_queryset().model.DoesNotExist:
+            raise Http404('No %s matches the given query.' % self.get_queryset().model._meta.object_name)
         serializer_class = self.get_serializer_class()
         serializer = serializer_class(trek, many=False, context={'request': request})
         return Response(serializer.data)
 
     def filter_published_lang_retrieve(self, request, queryset):
         # filter trek by publication language (including parents publication language)
-        qs = queryset
+        qs = self.get_queryset()
         language = request.GET.get('language', 'all')
         associated_published_fields = [f.name for f in qs.model._meta.get_fields() if f.name.startswith('published')]
 
@@ -59,8 +63,10 @@ class TrekViewSet(api_viewsets.GeotrekGeometricViewset):
 
 class TourViewSet(TrekViewSet):
     serializer_class = api_serializers.TourSerializer
-    queryset = TrekViewSet.queryset.annotate(count_children=Count('trek_children')) \
-        .filter(count_children__gt=0)
+
+    def get_queryset(self):
+        return super().get_queryset().annotate(count_children=Count('trek_children')) \
+            .filter(count_children__gt=0)
 
 
 class PracticeViewSet(api_viewsets.GeotrekViewSet):
