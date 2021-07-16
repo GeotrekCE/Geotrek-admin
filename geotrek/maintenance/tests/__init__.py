7de0af37608fd6493c807c1684fa2a42fc6f912d
Chatewgne <chatewgne@gmail.com>
2021-07-16 15:23:20 +0200

    Remove non standard test imports

--- geotrek/maintenance/tests/__init__.py
+++
@@ -1,5 +0,0 @@
-from .test_forms import *  # NOQA
-from .test_views import *  # NOQA
-from .test_intervention import *  # NOQA
-from .test_project import *  # NOQA
-from .test_filters import *  # NOQA
