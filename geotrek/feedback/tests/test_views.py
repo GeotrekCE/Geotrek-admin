8314fc6d216965c40f458e609bab9923290d6016
Chatewgne <chatewgne@gmail.com>
2021-08-12 13:23:34 +0200

    Override test method for NoDeleteMixin

--- geotrek/feedback/tests/test_views.py
+++ geotrek/feedback/tests/test_views.py
@@ -88,6 +88,49 @@ class ReportViewsTest(CommonTest):
         self.assertEqual(obj.email, data['email'])
         self.logout()
 
+    def test_crud_status(self):
+        if self.model is None:
+            return  # Abstract test should not run
+
+        self.login()
+
+        obj = self.modelfactory()
+
+        response = self.client.get(obj.get_list_url())
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get(obj.get_detail_url().replace(str(obj.pk), '1234567890'))
+        self.assertEqual(response.status_code, 404)
+
+        response = self.client.get(obj.get_detail_url())
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get(obj.get_update_url())
+        self.assertEqual(response.status_code, 200)
+        self._post_update_form(obj)
+        self._check_update_geom_permission(response)
+
+        response = self.client.get(obj.get_delete_url())
+        self.assertEqual(response.status_code, 200)
+
+        url = obj.get_detail_url()
+        obj.delete()
+        response = self.client.get(url)
+        # No delete mixin
+        self.assertEqual(response.status_code, 200)
+
+        self._post_add_form()
+
+        # Test to update without login
+        self.logout()
+
+        obj = self.modelfactory()
+
+        response = self.client.get(self.model.get_add_url())
+        self.assertEqual(response.status_code, 302)
+        response = self.client.get(obj.get_update_url())
+        self.assertEqual(response.status_code, 302)
+
 
 class BaseAPITest(TestCase):
     def setUp(self):
