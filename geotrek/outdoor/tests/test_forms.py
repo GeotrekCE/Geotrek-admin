7f713174b3b55c0f0fb3a16565c631e89031faac
Chatewgne <chatewgne@gmail.com>
2021-10-19 11:44:14 +0200

    Fix test coverage

--- geotrek/outdoor/tests/test_forms.py
+++ geotrek/outdoor/tests/test_forms.py
@@ -55,9 +55,8 @@ class CourseFormTest(TestCase):
     def test_form_init(self):
         site = SiteFactory()
         course = CourseFactory()
-        course.parent_sites.set([site])
-        form = CourseForm(user=self.user, instance=course)
-        self.assertEqual(form.initial['parent_sites'], [site])
+        form = CourseForm(user=self.user, instance=course, parent_sites=site.pk)
+        self.assertEqual(form.initial['parent_sites'], site)
 
 
 class CourseItinerancyTestCase(TestCase):
