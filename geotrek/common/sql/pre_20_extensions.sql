60d562b21304b7b090760b3fcbbaa8024affe8b9
Chatewgne <chatewgne@gmail.com>
2021-10-27 12:30:43 +0200

    Comment extension reason

--- geotrek/common/sql/pre_20_extensions.sql
+++ geotrek/common/sql/pre_20_extensions.sql
@@ -1,1 +1,3 @@
+-- Used to ensure extension is enabled even in test database (migrations disabled)
+-- Otherwise UUIDs on objects can not be generated on insert (including path splits)
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
