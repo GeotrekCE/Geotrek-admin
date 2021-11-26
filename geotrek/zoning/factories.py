7cd3f991f958be201880647970d5cb6b94c48ad9
J-E Castagnede <j.e.castagnede@gmail.com>
2021-11-26 17:31:08 +0100

    backport mapentity factory helper

--- geotrek/zoning/factories.py
+++ geotrek/zoning/factories.py
@@ -1,11 +1,46 @@
+import itertools
+
 import factory
 
 from django.conf import settings
 from django.contrib.gis.geos import Polygon, MultiPolygon
 
-from mapentity.helpers import bbox_split_srid_2154
 
 from . import models
+
+
+def bbox_split(bbox, by_x=2, by_y=2, cycle=False):
+    """Divide a box in rectangle, by_x parts and by_y parts"""
+    minx, miny, maxx, maxy = bbox
+
+    stepx = (maxx - minx) / by_x
+    stepy = (maxy - miny) / by_y
+
+    def gen():
+        """define as inner function to decorate it with cycle"""
+        stepx_tmp = minx
+        while stepx_tmp + stepx <= maxx:
+            stepx_next = stepx_tmp + stepx
+
+            stepy_tmp = miny
+            while stepy_tmp + stepy <= maxy:
+                stepy_next = stepy_tmp + stepy
+                yield (stepx_tmp, stepy_tmp, stepx_next, stepy_next)
+
+                stepy_tmp = stepy_next
+
+            stepx_tmp = stepx_next
+
+    if cycle:
+        return itertools.cycle(gen())
+    else:
+        return gen()
+
+
+def bbox_split_srid_2154(*args, **kwargs):
+    """Just round"""
+    gen = bbox_split(*args, **kwargs)
+    return iter(lambda: map(round, next(gen)), None)
 
 
 # Don't intersect with geom from PathFactory
