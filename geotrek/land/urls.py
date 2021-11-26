654f5381da17384ce9c16b9990cb9a9c57856916
Chatewgne <chatewgne@gmail.com>
2021-11-26 13:14:05 +0100

    Clean names in Land module

--- geotrek/land/urls.py
+++ geotrek/land/urls.py
@@ -1,12 +1,20 @@
 from django.conf import settings
-from mapentity.registry import registry
+from django.utils.translation import gettext as _
+from mapentity.registry import registry, MapEntityOptions
 
 from . import models
 
 app_name = 'land'
 
+
+class LandEdgeOptions(MapEntityOptions):
+    def __init__(self, model):
+        super().__init__(model)
+        self.label = _("Land")
+
+
 urlpatterns = registry.register(models.PhysicalEdge, menu=False)
-urlpatterns += registry.register(models.LandEdge, menu=settings.TREKKING_TOPOLOGY_ENABLED and settings.LANDEDGE_MODEL_ENABLED)
+urlpatterns += registry.register(models.LandEdge, options=LandEdgeOptions, menu=settings.TREKKING_TOPOLOGY_ENABLED and settings.LANDEDGE_MODEL_ENABLED)
 urlpatterns += registry.register(models.CompetenceEdge, menu=False)
 urlpatterns += registry.register(models.WorkManagementEdge, menu=False)
 urlpatterns += registry.register(models.SignageManagementEdge, menu=False)
