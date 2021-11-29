8e5e0d8f17e0fe29304c79710e783d99b1e08694
Chatewgne <chatewgne@gmail.com>
2021-11-29 10:47:14 +0100

    Manage translations for Status module in navigation bar

--- geotrek/land/urls.py
+++ geotrek/land/urls.py
@@ -8,13 +8,21 @@ app_name = 'land'
 
 
 class LandEdgeOptions(MapEntityOptions):
+    """ 
+    Overwrite options for Statuses module to redirect to LandEdge objects
+    """
     def __init__(self, model):
         super().__init__(model)
-        self.label = _("Land")
+        self.url_list = 'land:landedge_list'
+        self.url_add = 'land:landedge_add'
+        self.icon = 'images/landedge.png'
+        self.icon_small = 'images/landedge-16.png'
+        self.icon_big = 'images/landedge-96.png'
 
 
 urlpatterns = registry.register(models.PhysicalEdge, menu=False)
-urlpatterns += registry.register(models.LandEdge, options=LandEdgeOptions, menu=settings.TREKKING_TOPOLOGY_ENABLED and settings.LANDEDGE_MODEL_ENABLED)
+urlpatterns += registry.register(models.LandEdge, menu=False)
+urlpatterns += registry.register(models.Status, options=LandEdgeOptions, menu=settings.TREKKING_TOPOLOGY_ENABLED and settings.LANDEDGE_MODEL_ENABLED)
 urlpatterns += registry.register(models.CompetenceEdge, menu=False)
 urlpatterns += registry.register(models.WorkManagementEdge, menu=False)
 urlpatterns += registry.register(models.SignageManagementEdge, menu=False)
