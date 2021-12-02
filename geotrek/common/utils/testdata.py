a26922720359e4d645fa5924ce2e7373586b7392
Chatewgne <chatewgne@gmail.com>
2021-12-02 09:57:42 +0100

    Add tests for resize attachments

--- geotrek/common/utils/testdata.py
+++ geotrek/common/utils/testdata.py
@@ -1,3 +1,6 @@
+from io import BytesIO
+from PIL import Image
+
 from django.core.files.uploadedfile import SimpleUploadedFile
 
 import factory
@@ -21,6 +24,15 @@ def get_dummy_uploaded_image(name='dummy_img.png'):
 
 def get_dummy_uploaded_image_svg(name='dummy_img.svg'):
     return SimpleUploadedFile(name, SVG_FILE, content_type='image/svg')
+
+
+def get_big_dummy_uploaded_image(name='dummy_img.svg'):
+    file = BytesIO()
+    image = Image.new('RGBA', size=(2000, 2000), color=(155, 0, 0))
+    image.save(file, 'png')
+    file.name = 'test_big.png'
+    file.seek(0)
+    return SimpleUploadedFile(file.name, file.read(), content_type='image/png')
 
 
 def get_dummy_uploaded_file(name='dummy_file.txt'):
