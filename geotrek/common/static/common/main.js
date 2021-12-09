d7de41d5b3038c449d815d9da97a75aaa4c0d3cb
Chatewgne <chatewgne@gmail.com>
2021-12-09 17:46:09 +0100

    Revert accidental changes

---
+++ geotrek/common/static/common/main.js
@@ -0,0 +1,5 @@
+$(window).on('entity:view:list', function () {
+    // Move all topology-filters to separate tab
+    $('#mainfilter .right-filter').parent('p')
+        .detach().appendTo('#mainfilter > .right');
+});
