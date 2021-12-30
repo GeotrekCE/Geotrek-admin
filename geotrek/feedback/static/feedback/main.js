649e8e85b1ebf516c00a0b59e5aaa839e6c0f59f
Chatewgne <chatewgne@gmail.com>
2021-12-30 16:14:27 +0100

    Dynamically display message fields in Report forms

---
+++ geotrek/feedback/static/feedback/main.js
@@ -0,0 +1,24 @@
+$(window).on('entity:view:add entity:view:update', function (e, data) {
+    $('#div_id_message_sentinel').prop('hidden', true);
+    $('#div_id_message_supervisor').prop('hidden', true);
+    $('#id_status').change(function () {
+        display_message_fields_on_status_change();
+    });
+    $('#id_assigned_user').change(function () {
+        display_message_fields_on_supervisor_change();
+    });
+    return;
+});
+
+function display_message_fields_on_status_change() {
+    var status_ids = JSON.parse($('#status_ids').text());
+    var selected = $('#id_status').val() || [];
+    do_display = ((status_ids[selected] == "resolved") || (status_ids[selected] == "classified"))
+    $('#div_id_message_sentinel').prop('hidden', !do_display);
+}
+
+function display_message_fields_on_supervisor_change() {
+    var selected = $('#id_assigned_user').val() || null;
+    $('#div_id_message_sentinel').prop('hidden', (selected == null));
+    $('#div_id_message_supervisor').prop('hidden', (selected == null));
+}
--- geotrek/feedback/templatetags/feedback_tags.py
+++ geotrek/feedback/templatetags/feedback_tags.py
@@ -1,3 +1,5 @@
+import json
+from geotrek.feedback.models import ReportStatus
 from django import template
 from django.conf import settings
 
@@ -8,3 +10,12 @@ register = template.Library()
 @register.simple_tag
 def suricate_management_enabled():
     return settings.SURICATE_MANAGEMENT_ENABLED
+
+
+@register.simple_tag
+def status_ids():
+    status_ids = {
+        status.pk: str(status.suricate_id)
+        for status in ReportStatus.objects.all()
+    }
+    return json.dumps(status_ids)
