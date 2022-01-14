9ada9861b6362eb0fa52d8c60ce39729be956f02
LePetitTim <timothee.de-montety@makina-corpus.com>
2022-01-14 17:52:20 +0100

    [#2845] Change help equipment

--- geotrek/trekking/models.py
+++ geotrek/trekking/models.py
@@ -140,7 +140,7 @@ class Trek(Topology, StructureRelated, PicturesMixin, PublishableMixin, MapEntit
     ratings = models.ManyToManyField(Rating, related_name='sites', blank=True)
     ratings_description = models.TextField(verbose_name=_("Ratings description"), blank=True)
     equipment = models.TextField(verbose_name=_("Equipment"), blank=True,
-                                 help_text=_("Equipment needed"))
+                                 help_text=_("Equipment needed, adviced ..."))
     themes = models.ManyToManyField(Theme, related_name="treks", blank=True, verbose_name=_("Themes"),
                                     help_text=_("Main theme(s)"))
     networks = models.ManyToManyField('TrekNetwork', related_name="treks", blank=True, verbose_name=_("Networks"),
