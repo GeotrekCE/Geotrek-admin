c6de4d8cf925a2002add64feeae693e19bc8d2a8
Emmanuelle Helly <emmanuelle.helly@makina-corpus.com>
2022-01-25 16:25:58 +0100

    Display infrastructures

--- geotrek/infrastructure/static/infrastructure/main.js
+++ geotrek/infrastructure/static/infrastructure/main.js
@@ -4,21 +4,26 @@
 
 $(window).on('entity:map', function (e, data) {
     var map = data.map;
+    loaded_infrastructure = false;
 
     // Show infrastructure layer in application maps
 	var layer = new L.ObjectsLayer(null, {
 		modelname: 'infrastructure',
 		style: L.Util.extend(window.SETTINGS.map.styles['infrastructure'] || {}, { clickable:false }),
-		pointToLayer: function (feature, latlng) {
-			var infrastructureIcon = L.icon({
-				iconUrl: feature.properties.type.pictogram,
-				iconSize: [18, 18],
-				iconAnchor: [9, 9],
-			});
-			return L.marker(latlng, {icon: infrastructureIcon});
-		}
 	});
 	var url = window.SETTINGS.urls['infrastructure_layer'];
-	layer.load(url);
-	map.layerscontrol.addOverlay(layer, tr('Infrastructure'), tr('Infrastructure'));
+
+    if (data.modelname != layer.modelname){
+	    map.layerscontrol.addOverlay(layer, tr('Infrastructure'), tr('Infrastructure'));
+    };
+
+    map.on('layeradd', function (e) {
+        var options = e.layer.options || { 'modelname': 'None' };
+        if (! loaded_infrastructure) {
+            if (options.modelname == 'infrastructure' && options.modelname != data.modelname) {
+                e.layer.load(url);
+                loaded_infrastructure = true;
+            }
+        }
+    });
 });
