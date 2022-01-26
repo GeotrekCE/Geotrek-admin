2f6882085049bf72bd552a33942e27bcbde693b2
Emmanuelle Helly <emmanuelle.helly@makina-corpus.com>
2022-01-26 08:04:16 +0100

    Add intervention layer

---
+++ geotrek/maintenance/static/maintenance/main.js
@@ -0,0 +1,31 @@
+//
+// Infrastructure
+//
+
+$(window).on('entity:map', function (e, data) {
+    var modelname = 'intervention';
+    var layername = `${modelname}_layer`;
+	var url = window.SETTINGS.urls[layername];
+    var loaded_infrastructure = false;
+    var map = data.map;
+
+    // Show infrastructure layer in application maps
+	var layer = new L.ObjectsLayer(null, {
+		modelname: modelname,
+		style: L.Util.extend(window.SETTINGS.map.styles[modelname] || {}, { clickable:false }),
+	});
+
+    if (data.modelname != modelname){
+	    map.layerscontrol.addOverlay(layer, tr('Intervention'), tr('Maintenance'));
+    };
+
+    map.on('layeradd', function (e) {
+        var options = e.layer.options || { 'modelname': 'None' };
+        if (! loaded_infrastructure) {
+            if (options.modelname == modelname && options.modelname != data.modelname) {
+                e.layer.load(url);
+                loaded_infrastructure = true;
+            }
+        }
+    });
+});
