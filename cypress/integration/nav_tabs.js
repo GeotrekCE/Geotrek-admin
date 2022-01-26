2f3d331ca6f1d871becf0a85efbc8ff173c77ead
LePetitTim <timothee.de-montety@makina-corpus.com>
2022-01-26 18:30:03 +0100

    Add test tab cypress

---
+++ cypress/integration/nav_tabs.js
@@ -0,0 +1,29 @@
+describe('Nav tabs properties/attachments', () => {
+  beforeEach(() => {
+      const username = 'admin'
+      const password = 'admin'
+      cy.request('/login/?next=/')
+        .its('body')
+        .then((body) => {
+          // we can use Cypress.$ to parse the string body
+          // thus enabling us to query into it easily
+          const $html = Cypress.$(body)
+          const csrf = $html.find('input[name=csrfmiddlewaretoken]').val()
+
+          cy.loginByCSRF(csrf, username, password)
+          .then((resp) => {
+            expect(resp.status).to.eq(200)
+          })
+        })
+  })
+
+  it('Use tabs', () => {
+    cy.visit('http://localhost:8000/trek/1/')
+    cy.get("a#tab-properties").should('contain', '.active')
+    cy.get("a#tab-attachments-accessibility").should('not.contain', '.active')
+    cy.get("a#tab-attachments-accessibility").click()
+    cy.get("a#tab-attachments-accessibility").should('contain', '.active')
+    cy.get("a#tab-properties").should('not.contain', '.active')
+  })
+
+})
