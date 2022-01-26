0f30d5e880334c56c04381c81aa345f0b486f608
Emmanuelle Helly <emmanuelle.helly@makina-corpus.com>
2022-01-26 07:51:54 +0100

    Improve js

--- geotrek/infrastructure/static/infrastructure/main.js
+++ geotrek/infrastructure/static/infrastructure/main.js
@@ -3,24 +3,26 @@
 //
 
 $(window).on('entity:map', function (e, data) {
+    var modelname = 'infrastructure';
+    var layername = `${modelname}_layer`;
+	var url = window.SETTINGS.urls[layername];
+    var loaded_infrastructure = false;
     var map = data.map;
-    loaded_infrastructure = false;
 
     // Show infrastructure layer in application maps
 	var layer = new L.ObjectsLayer(null, {
-		modelname: 'infrastructure',
-		style: L.Util.extend(window.SETTINGS.map.styles['infrastructure'] || {}, { clickable:false }),
+		modelname: modelname,
+		style: L.Util.extend(window.SETTINGS.map.styles[modelname] || {}, { clickable:false }),
 	});
-	var url = window.SETTINGS.urls['infrastructure_layer'];
 
-    if (data.modelname != layer.modelname){
+    if (data.modelname != modelname){
 	    map.layerscontrol.addOverlay(layer, tr('Infrastructure'), tr('Infrastructure'));
     };
 
     map.on('layeradd', function (e) {
         var options = e.layer.options || { 'modelname': 'None' };
         if (! loaded_infrastructure) {
-            if (options.modelname == 'infrastructure' && options.modelname != data.modelname) {
+            if (options.modelname == modelname && options.modelname != data.modelname) {
                 e.layer.load(url);
                 loaded_infrastructure = true;
             }
