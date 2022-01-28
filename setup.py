fd38e6e2922abaeb03d21e1a5a7c9ff9d8b00d91
LePetitTim <timothee.de-montety@makina-corpus.com>
2022-01-28 14:39:34 +0100

    Update mapentity block

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     scripts=['manage.py'],
     install_requires=[
         'Django==3.1.*',
-        'mapentity==7.1.1',
+        'mapentity==7.1.2',
         'env_file',
         # pinned by requirements.txt
         'python-memcached',
