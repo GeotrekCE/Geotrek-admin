6510d91b230a818a3c120033300e318c678137df
LePetitTim <timothee.de-montety@makina-corpus.com>
2022-02-09 16:55:55 +0100

    Fix migration 2 76 2

--- geotrek/trekking/migrations/0039_move_traduction_disabled_infrastructure_equipment.py
+++ geotrek/trekking/migrations/0039_move_traduction_disabled_infrastructure_equipment.py
@@ -11,12 +11,25 @@ def forward(apps, schema_editor):
                 f"SELECT 1 FROM information_schema.columns WHERE table_name='trekking_trek' AND column_name='disabled_infrastructure_{lang}'"
             )
             if cursor.fetchone():
-                cursor.execute(f"UPDATE trekking_trek SET accessibility_infrastructure_{lang}=disabled_infrastructure_{lang} WHERE accessibility_infrastructure_{lang} = '';")
+                cursor.execute(
+                    f"SELECT 1 FROM information_schema.columns WHERE table_name='trekking_trek' AND column_name='accessibility_infrastructure_{lang}'"
+                )
+                if cursor.fetchone():
+                    cursor.execute(f"UPDATE trekking_trek SET accessibility_infrastructure_{lang}=disabled_infrastructure_{lang} WHERE accessibility_infrastructure_{lang} = '';")
+                else:
+                    cursor.execute(f'ALTER TABLE trekking_trek RENAME disabled_infrastructure_{lang} TO accessibility_infrastructure_{lang}')
             cursor.execute(
                 f"SELECT 1 FROM information_schema.columns WHERE table_name='trekking_trek' AND column_name='equipment_{lang}'"
             )
             if cursor.fetchone():
-                cursor.execute(f"UPDATE trekking_trek SET gear_{lang}=equipment_{lang} WHERE equipment_{lang} = ''")
+                cursor.execute(
+                    f"SELECT 1 FROM information_schema.columns WHERE table_name='trekking_trek' AND column_name='gear_{lang}'"
+                )
+                if cursor.fetchone():
+                    cursor.execute(f"UPDATE trekking_trek SET gear_{lang}=equipment_{lang} WHERE equipment_{lang} = ''")
+                else:
+                    cursor.execute(
+                        f'ALTER TABLE trekking_trek RENAME equipment_{lang} TO gear_{lang}')
 
 
 class Migration(migrations.Migration):
