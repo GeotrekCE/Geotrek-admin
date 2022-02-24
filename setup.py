d4f2c4c25a0f4b425bda7475eab7cbb8098a4a92
J-E Castagnede <j.e.castagnede@gmail.com>
2022-02-24 14:50:43 +0100

    use dev mapentity version

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     scripts=['manage.py'],
     install_requires=[
         'Django==3.1.*',
-        'mapentity>=7.1.3',
+        'mapentity@git+https://github.com/makinacorpus/django-mapentity.git@paginate_list_geotrek_integration#egg=mapentity',
         'env_file',
         # pinned by requirements.txt
         'python-memcached',
