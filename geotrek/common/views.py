77361a0a19f47e3f520896993c025bd47909dc5f
Emmanuelle Helly <emmanuelle.helly@makina-corpus.com>
2022-03-14 10:56:03 +0100

    Display fields name to be completed

--- geotrek/common/views.py
+++ geotrek/common/views.py
@@ -104,6 +104,15 @@ class MarkupPublic(PublicOrReadPermMixin, DocumentPublicMixin, mapentity_views.M
     pass
 
 
+class CompletenessMixin:
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        modelname = self.get_model()._meta.object_name.lower()
+        if modelname in settings.COMPLETENESS_FIELDS:
+            context['completeness_fields'] = settings.COMPLETENESS_FIELDS[modelname]
+        return context
+
+
 #
 # Concrete views
 # ..............................
