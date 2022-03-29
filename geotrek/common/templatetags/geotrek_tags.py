dd10f6af144c2af46aaaa65a60a7f373bc2b0ed9
LePetitTim <timothee.de-montety@makina-corpus.com>
2022-03-29 17:45:28 +0200

    Fix template tag duration

--- geotrek/common/templatetags/geotrek_tags.py
+++ geotrek/common/templatetags/geotrek_tags.py
@@ -48,10 +48,7 @@ def duration(value):
     seconds = timedelta(minutes=float(value) * 60)
     duration = datetime(1, 1, 1) + seconds
 
-    if duration.day == 1:
-        final_duration = _("%s day") % duration.day
-
-    elif duration.day > 1:
+    if duration.day > 1:
         if duration.hour > 0 or duration.minute > 0:
             final_duration = _("%s days" % duration.day)
 
