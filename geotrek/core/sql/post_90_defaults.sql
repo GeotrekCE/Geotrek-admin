0e8d385ea34ab43755ac9866b0047bcbfa6c0a04
LePetitTim <timothee.de-montety@makina-corpus.com>
2022-04-06 14:57:38 +0200

    Fix sql core default

--- geotrek/core/sql/post_90_defaults.sql
+++ geotrek/core/sql/post_90_defaults.sql
@@ -16,7 +16,7 @@ ALTER TABLE core_path ALTER COLUMN arrival SET DEFAULT '';
 ALTER TABLE core_path ALTER COLUMN eid SET DEFAULT '';
 ALTER TABLE core_path ALTER COLUMN draft SET DEFAULT False;
 ALTER TABLE core_path ALTER COLUMN uuid SET DEFAULT gen_random_uuid();
-ALTER TABLE core_path ALTER COLUMN geom3d SET DEFAULT NULL;
+ALTER TABLE core_path ALTER COLUMN geom_3d SET DEFAULT NULL;
 ALTER TABLE core_path ALTER COLUMN "length" SET DEFAULT 0.0;
 ALTER TABLE core_path ALTER COLUMN ascent SET DEFAULT 0.0;
 ALTER TABLE core_path ALTER COLUMN descent SET DEFAULT 0;
@@ -31,13 +31,13 @@ ALTER TABLE core_path ALTER COLUMN date_update SET DEFAULT now();
 -- Topology
 -----------
 -- paths
-ALTER TABLE core_topology ALTER COLUMN offset SET DEFAULT 0.0;
+ALTER TABLE core_topology ALTER COLUMN "offset" SET DEFAULT 0.0;
 ALTER TABLE core_topology ALTER COLUMN kind SET DEFAULT '';
 ALTER TABLE core_topology ALTER COLUMN "length" SET DEFAULT 0.0;
 ALTER TABLE core_topology ALTER COLUMN geom_need_update SET DEFAULT FALSE;
 -- geom
 ALTER TABLE core_topology ALTER COLUMN uuid SET DEFAULT gen_random_uuid();
-ALTER TABLE core_topology ALTER COLUMN geom3d SET DEFAULT NULL;
+ALTER TABLE core_topology ALTER COLUMN geom_3d SET DEFAULT NULL;
 ALTER TABLE core_topology ALTER COLUMN "length" SET DEFAULT 0.0;
 ALTER TABLE core_topology ALTER COLUMN ascent SET DEFAULT 0.0;
 ALTER TABLE core_topology ALTER COLUMN descent SET DEFAULT 0;
@@ -55,7 +55,7 @@ ALTER TABLE core_topology ALTER COLUMN deleted SET DEFAULT False;
 -- topo_object
 ALTER TABLE core_pathaggregation ALTER COLUMN start_position SET DEFAULT 0;
 ALTER TABLE core_pathaggregation ALTER COLUMN end_position SET DEFAULT 0;
-ALTER TABLE core_pathaggregation ALTER COLUMN order SET DEFAULT 0;
+ALTER TABLE core_pathaggregation ALTER COLUMN "order" SET DEFAULT 0;
 
 
 -- PathSource
