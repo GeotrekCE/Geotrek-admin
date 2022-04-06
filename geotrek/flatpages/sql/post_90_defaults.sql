306d6c7989dcb19cecc3ea7961dbdebe1fe5e6b6
LePetitTim <timothee.de-montety@makina-corpus.com>
2022-04-06 16:00:05 +0200

    Add default sql flatpages

---
+++ geotrek/flatpages/sql/post_90_defaults.sql
@@ -0,0 +1,13 @@
+-- FlatPage
+-----------
+ALTER TABLE flatpages_flatpage ALTER COLUMN title SET DEFAULT '';
+ALTER TABLE flatpages_flatpage ALTER COLUMN external_url SET DEFAULT '';
+-- content
+ALTER TABLE flatpages_flatpage ALTER COLUMN target SET DEFAULT 'all';
+-- source
+-- portal
+-- order
+ALTER TABLE flatpages_flatpage ALTER COLUMN date_insert SET DEFAULT now();
+ALTER TABLE flatpages_flatpage ALTER COLUMN date_update SET DEFAULT now();
+ALTER TABLE flatpages_flatpage ALTER COLUMN published SET DEFAULT FALSE;
+-- publication_date
