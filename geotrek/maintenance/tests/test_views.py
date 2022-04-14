be9686d7369b0c6e900799e961daf541dca9e555
J-E Castagnede <j.e.castagnede@gmail.com>
2022-04-14 16:04:22 +0200

    use srid instead of -changing- name

--- geotrek/maintenance/tests/test_views.py
+++ geotrek/maintenance/tests/test_views.py
@@ -619,7 +619,7 @@ class ExportTest(TranslationResetMixin, TestCase):
         self.assertIn('MultiLineString', geom_types)
 
         for layer in layers.values():
-            self.assertEqual(layer.srs.name, 'RGF93_Lambert_93')
+            self.assertEqual(layer.srs.srid, 2154)
             self.assertCountEqual(layer.fields, [
                 'id', 'name', 'period', 'type', 'domain', 'constraint',
                 'global_cos', 'interventi', 'comments',
