c3ab0d52c99a5d3ae543f4a09334b630a27d9356
Chatewgne <chatewgne@gmail.com>
2022-04-14 16:06:28 +0200

    Cypress - Test filed email

--- cypress/integration/nav_reports_workflow.js
+++ cypress/integration/nav_reports_workflow.js
@@ -18,11 +18,7 @@ describe('Nav reports workflow', () => {
   })
 
   it('Handles resolved intervention reports', () => {
-    cy.visit('http://localhost:8000/report/list')
-    cy.get("a[title='test@resolved.fr']").should('have.attr', 'href')
-      .then((href) => {
-        cy.visit(href)
-      })
+    cy.visit('http://localhost:8000/report/12/')
     cy.get(".btn.btn-primary.ml-auto").click()
     // Cannot use selectors for sentinel and supervisor messages yet
     cy.get("#id_message_sentinel_predefined").should("not.be.visible")
@@ -42,9 +38,44 @@ describe('Nav reports workflow', () => {
     cy.get("#id_message_sentinel").should("have.value", "")
     // Change selected status back to initial one
     cy.get("#id_status").select("1")
-    // Cannot use selectors for sentinel and supervisor messages
+    // Cannot use selectors for sentinel and supervisor messages anymore
     cy.get("#id_message_sentinel_predefined").should("not.be.visible")
     cy.get("#id_message_sentinel").should("not.be.visible")
     cy.get("#id_message_supervisor").should("not.be.visible")
   })
+
+
+
+  it('Handles filed reports', () => {
+    cy.visit('http://localhost:8000/report/13/')
+    cy.get(".btn.btn-primary.ml-auto").click()
+    // Cannot use selectors for sentinel and supervisor messages yet
+    cy.get("#id_message_sentinel_predefined").should("not.be.visible")
+    cy.get("#id_message_sentinel").should("not.be.visible")
+    cy.get("#id_message_supervisor").should("not.be.visible")
+    // Change assigned user
+    cy.get("#id_assigned_user").select("5")
+    // Can use selectors for sentinel and supervisor messages
+    cy.get("#id_message_sentinel_predefined").should("be.visible")
+    cy.get("#id_message_sentinel").should("be.visible")
+    cy.get("#id_message_supervisor").should("be.visible")
+    // Select a predefined email
+    cy.get("#id_message_sentinel_predefined").select("2")
+    cy.get("#id_message_sentinel").should("have.value", "Faire attention a la marche")
+    cy.get("#id_message_supervisor").should("have.value", "Faire attention a la marche")
+    // Select another predefined email
+    cy.get("#id_message_sentinel_predefined").select("3")
+    cy.get("#id_message_sentinel").should("have.value", "Ce probleme n'en sera bientot plus un")
+    cy.get("#id_message_supervisor").should("have.value", "Ce probleme n'en sera bientot plus un")
+    // Remove predefined email
+    cy.get("#id_message_sentinel_predefined").select("")
+    cy.get("#id_message_sentinel").should("have.value", "")
+    // Change assigned user back
+    cy.get("#id_assigned_user").select("")
+    // Cannot use selectors for sentinel and supervisor messages anymore
+    cy.get("#id_message_sentinel_predefined").should("not.be.visible")
+    cy.get("#id_message_sentinel").should("not.be.visible")
+    cy.get("#id_message_supervisor").should("not.be.visible")
+  })
+
 })
