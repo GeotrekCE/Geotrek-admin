af86d595cfde52c39357c13e6beb785d11739147
J-E Castagnede <j.e.castagnede@gmail.com>
2022-05-20 14:55:38 +0200

    convert land models geojson

--- geotrek/land/serializers.py
+++ geotrek/land/serializers.py
@@ -14,6 +14,15 @@ class LandEdgeSerializer(DynamicFieldsMixin, serializers.ModelSerializer):
         fields = "__all__"
 
 
+class LandEdgeGeojsonSerializer(MapentityGeojsonModelSerializer):
+    color_index = serializers.IntegerField()
+    name = serializers.CharField()
+
+    class Meta(MapentityGeojsonModelSerializer.Meta):
+        model = LandEdge
+        fields = ["id", "name", "draft"]
+
+
 class PhysicalEdgeSerializer(DynamicFieldsMixin, serializers.ModelSerializer):
     physical_type = serializers.CharField(source='physical_type_display')
     length = serializers.FloatField(source='length_display')
@@ -25,7 +34,7 @@ class PhysicalEdgeSerializer(DynamicFieldsMixin, serializers.ModelSerializer):
 
 class PhysicalEdgeGeojsonSerializer(MapentityGeojsonModelSerializer):
     color_index = serializers.IntegerField()
-    name = serializers.CharField(source='name')
+    name = serializers.CharField()
 
     class Meta(MapentityGeojsonModelSerializer.Meta):
         model = PhysicalEdge
@@ -41,6 +50,15 @@ class CompetenceEdgeSerializer(DynamicFieldsMixin, serializers.ModelSerializer):
         fields = "__all__"
 
 
+class CompetenceEdgeGeojsonSerializer(MapentityGeojsonModelSerializer):
+    color_index = serializers.IntegerField()
+    name = serializers.CharField()
+
+    class Meta(MapentityGeojsonModelSerializer.Meta):
+        model = CompetenceEdge
+        fields = ["id", "color_index", "name"]
+
+
 class SignageManagementEdgeSerializer(DynamicFieldsMixin, serializers.ModelSerializer):
     organization = serializers.CharField(source='organization_display')
     length = serializers.FloatField(source='length_display')
@@ -50,6 +68,15 @@ class SignageManagementEdgeSerializer(DynamicFieldsMixin, serializers.ModelSeria
         fields = "__all__"
 
 
+class SignageManagementEdgeGeojsonSerializer(MapentityGeojsonModelSerializer):
+    color_index = serializers.IntegerField()
+    name = serializers.CharField()
+
+    class Meta(MapentityGeojsonModelSerializer.Meta):
+        model = SignageManagementEdge
+        fields = ["id", "color_index", "name"]
+
+
 class WorkManagementEdgeSerializer(DynamicFieldsMixin, serializers.ModelSerializer):
     organization = serializers.CharField(source='organization_display')
     length = serializers.FloatField(source='length_display')
@@ -57,3 +84,12 @@ class WorkManagementEdgeSerializer(DynamicFieldsMixin, serializers.ModelSerializ
     class Meta:
         model = WorkManagementEdge
         fields = "__all__"
+
+
+class WorkManagementEdgeGeojsonSerializer(MapentityGeojsonModelSerializer):
+    color_index = serializers.IntegerField()
+    name = serializers.CharField()
+
+    class Meta(MapentityGeojsonModelSerializer.Meta):
+        model = WorkManagementEdge
+        fields = ["id", "color_index", "name"]
