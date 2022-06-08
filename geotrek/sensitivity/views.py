54acc03b5be3577e74c5ee622fe0ec9855f7ef54
J-E Castagnede <j.e.castagnede@gmail.com>
2022-06-08 11:42:21 +0200

    pep8

--- geotrek/sensitivity/views.py
+++ geotrek/sensitivity/views.py
@@ -13,7 +13,6 @@ from rest_framework import permissions as rest_permissions, viewsets
 
 from geotrek.api.v2.functions import Buffer, Area
 from geotrek.authent.decorators import same_structure_required
-from geotrek.common.mixins.api import APIViewSet
 from geotrek.common.functions import GeometryType
 from geotrek.common.mixins.api import APIViewSet
 from geotrek.common.mixins.views import CustomColumnsMixin
