b3eb1b8a921aa766c9ae1b92912483f6f590e92e
Chatewgne <chatewgne@gmail.com>
2022-07-21 12:59:40 +0200

    Copy form behaviour from sentinel messaging to administrators messaging

--- geotrek/feedback/static/feedback/workflow.js
+++ geotrek/feedback/static/feedback/workflow.js
@@ -1,5 +1,6 @@
 $(window).on('entity:view:add entity:view:update', function (e, data) {
     $('#div_id_message_sentinel').prop('hidden', true);
+    $('#div_id_message_administrators').prop('hidden', true);
     $('#div_id_message_sentinel_predefined').prop('hidden', true);
     $('#div_id_message_supervisor').prop('hidden', true);
     $('#div_id_uses_timers').prop('hidden', true);
@@ -20,6 +21,7 @@ function display_message_fields_on_status_change() {
     var selected = $('#id_status').val() || null;
     do_display = ((status_ids_and_colors[selected]['id'] == "solved") || (status_ids_and_colors[selected]['id'] == "classified"))
     $('#div_id_message_sentinel').prop('hidden', !do_display);
+    $('#div_id_message_administrators').prop('hidden', !do_display);
     $('#div_id_message_sentinel_predefined').prop('hidden', !do_display);
     // Prevent assigning and classifying at the same time - or rejecting and assigning
     if (status_ids_and_colors[selected]['id'] == "classified") {
@@ -37,6 +39,7 @@ function display_message_fields_on_status_change() {
 function display_message_fields_on_supervisor_change() {
     var selected = $('#id_assigned_user').val() || null;
     $('#div_id_message_sentinel').prop('hidden', (selected == null));
+    $('#div_id_message_administrators').prop('hidden', (selected == null));
     $('#div_id_message_sentinel_predefined').prop('hidden', (selected == null));
     $('#div_id_message_supervisor').prop('hidden', (selected == null));
     $('#div_id_uses_timers').prop('hidden', (selected == null));
@@ -50,11 +53,13 @@ function display_predefined_email_in_email_field() {
     if (selected == null) {
         $('#id_message_supervisor').val("");
         $('#id_message_sentinel').val("");
+        $('#id_message_administrators').val("");
     } else {
         text = predefined_emails[selected]["text"];
         text = text.replace(/##supervisor##/g, resolved_intervention_info["username"]);
         text = text.replace(/##intervention_date##/g, resolved_intervention_info["date"]);
         $('#id_message_supervisor').val(text);
         $('#id_message_sentinel').val(text);
+        $('#id_message_administrators').val(text);
     }
 }
