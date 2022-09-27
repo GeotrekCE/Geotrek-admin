937e9338bed254a96af19ee874ad2c556189000e
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2022-09-27 11:00:30 +0200

    add cypress test

---
+++ cypress/integration/nav_tourism.js
@@ -0,0 +1,41 @@
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false
+})
+
+describe('Create tourism event', () => {
+  before(() => {
+    const username = 'admin'
+    const password = 'admin'
+
+    cy.loginByCSRF(username, password)
+      .then((resp) => {
+         expect(resp.status).to.eq(200)
+      })
+  })
+
+  beforeEach(() => {
+    cy.setCookie('django_language', 'en');
+    Cypress.Cookies.preserveOnce('sessionid', 'csrftoken');
+  });
+
+  it('Should dynamicly show/hidde participant number', () => {
+    cy.visit('/touristicevent/add/');
+    // check initially hidden
+    cy.get('#div_id_participant_number').should("not.be.visible");
+
+    // check toggle hide/show
+    cy.get('#id_bookable').check({force: true});
+    cy.get('#div_id_participant_number').should("be.visible");
+    cy.get('#id_bookable').uncheck({force: true});
+    cy.get('#div_id_participant_number').should("not.be.visible");
+
+
+
+
+
+
+  })
+
+})
