251e8e5f1c39b27904e2d93b14fc5c0b39c5adec
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2022-09-29 16:56:49 +0200

    make place blankable

--- geotrek/tourism/models.py
+++ geotrek/tourism/models.py
@@ -480,7 +480,7 @@ class TouristicEvent(ZoningPropertiesMixin, AddPropertyMixin, PublishableMixin, 
     cancelled = models.BooleanField(default=False, verbose_name=_("Cancelled"), help_text=_("Boolean indicating if Event is cancelled"))
     cancellation_reason = models.ForeignKey(CancellationReason, verbose_name=_("Cancellation reason"), related_name="touristic_events", null=True, blank=True, on_delete=models.PROTECT)
     objects = TouristicEventManager()
-    place = models.ForeignKey(TouristicEventPlace, related_name="touristicevents", verbose_name=_("Place"), on_delete=models.PROTECT, null=True)
+    place = models.ForeignKey(TouristicEventPlace, related_name="touristicevents", verbose_name=_("Place"), on_delete=models.PROTECT, null=True, blank=True)
     id_prefix = 'E'
 
     class Meta:
