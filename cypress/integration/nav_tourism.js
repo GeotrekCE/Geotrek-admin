7dc5316e34456d81b48e1b6e87589a18b9f2bb53
Chatewgne <chatewgne@gmail.com>
2022-09-30 13:43:57 +0200

    Revert hide capacity in form on uncheck bookable

--- cypress/integration/nav_tourism.js
+++ cypress/integration/nav_tourism.js
@@ -14,18 +14,8 @@ describe('Create tourism event', () => {
     Cypress.Cookies.preserveOnce('sessionid', 'csrftoken');
   });
 
-  it('Should dynamicly show/hidde participant number', () => {
-    cy.visit('/touristicevent/add/').get("#modelfields").scrollTo('bottom');
-    // check initially hidden
-    cy.get('#div_id_capacity').should("not.be.visible");
-    // check toggle hide/show
-    cy.get('#id_bookable').check({ force: true });
-    cy.get('#div_id_capacity').should("be.visible");
-    cy.get('#id_bookable').uncheck({ force: true });
-    cy.get('#div_id_capacity').should("not.be.visible");
-  })
-
   it('Should dynamicly show/hidde cancellation reason', () => {
+    cy.visit('/touristicevent/add/').get("#modelfields").scrollTo('bottom');
     // check initially hidden
     cy.get('#div_id_cancellation_reason').should("not.be.visible");
     // check toggle hide/show
