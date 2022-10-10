55e2e04a6f99d0c403006f32495fd1c90f26dee8
Marc-Antoine Dupré <marc-antoine.dupre@makina-corpus.com>
2022-10-10 16:37:36 +0200

    Refactor Blade form classes
    No change in behavior, I only:
    - have both `BladeForm` classes (DS/NDS) subclass `BaseBladeForm`,
    - move all identical code to `BaseBladeForm`,
    - leave only differences in subclasses.

--- geotrek/signage/forms.py
+++ geotrek/signage/forms.py
@@ -38,7 +38,6 @@ LineFormset = inlineformset_factory(Blade, Line, form=LineForm, extra=1)
 
 
 class BaseBladeForm(CommonForm):
-    topology = TopologyField(label="")
     geomfields = ['topology']
 
     fieldslayout = [
@@ -62,12 +61,14 @@ class BaseBladeForm(CommonForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
         self.helper.form_tag = False
         if not self.instance.pk:
             self.signage = kwargs.get('initial', {}).get('signage')
             self.helper.form_action += '?signage=%s' % self.signage.pk
         else:
             self.signage = self.instance.signage
+
         value_max = self.signage.blade_set.all().aggregate(max=Max('number'))['max']
         if settings.BLADE_CODE_TYPE == int:
             if not value_max:
@@ -100,37 +101,12 @@ class BaseBladeForm(CommonForm):
 
 
 if settings.TREKKING_TOPOLOGY_ENABLED:
-    class BladeForm(CommonForm):
+    class BladeForm(BaseBladeForm):
         topology = TopologyField(label="")
-        geomfields = ['topology']
-
-        fieldslayout = [
-            Div(
-                'number',
-                'direction',
-                'type',
-                'condition',
-                'color',
-                Fieldset(_('Lines')),
-            )
-        ] if settings.LINE_ENABLED else [
-            Div(
-                'number',
-                'direction',
-                'type',
-                'condition',
-                'color',
-            )
-        ]
 
         def __init__(self, *args, **kwargs):
             super().__init__(*args, **kwargs)
-            self.helper.form_tag = False
-            if not self.instance.pk:
-                self.signage = kwargs.get('initial', {}).get('signage')
-                self.helper.form_action += '?signage=%s' % self.signage.pk
-            else:
-                self.signage = self.instance.signage
+
             self.fields['topology'].initial = self.signage
             self.fields['topology'].widget.modifiable = True
             self.fields['topology'].label = '%s%s %s' % (
@@ -138,43 +114,14 @@ if settings.TREKKING_TOPOLOGY_ENABLED:
                 _("On %s") % _(self.signage.kind.lower()),
                 '<a href="%s">%s</a>' % (self.signage.get_detail_url(), str(self.signage))
             )
-            value_max = self.signage.blade_set.all().aggregate(max=Max('number'))['max']
-            if settings.BLADE_CODE_TYPE == int:
-                if not value_max:
-                    self.fields['number'].initial = "1"
-                elif value_max.isdigit():
-                    self.fields['number'].initial = str(int(value_max) + 1)
-            elif settings.BLADE_CODE_TYPE is str:
-                if not value_max:
-                    self.fields['number'].initial = "A"
-                elif len(value_max) == 1 and "A" <= value_max[0] < "Z":
-                    self.fields['number'].initial = chr(ord(value_max[0]) + 1)
 
-        def save(self, *args, **kwargs):
-            self.instance.set_topology(self.signage)
-            self.instance.signage = self.signage
-            return super(CommonForm, self).save(*args, **kwargs)
-
-        def clean_number(self):
-            blades = self.signage.blade_set.all()
-            if self.instance.pk:
-                blades = blades.exclude(number=self.instance.number)
-            already_used = ', '.join([str(number) for number in blades.values_list('number', flat=True)])
-            if blades.filter(number=self.cleaned_data['number']).exists():
-                raise forms.ValidationError(_("Number already exists, numbers already used : %s" % already_used))
-            return self.cleaned_data['number']
-
-        class Meta:
-            model = Blade
-            fields = ['id', 'number', 'direction', 'type', 'condition', 'color']
 else:
     class BladeForm(BaseBladeForm):
-        geomfields = ['topology']
         topology = GeometryField(label="")
 
         def __init__(self, *args, **kwargs):
-
             super().__init__(*args, **kwargs)
+
             self.fields['topology'].initial = self.signage.geom
             self.fields['topology'].widget = LeafletWidget(attrs={'geom_type': 'POINT'})
             self.fields['topology'].widget.modifiable = False
@@ -183,7 +130,7 @@ else:
                 _("On %s") % _(self.signage.kind.lower()),
                 '<a href="%s">%s</a>' % (self.signage.get_detail_url(), str(self.signage))
             )
-            self.helper.form_tag = False
+
 
 if settings.TREKKING_TOPOLOGY_ENABLED:
     class BaseSignageForm(BaseInfrastructureForm):
