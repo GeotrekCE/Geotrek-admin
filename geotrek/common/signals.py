58bfc3dfc0b0c8fe71446f3ac85b0b76b0a21a7d
J-E Castagnede <j.e.castagnede@gmail.com>
2022-11-16 15:49:18 +0100

    fix signals

--- geotrek/common/signals.py
+++ geotrek/common/signals.py
@@ -12,5 +12,6 @@ from geotrek.common.models import Attachment, AccessibilityAttachment
 def update_content_object_date_update(sender, instance, *args, **kwargs):
     """ after each creation / edition / deletion, increment date_updated to avoid object cache """
     content_object = instance.content_object
-    content_object.date_update = now()
-    content_object.save(update_fields=['date_update'])
+    if content_object and hasattr(content_object, 'date_update'):
+        content_object.date_update = now()
+        content_object.save(update_fields=['date_update'])
