8700023bbf8dca5f37649053e41012671d5a6e2c
J-E Castagnede <j.e.castagnede@gmail.com>
2022-11-18 16:23:58 +0100

    cache list api v2 theme endpoint

--- geotrek/api/v2/decorators.py
+++ geotrek/api/v2/decorators.py
@@ -14,3 +14,18 @@ class APIV2CacheResponseDetail(BaseCacheResponse):
 
 
 cache_response_detail = APIV2CacheResponseDetail
+
+
+class APIV2CacheResponseList(BaseCacheResponse):
+    def __init__(self,
+                 timeout='object_cache_timeout',
+                 key_func='object_cache_key_func',
+                 cache='api_v2',
+                 cache_errors=None):
+        super().__init__(timeout='list_cache_timeout',
+                         key_func='list_cache_key_func',
+                         cache='api_v2',
+                         cache_errors=None)
+
+
+cache_response_list = APIV2CacheResponseList
