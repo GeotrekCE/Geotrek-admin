07551febe933502a5662a5eca6f9efe1d74c4bcc
Chatewgne <chatewgne@gmail.com>
2022-11-22 11:50:08 +0100

    Fix merge

--- geotrek/common/migrations/0028_hdviewpoint.py
+++ geotrek/common/migrations/0028_hdviewpoint.py
@@ -10,7 +10,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('contenttypes', '0002_remove_content_type_name'),
-        ('common', '0025_auto_20220425_1550'),
+        ('common', '0027_flush_cache_1234'),
     ]
 
     operations = [
