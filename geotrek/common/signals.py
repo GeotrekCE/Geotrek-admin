bcc508f75e0df3253e32717528014ae2cd0f2af3
Chatewgne <chatewgne@gmail.com>
2022-12-02 15:16:30 +0100

    Update date_update on HD View Points related objects when modified

--- geotrek/common/signals.py
+++ geotrek/common/signals.py
@@ -2,13 +2,15 @@ from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from django.utils.timezone import now
 
-from geotrek.common.models import Attachment, AccessibilityAttachment
+from geotrek.common.models import Attachment, AccessibilityAttachment, HDViewPoint
 
 
 @receiver(post_save, sender=Attachment)
 @receiver(post_save, sender=AccessibilityAttachment)
+@receiver(post_save, sender=HDViewPoint)
 @receiver(post_delete, sender=Attachment)
 @receiver(post_delete, sender=AccessibilityAttachment)
+@receiver(post_delete, sender=HDViewPoint)
 def update_content_object_date_update(sender, instance, *args, **kwargs):
     """ after each creation / edition / deletion, increment date_updated to avoid object cache """
     content_object = instance.content_object
