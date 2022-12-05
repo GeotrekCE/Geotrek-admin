3e18a82d88a44a434852797e228ea80bbe871171
Chatewgne <chatewgne@gmail.com>
2022-12-05 15:11:12 +0100

    Add admin list view for HD view points

--- geotrek/common/admin.py
+++ geotrek/common/admin.py
@@ -104,6 +104,41 @@ class LabelAdmin(TabbedTranslationAdmin):
     search_fields = ('name', )
 
 
+
+
+class HDViewPointAdmin(admin.ModelAdmin):
+    date_hierarchy = 'date_update'
+    search_fields = ('title', 'legend', 'author', 'object_id')
+    list_display = ('update_link', 'legend', 'author', 'related_object_link', 'content_type', 'license')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.list_display_links = None
+
+    def has_add_permission(self, request):
+        """ Do not add from Adminsite.
+        """
+        return False
+
+    def update_link(self, obj):
+        """Returns content object link"""
+        return format_html(
+            '<a data-pk="{}" href="{}" >{}</a>',
+            obj.pk, obj.full_url, obj.title
+        )
+
+    def related_object_link(self, obj):
+        """Returns content object link"""
+        content_url = obj.content_object.get_detail_url()
+        return format_html(
+            '<a data-pk="{}" href="{}" >{}</a>',
+            obj.object_id, content_url, str(obj.content_object)
+        )
+
+    related_object_link.short_description = _('Related to')
+    update_link.short_description = _('Title')
+
+
 admin.site.register(common_models.Organism, OrganismAdmin)
 admin.site.register(common_models.Attachment, AttachmentAdmin)
 admin.site.register(common_models.FileType, FileTypeAdmin)
@@ -113,3 +148,4 @@ admin.site.register(common_models.TargetPortal, TargetPortalAdmin)
 admin.site.register(common_models.ReservationSystem, ReservationSystemAdmin)
 admin.site.register(common_models.Label, LabelAdmin)
 admin.site.register(common_models.License, LicenseAdmin)
+admin.site.register(common_models.HDViewPoint, HDViewPointAdmin)
