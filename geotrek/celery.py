a9a7262a32b453f55ae2f89bf6ae6ad6c9e19637
jecisnotdead <124135570+jecisnotdead@users.noreply.github.com>
2023-02-01 09:10:52 +0100

    Update celery.py

--- geotrek/celery.py
+++ geotrek/celery.py
@@ -1,5 +1,6 @@
 from celery import Celery
 import os
+import sys
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'geotrek.settings')
