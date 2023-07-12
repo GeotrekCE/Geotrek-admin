3ad03da42496adba375ca329462b7391ff1751c8
PON <PO.ndima@gmail>
2023-07-12 16:51:30 +0200

    Fixed test

--- geotrek/api/tests/test_v2.py
+++ geotrek/api/tests/test_v2.py
@@ -77,7 +77,8 @@ TREK_PROPERTIES_GEOJSON_STRUCTURE = sorted([
     'themes', 'update_datetime', 'url', 'uuid', 'view_points', 'web_links'
 ])
 
-PATH_PROPERTIES_GEOJSON_STRUCTURE = sorted(['comments', 'length_2d', 'length_3d', 'name', 'provider', 'url', 'uuid'])
+PATH_PROPERTIES_GEOJSON_STRUCTURE = sorted(['id', 'name', 'comments', 'departure', 'arrival', 'length_2d', 'length_3d',
+                                            'networks', 'usages', 'stake', 'provider', 'url', 'uuid'])
 
 TOUR_PROPERTIES_GEOJSON_STRUCTURE = sorted(TREK_PROPERTIES_GEOJSON_STRUCTURE + ['count_children', 'steps'])
 
