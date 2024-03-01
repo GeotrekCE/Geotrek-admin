809dfb99fa3ac2fecd5d94da7e2417f7abfb8548
Chatewgne <chatewgne@gmail.com>
2024-03-01 15:29:59 +0100

    ✅ [TEST] Add tests for CirkwiParser (refs #3947)

--- geotrek/cirkwi/parsers.py
+++ geotrek/cirkwi/parsers.py
@@ -10,6 +10,7 @@ from geotrek.trekking.parsers import ApidaeTrekParser
 
 class CirkwiParser(AttachmentParserMixin, XmlParser):
     eid = 'eid'
+    default_language = settings.MODELTRANSLATION_DEFAULT_LANGUAGE
     field_options = {
         "geom": {"required": True},
         "name": {"required": True},
@@ -19,18 +20,19 @@ class CirkwiParser(AttachmentParserMixin, XmlParser):
     }
 
     def get_part(self, dst, src, val):
-        if val is None:
-            return None
         # Recursively extract XML attributes
         if '@@' in src and src[:2] != '@@':
             part, attrib = src.split('@@', 1)
             return self.get_part(dst, f"@@{attrib}", val.find(part))
         # Extract XML attributes
-        elif src[:2] == '@@':
+        elif src.startswith('@@'):
             return val.attrib[src[2:]]
         else:
+            # Replace language attribute
+            if "'<LANG>'" in src:
+                src = src.replace("<LANG>", self.default_language)
             # Return a list of XML elements
-            if src[-2:] == '/*':
+            if src.endswith('/*'):
                 return val.findall(src[:-2])
             # Return inner text if XML element exists
             if val.find(src) is None:
@@ -57,24 +59,22 @@ class CirkwiParser(AttachmentParserMixin, XmlParser):
         return attachments
 
 
-class CirkwiTrekParserFr(CirkwiParser):
-    default_language = 'fr'
+class CirkwiTrekParser(CirkwiParser):
     model = Trek
-    url = "https://demo-admin.geotrek.fr/static/circkwi.xml"
     results_path = 'circuit'
     fields = {
         "eid": "@@id_circuit",
-        "name": f"informations/information[@langue='{default_language}']/titre",
-        "description": (f"informations/information[@langue='{default_language}']/description",
-                        f"informations/information[@langue='{default_language}']/informations_complementaires/information_complementaire/titre",
-                        f"informations/information[@langue='{default_language}']/informations_complementaires/information_complementaire/description"),
+        "name": "informations/information[@langue='<LANG>']/titre",
+        "description": ("informations/information[@langue='<LANG>']/description",
+                        "informations/information[@langue='<LANG>']/informations_complementaires/information_complementaire/titre",
+                        "informations/information[@langue='<LANG>']/informations_complementaires/information_complementaire/description"),
         "geom": "sens_circuit/fichier_trace@@url",
     }
     constant_fields = {
         'practice': "Pédestre"
     }
     non_fields = {
-        'attachments': f"informations/information[@langue='{default_language}']/medias/images/image/*"
+        'attachments': "informations/information[@langue='<LANG>']/medias/images/image/*"
     }
     natural_keys = {
         'practice': 'name'
@@ -85,26 +85,25 @@ class CirkwiTrekParserFr(CirkwiParser):
         return ApidaeTrekParser._get_geom_from_gpx(response.content)
 
 
-class CirkwiTouristicContentParserFr(CirkwiParser):
-    default_language = 'fr'
+class CirkwiTouristicContentParser(CirkwiParser):
     model = TouristicContent
-    url = "https://demo-admin.geotrek.fr/static/poicirkwi.xml"
+    default_language = settings.MODELTRANSLATION_DEFAULT_LANGUAGE
     results_path = 'poi'
     fields = {
         "eid": "@@id_poi",
-        "name": f"informations/information[@langue='{default_language}']/titre",
-        "description": (f"informations/information[@langue='{default_language}']/description",
-                        f"informations/information[@langue='{default_language}']/informations_complementaires/information_complementaire/titre",
-                        f"informations/information[@langue='{default_language}']/informations_complementaires/information_complementaire/description"),
+        "name": "informations/information[@langue='<LANG>']/titre",
+        "description": ("informations/information[@langue='<LANG>']/description",
+                        "informations/information[@langue='<LANG>']/informations_complementaires/information_complementaire/titre",
+                        "informations/information[@langue='<LANG>']/informations_complementaires/information_complementaire/description"),
         "geom": ("adresse/position/lng", "adresse/position/lat"),
-        "practical_info": ("adresse/num", "adresse/rue", "adresse/cp", "adresse/ville", f"informations/information[@langue='{default_language}']/informations_complementaires/information_complementaire/*"),
+        "practical_info": ("adresse/num", "adresse/rue", "adresse/cp", "adresse/ville", "informations/information[@langue='<LANG>']/informations_complementaires/information_complementaire/*"),
         "category": "categories/categorie/*",
     }
     m2m_fields = {
         "type1": "categories/categorie/*",
     }
     non_fields = {
-        'attachments': f"informations/information[@langue='{default_language}']/medias/images/image/*"
+        'attachments': "informations/information[@langue='<LANG>']/medias/images/image/*"
     }
     field_options = {
         "geom": {"required": True},
@@ -129,13 +128,9 @@ class CirkwiTouristicContentParserFr(CirkwiParser):
         for other_info in other_infos:
             infos += f"<br><strong>{other_info.find('titre').text} : </strong><br>"
             infos += f"{other_info.find('description').text}<br>"
-        if not infos:
-            return None
         return infos
 
     def filter_category(self, src, val):
-        if val is None:
-            return None
         name = val[0].attrib["nom"]
         return self.apply_filter('category', src, name)
 
@@ -143,7 +138,7 @@ class CirkwiTouristicContentParserFr(CirkwiParser):
         if val is None or len(val) < 2:
             return None
         label = val[1].attrib["nom"]
-        if self.field_options["type1"]["create"]:
+        if self.field_options.get("type1", {}).get("create", False):
             type1, __ = TouristicContentType1.objects.get_or_create(category=self.obj.category, label=label)
         else:
             try:
@@ -152,6 +147,7 @@ class CirkwiTouristicContentParserFr(CirkwiParser):
                 self.add_warning(
                     _("Type 1 '{type}' does not exist for category '{cat}'. Please add it").format(
                         type=label, cat=self.obj.category.label))
+                return []
         return [type1]
 
     def filter_geom(self, src, val):
