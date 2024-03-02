846896550225fcbf89de3c94aae869023940b4e5
Marc-Antoine Dupré <marc-antoine.dupre@makina-corpus.com>
2024-03-02 12:24:17 +0100

    [wip] corrige compatibilité entre treebeard's MoveNodeForm et modeltranslation's TabbedTranslationAdmin

--- geotrek/flatpages/admin.py
+++ geotrek/flatpages/admin.py
@@ -27,12 +27,23 @@ class FlatPagesAdminForm(ModelForm):
 
 
 # class FlatPagesAdmin(TabbedTranslationAdmin):
-class FlatPagesAdmin(TreeAdmin):
+class FlatPagesAdmin(TreeAdmin, TabbedTranslationAdmin):
     list_display = ('title', 'published', 'publication_date', 'portals', )
     list_filter = ('published', )
     search_fields = ('title', 'content')
-    form = movenodeform_factory(flatpages_models.FlatPage, FlatPageForm)
+    form = movenodeform_factory(flatpages_models.FlatPage, form=FlatPageForm)
     # form = FlatPagesAdminForm
+
+    # This is an alternative to overriding super(TabbedTranslationAdmin)._get_declared_fields
+    # Manual specification of fields to display treebeard's `_position` and `_ref_node_id` field.
+    # fields = (
+    #     'title',
+    #     'published',
+    #     'portals',
+    #     'content',
+    #     '_position',
+    #     '_ref_node_id',
+    # )
 
     # def add_view(self, request, form_url='', extra_context=None):
     #     return FlatPageCreate.as_view()(request)
@@ -43,6 +54,28 @@ class FlatPagesAdmin(TreeAdmin):
     def portals(self, obj):
         return ', '.join([portal.name for portal in obj.portal.all()])
     portals.short_description = _("Portals")
+
+    # Override modeltranslation's TranslationAdmin._get_declared_fieldsets
+    # The original method does not preserve MoveNodeForm's declared FormFields (aka class attributes) when
+    # dynamically creating a Form class for the Admin add and change views.
+    def _get_declared_fieldsets(self, request, obj=None):
+        # Take custom modelform fields option into account
+        # mdu : use self.form.base_fields rather than self.form._meta.fields
+        # The latter does not have the FormFields declared as class attributes, hence breaking MoveNodeForm.
+        if not self.fields and hasattr(self.form, 'base_fields') and self.form.base_fields:
+            self.fields = self.form.base_fields.keys()
+
+        # takes into account non-standard add_fieldsets attribute used by UserAdmin
+        fieldsets = (
+            self.add_fieldsets
+            if getattr(self, 'add_fieldsets', None) and obj is None
+            else self.fieldsets
+        )
+        if fieldsets:
+            return self._patch_fieldsets(fieldsets)
+        elif self.fields:
+            return [(None, {'fields': self.replace_orig_field(self.get_fields(request, obj))})]
+        return None
 
 
 class MyListFilter(admin.filters.SimpleListFilter):
