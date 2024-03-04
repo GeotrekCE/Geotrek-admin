b5923ece58f084da38313cf91a5ca74b3d52181b
Marc-Antoine Dupré <marc-antoine.dupre@makina-corpus.com>
2024-03-04 07:42:38 +0100

    [wip] nettoyage

--- geotrek/flatpages/admin.py
+++ geotrek/flatpages/admin.py
@@ -6,7 +6,6 @@ from treebeard.forms import movenodeform_factory
 
 from geotrek.flatpages import models as flatpages_models
 from geotrek.flatpages.forms import FlatPageForm
-from geotrek.flatpages.views import FlatPageCreate, FlatPageUpdate, MenuItemCreate
 
 if 'modeltranslation' in settings.INSTALLED_APPS:
     from modeltranslation.admin import TabbedTranslationAdmin
@@ -14,25 +13,11 @@ else:
     from django.contrib.admin import ModelAdmin as TabbedTranslationAdmin
 
 
-from tinymce.widgets import TinyMCE, AdminTinyMCE
-from django.forms import ModelForm
-from django import forms
-
-class FlatPagesAdminForm(ModelForm):
-
-    content_fr = forms.CharField(widget=TinyMCE(attrs={"class": "vLargeTextField mt mt-field-content-fr mt-default"}))
-    content_en = forms.CharField(widget=TinyMCE(attrs={"class": "vLargeTextField mt mt-field-content-fr mt-default"}))
-    content_es = forms.CharField(widget=TinyMCE(attrs={"class": "vLargeTextField mt mt-field-content-fr mt-default"}))
-    content_it = forms.CharField(widget=TinyMCE(attrs={"class": "vLargeTextField mt mt-field-content-fr mt-default"}))
-
-
-# class FlatPagesAdmin(TabbedTranslationAdmin):
 class FlatPagesAdmin(TabbedTranslationAdmin, TreeAdmin):
     list_display = ('title', 'published', 'publication_date', 'portals', )
     list_filter = ('published', )
     search_fields = ('title', 'content')
     form = movenodeform_factory(flatpages_models.FlatPage, form=FlatPageForm)
-    # form = FlatPagesAdminForm
 
     # This is an alternative to overriding super(TabbedTranslationAdmin)._get_declared_fields
     # Manual specification of fields to display treebeard's `_position` and `_ref_node_id` field.
@@ -44,12 +29,6 @@ class FlatPagesAdmin(TabbedTranslationAdmin, TreeAdmin):
     #     '_position',
     #     '_ref_node_id',
     # )
-
-    # def add_view(self, request, form_url='', extra_context=None):
-    #     return FlatPageCreate.as_view()(request)
-    #
-    # def change_view(self, request, object_id, form_url='', extra_context=None):
-    #     return FlatPageUpdate.as_view()(request, pk=object_id)
 
     def portals(self, obj):
         return ', '.join([portal.name for portal in obj.portal.all()])
@@ -90,10 +69,9 @@ class MyListFilter(admin.filters.SimpleListFilter):
 
 
 # class MenuItemAdmin(TabbedTranslationAdmin, TreeAdmin): # diamond inheritance problem
-class MenuItemAdmin(TreeAdmin):
+class MenuItemAdmin(TabbedTranslationAdmin, TreeAdmin):
     list_display = ('label', )
     form = movenodeform_factory(flatpages_models.MenuItem)
-    # change_form_template = "flatpages/change_form.html"
     list_filter = (
         ("portals", admin.filters.RelatedOnlyFieldListFilter),
     )
