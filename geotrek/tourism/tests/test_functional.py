c8b1de5ba98550a999d49552e8d89574abc3d421
Jean-Etienne Castagnede <j.e.castagnede@gmail.com>
2024-03-06 08:40:08 +0000

    fix test

--- geotrek/tourism/tests/test_functional.py
+++ geotrek/tourism/tests/test_functional.py
@@ -368,7 +368,7 @@ class TouristicEventViewsTests(GeotrekAPITestCase, CommonTest):
         total_count = sum(map(attrgetter('count'), counts))
         self.assertEqual(event.participants_total, total_count)
         self.assertEqual(event.participants_total_verbose_name, "Number of participants")
-        with self.assertNumQueries(16):
+        with self.assertNumQueries(17):
             response = self.client.get(event.get_format_list_url())
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.get('Content-Type'), 'text/csv')
