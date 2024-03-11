926e0fa9f3ee39df3610f62ddd9407e5eedcc61d
J-E Castagnede <j.e.castagnede@gmail.com>
2024-03-11 17:29:38 +0100

    delete sync_rando command and views

--- geotrek/common/tests/factories.py
+++ geotrek/common/tests/factories.py
@@ -1,8 +1,4 @@
 import factory
-import os
-import tempfile
-from zipfile import ZipFile
-
 from geotrek.authent.tests.factories import UserFactory
 from geotrek.common.models import Attachment
 from geotrek.common.utils.testdata import (dummy_filefield_as_sequence,
@@ -10,40 +6,6 @@ from geotrek.common.utils.testdata import (dummy_filefield_as_sequence,
                                            get_dummy_uploaded_file)
 
 from .. import models
-
-from geotrek.common.management.commands.sync_rando import Command
-
-from django.conf import settings
-from django.test.client import RequestFactory
-
-
-class FakeSyncCommand(Command):
-    categories = '1'
-    verbosity = 2
-    host = 'localhost:8000'
-    secure = True
-    with_infrastructures = True
-    with_signages = True
-    with_events = True
-    rando_url = 'localhost:3000'
-
-    def __init__(self, portal='', source='', skip_dem=False, skip_pdf=False, skip_profile_png=False):
-        super().__init__(stdout=None, stderr=None, no_color=False, force_color=False)
-        self.dst_root = settings.TMP_DIR
-        self.temporary_directory = tempfile.TemporaryDirectory(dir=settings.VAR_DIR)
-        self.tmp_root = self.temporary_directory.name
-        zipname = os.path.join('zip', 'tiles', 'global.zip')
-        global_file = os.path.join(self.tmp_root, zipname)
-        dirname = os.path.dirname(global_file)
-        if not os.path.exists(dirname):
-            os.makedirs(dirname)
-        self.zipfile = ZipFile(os.path.join(self.tmp_root, 'zip', 'tiles', 'global.zip'), 'w')
-        self.factory = RequestFactory()
-        self.source = source
-        self.portal = portal
-        self.skip_dem = skip_dem
-        self.skip_pdf = skip_pdf
-        self.skip_profile_png = skip_profile_png
 
 
 class OrganismFactory(factory.django.DjangoModelFactory):
