e156ab2efcbf07026fa7e9e2db66e0b557809cbc
Marc-Antoine Dupré <marc-antoine.dupre@makina-corpus.com>
2024-03-14 17:35:35 +0100

    [wip] remplace FlatPageFactory avec _create_flatpage_and_menuitem pour vérifier mobile tests

--- geotrek/api/tests/test_mobile/test_api_mobile_flatpage.py
+++ geotrek/api/tests/test_mobile/test_api_mobile_flatpage.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.urls import reverse
 from django.test.testcases import TestCase
 from django.utils import translation
@@ -9,6 +10,90 @@ from geotrek.flatpages.models import FlatPage, FLATPAGES_TARGETS
 FLATPAGE_DETAIL_PROPERTIES_JSON_STRUCTURE = sorted([
     'id', 'title', 'external_url', 'content'
 ])
+
+
+def _create_flatpage_and_menuitem(**kwargs):
+    """Support function to convert all FlatPages created for the tests into the creation of
+    a couple FlatPage+MenuItem. The mobile API stays the same (only a FlatPage type is exposed) but
+    there are now 2 models under the hood.
+    """
+
+    translated_fields = [
+        "title",
+        "published",
+        "external_url",
+    ]
+
+    copied_fields = {
+        "title": "label",
+        "published": "published",
+        "portals": "portals",
+    }
+
+    moved_fields = {
+        "external_url": "link_url",
+        "target": "platform",
+    }
+
+    removed_fields = [
+        "order",
+    ]
+
+    # oldFlatPage.target -> MenuItem.platform
+    flatpage_target = kwargs.get("target")
+    external_url = kwargs.get("external_url")
+
+    # oldFlatPage.external_url -> MenuItem.link_url
+
+    # FlatPage.title +> MenuItem.label
+
+    # FlatPage.published +> MenuItem.published
+
+    # oldFlatPage.portal +> FlatPage.portals, MenuItem.portals
+
+    # oldFlatPage.order -> ordre des MenuItems dans l'arborescence ( utiliser add_child(root, "last-child") )
+
+    # Rules :
+    # if oldFlatPage.target is "hidden" -> do not create a MenuItem unless the oldFlatPage has a external_url defined
+    # if oldFlatPage has both external_url and content defined pick the content, drop external_url
+
+    page_kwargs = kwargs.copy()
+
+    for field in list(moved_fields.keys()) + removed_fields:
+        try:
+            del page_kwargs[field]
+        except KeyError:
+            pass
+
+    page = FlatPageFactory.create(**page_kwargs)
+
+    if flatpage_target == "hidden":
+        return page
+
+    menu_kwargs = {}
+
+    def tr_name(name, lang):
+        return f"{name}_{lang}"
+
+    menu_fields = {}
+    menu_fields.update(copied_fields)
+    menu_fields.update(moved_fields)
+
+    for src, dst in menu_fields.items():
+        if src in translated_fields:
+            for lang in settings.MODELTRANSLATION_LANGUAGES:
+                try:
+                    menu_kwargs[tr_name(dst, lang)] = kwargs[tr_name(src, lang)]
+                except KeyError:
+                    pass
+        else:
+            try:
+                menu_kwargs[dst] = kwargs[src]
+            except KeyError:
+                pass
+
+    MenuItemFactory.create(page=page, **menu_kwargs)
+    return page
 
 
 class FlatPageTest(TestCase):
@@ -44,7 +129,7 @@ class FlatPageTest(TestCase):
         self.assertEqual(json_response.get('title'), self.flatpage.title)
 
     def test_flatpage_detail_with_lang_param(self):
-        fp = FlatPageFactory(published_en=True, published_fr=True, title_fr="Bonjour", title_en="Hello")
+        fp = _create_flatpage_and_menuitem(published_en=True, published_fr=True, title_fr="Bonjour", title_en="Hello")
 
         resp = self.client.get(
             reverse('apimobile:flatpage-detail', args=(fp.pk, )),
@@ -63,8 +148,8 @@ class FlatPageTest(TestCase):
         self.assertEqual(title, "Hello")
 
     def test_flatpage_detail_with_lang_param(self):
-        fp = FlatPageFactory(published_en=True, published_fr=True, title_fr="Bonjour", title_en="Hello")
-        fp2 = FlatPageFactory(published_en=True, published_fr=True, title_fr="Au revoir", title_en="Goodbye")
+        fp = _create_flatpage_and_menuitem(published_en=True, published_fr=True, title_fr="Bonjour", title_en="Hello")
+        fp2 = _create_flatpage_and_menuitem(published_en=True, published_fr=True, title_fr="Au revoir", title_en="Goodbye")
 
         resp = self.client.get(reverse('apimobile:flatpage-list'), HTTP_ACCEPT_LANGUAGE='fr')
 
@@ -85,10 +170,10 @@ class FlatPageTest(TestCase):
     def test_flatpage_list_published_only_with_lang_param(self):
         FlatPage.objects.update(published_en=False, published_fr=False)
 
-        FlatPageFactory(published_en=False, published_fr=False)
-        fp1 = FlatPageFactory(published_en=True, published_fr=False)
-        fp2 = FlatPageFactory(published_en=False, published_fr=True)
-        fp3 = FlatPageFactory(published_en=True, published_fr=True)
+        _create_flatpage_and_menuitem(published_en=False, published_fr=False)
+        fp1 = _create_flatpage_and_menuitem(published_en=True, published_fr=False)
+        fp2 = _create_flatpage_and_menuitem(published_en=False, published_fr=True)
+        fp3 = _create_flatpage_and_menuitem(published_en=True, published_fr=True)
 
         resp = self.client.get(reverse('apimobile:flatpage-list'), HTTP_ACCEPT_LANGUAGE='fr')
 
@@ -109,8 +194,8 @@ class FlatPageTest(TestCase):
         self.assertIn(fp3.id, fp_ids)
 
     def test_flatpage_detail_published_only_with_lang_param(self):
-        fp1 = FlatPageFactory(published_en=True, published_fr=False)
-        fp2 = FlatPageFactory(published_en=False, published_fr=True)
+        fp1 = _create_flatpage_and_menuitem(published_en=True, published_fr=False)
+        fp2 = _create_flatpage_and_menuitem(published_en=False, published_fr=True)
 
         resp = self.client.get(
             reverse('apimobile:flatpage-detail', args=(fp1.pk, )),
@@ -127,10 +212,10 @@ class FlatPageTest(TestCase):
     def test_flatpage_detail_with_portal_filter(self):
         p1 = TargetPortalFactory()
         p2 = TargetPortalFactory()
-        fp0 = FlatPageFactory(published_fr=True, portals=None)
-        fp1 = FlatPageFactory(published_fr=True, portals=[p1])
-        fp2 = FlatPageFactory(published_fr=True, portals=[p2])
-        fp3 = FlatPageFactory(published_fr=True, portals=[p1, p2])
+        fp0 = _create_flatpage_and_menuitem(published_fr=True, portals=None)
+        fp1 = _create_flatpage_and_menuitem(published_fr=True, portals=[p1])
+        fp2 = _create_flatpage_and_menuitem(published_fr=True, portals=[p2])
+        fp3 = _create_flatpage_and_menuitem(published_fr=True, portals=[p1, p2])
 
         resp = self.client.get(
             reverse('apimobile:flatpage-detail', args=[fp0.pk]),
@@ -167,10 +252,10 @@ class FlatPageTest(TestCase):
         FlatPage.objects.update(published_fr=False)
         p1 = TargetPortalFactory()
         p2 = TargetPortalFactory()
-        fp0 = FlatPageFactory(published_fr=True, portals=None)
-        fp1 = FlatPageFactory(published_fr=True, portals=[p1])
-        fp2 = FlatPageFactory(published_fr=True, portals=[p2])
-        fp3 = FlatPageFactory(published_fr=True, portals=[p1, p2])
+        fp0 = _create_flatpage_and_menuitem(published_fr=True, portals=None)
+        fp1 = _create_flatpage_and_menuitem(published_fr=True, portals=[p1])
+        fp2 = _create_flatpage_and_menuitem(published_fr=True, portals=[p2])
+        fp3 = _create_flatpage_and_menuitem(published_fr=True, portals=[p1, p2])
 
         resp = self.client.get(
             reverse('apimobile:flatpage-list'),
@@ -188,9 +273,9 @@ class FlatPageTest(TestCase):
 
     def test_flatpage_list_returns_ordered_pages(self):
         FlatPage.objects.update(published_fr=False)
-        fp0 = FlatPageFactory(published_fr=True, order=2)
-        fp1 = FlatPageFactory(published_fr=True, order=3)
-        fp2 = FlatPageFactory(published_fr=True, order=1)
+        fp0 = _create_flatpage_and_menuitem(published_fr=True, order=2)
+        fp1 = _create_flatpage_and_menuitem(published_fr=True, order=3)
+        fp2 = _create_flatpage_and_menuitem(published_fr=True, order=1)
 
         resp = self.client.get(
             reverse('apimobile:flatpage-list'),
@@ -204,10 +289,10 @@ class FlatPageTest(TestCase):
 
     def test_flatpage_list_filter_by_target(self):
         FlatPage.objects.update(published_fr=False)
-        fp0 = FlatPageFactory(published_fr=True, target=FLATPAGES_TARGETS.HIDDEN)
-        fp1 = FlatPageFactory(published_fr=True, target=FLATPAGES_TARGETS.MOBILE)
-        fp2 = FlatPageFactory(published_fr=True, target=FLATPAGES_TARGETS.ALL)
-        fp3 = FlatPageFactory(published_fr=True, target=FLATPAGES_TARGETS.WEB)
+        fp0 = _create_flatpage_and_menuitem(published_fr=True, target=str(FLATPAGES_TARGETS.HIDDEN))
+        fp1 = _create_flatpage_and_menuitem(published_fr=True, target=str(FLATPAGES_TARGETS.MOBILE))
+        fp2 = _create_flatpage_and_menuitem(published_fr=True, target=str(FLATPAGES_TARGETS.ALL))
+        fp3 = _create_flatpage_and_menuitem(published_fr=True, target=str(FLATPAGES_TARGETS.WEB))
 
         resp = self.client.get(
             reverse('apimobile:flatpage-list'),
