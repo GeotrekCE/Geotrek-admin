4d960d9274b28bb875d114ced814c8c4ebd119fa
J-E Castagnede <j.e.castagnede@gmail.com>
2024-03-22 15:55:09 +0100

    format new files

--- geotrek/sensitivity/tests/test_filters.py
+++ geotrek/sensitivity/tests/test_filters.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
 from geotrek.sensitivity.filters import SensitiveAreaFilterSet
+
 from .factories import SensitiveAreaFactory
 
 
@@ -16,14 +17,18 @@ class SensitiveAreaFilterTest(TestCase):
         self.assertEqual(0, filter_set.qs.count())
 
     def test_provider_filter_with_providers(self):
-        sensitive_area1 = SensitiveAreaFactory.create(provider='my_provider1')
-        sensitive_area2 = SensitiveAreaFactory.create(provider='my_provider2')
+        sensitive_area1 = SensitiveAreaFactory.create(provider="my_provider1")
+        sensitive_area2 = SensitiveAreaFactory.create(provider="my_provider2")
 
         filter_set = SensitiveAreaFilterSet()
         filter_form = filter_set.form
 
-        self.assertIn('<option value="my_provider1">my_provider1</option>', filter_form.as_p())
-        self.assertIn('<option value="my_provider2">my_provider2</option>', filter_form.as_p())
+        self.assertIn(
+            '<option value="my_provider1">my_provider1</option>', filter_form.as_p()
+        )
+        self.assertIn(
+            '<option value="my_provider2">my_provider2</option>', filter_form.as_p()
+        )
 
         self.assertIn(sensitive_area1, filter_set.qs)
         self.assertIn(sensitive_area2, filter_set.qs)
--- geotrek/signage/tests/test_filters.py
+++ geotrek/signage/tests/test_filters.py
@@ -21,14 +21,14 @@ class SignageFilterTest(TestCase):
         form = self.filterset().form.as_p()
         self.assertIn('<option value="2015">2015</option>', form)
         self.assertIn('<option value="2016">2016</option>', form)
-        filter = self.filterset(data={'implantation_year': [2015]})
+        filter = self.filterset(data={"implantation_year": [2015]})
         self.assertTrue(i in filter.qs)
         self.assertFalse(i2 in filter.qs)
 
     def test_implantation_year_filter_with_str(self):
         i = SignageFactory.create(implantation_year=2015)
         i2 = SignageFactory.create(implantation_year=2016)
-        filter_set = self.filterset(data={'implantation_year': 'toto'})
+        filter_set = self.filterset(data={"implantation_year": "toto"})
         filter_form = filter_set.form
 
         self.assertIn('<option value="2015">2015</option>', filter_form.as_p())
@@ -38,18 +38,24 @@ class SignageFilterTest(TestCase):
         self.assertIn(i2, filter_set.qs)
 
     def test_form_should_have_signage_intervention_year_choices(self):
-        SignageInterventionFactory(begin_date='2015-01-01')
-        SignageInterventionFactory(begin_date='2020-01-01')
+        SignageInterventionFactory(begin_date="2015-01-01")
+        SignageInterventionFactory(begin_date="2020-01-01")
         filter_set = self.filterset()
-        choice_values = [choice[0] for choice in filter_set.form.fields['intervention_year'].choices]
+        choice_values = [
+            choice[0] for choice in filter_set.form.fields["intervention_year"].choices
+        ]
         self.assertIn(2015, choice_values)
         self.assertIn(2020, choice_values)
         self.assertNotIn(2022, choice_values)
 
     def test_filter_by_intervention_year(self):
-        filtered_signage_intervention = SignageInterventionFactory(begin_date='2015-01-01')
-        non_filtered_signage_intervention = SignageInterventionFactory(begin_date='2020-01-01')
-        filter_set = self.filterset(data={'intervention_year': [2015]})
+        filtered_signage_intervention = SignageInterventionFactory(
+            begin_date="2015-01-01"
+        )
+        non_filtered_signage_intervention = SignageInterventionFactory(
+            begin_date="2020-01-01"
+        )
+        filter_set = self.filterset(data={"intervention_year": [2015]})
         qs = filter_set.qs
 
         self.assertEqual(1, len(qs))
@@ -64,14 +70,18 @@ class SignageFilterTest(TestCase):
         self.assertEqual(0, filter_set.qs.count())
 
     def test_provider_filter_with_providers(self):
-        signage1 = SignageFactory.create(provider='my_provider1')
-        signage2 = SignageFactory.create(provider='my_provider2')
+        signage1 = SignageFactory.create(provider="my_provider1")
+        signage2 = SignageFactory.create(provider="my_provider2")
 
         filter_set = self.filterset()
         filter_form = filter_set.form
 
-        self.assertIn('<option value="my_provider1">my_provider1</option>', filter_form.as_p())
-        self.assertIn('<option value="my_provider2">my_provider2</option>', filter_form.as_p())
+        self.assertIn(
+            '<option value="my_provider1">my_provider1</option>', filter_form.as_p()
+        )
+        self.assertIn(
+            '<option value="my_provider2">my_provider2</option>', filter_form.as_p()
+        )
 
         self.assertIn(signage1, filter_set.qs)
         self.assertIn(signage2, filter_set.qs)
@@ -93,6 +103,12 @@ class BladeFilterSetTest(TestCase):
         cls.blade2 = cls.factory(signage=cls.signage2)
 
     def test_filter_by_organism(self):
-        filter = BladeFilterSet(data={'manager': [self.manager.pk,]})
+        filter = BladeFilterSet(
+            data={
+                "manager": [
+                    self.manager.pk,
+                ]
+            }
+        )
         self.assertIn(self.blade, filter.qs)
         self.assertNotIn(self.blade2, filter.qs)
