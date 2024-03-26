fe57a7501c9cc5b98d0c0aea02f6ef4fe09ae1fc
Marc-Antoine Dupré <marc-antoine.dupre@makina-corpus.com>
2024-03-26 22:41:13 +0100

    ♻ [Refactor] Convert FlatPageForm tests into tests on FlatPage Admin view
    The coming introduction of another Admin (`TreeAdmin` from the package `django-treebeard`) will lead to deep coupling between Form classes and Admin classes so view-based tests will be more suited.

--- geotrek/flatpages/tests/test_admin.py
+++ geotrek/flatpages/tests/test_admin.py
@@ -1,8 +1,9 @@
+from django.contrib.auth.models import Permission
 from django.test import TestCase
+from django.urls import reverse
 from geotrek.authent.tests.factories import UserProfileFactory
 from geotrek.common.models import FileType, Attachment
 from geotrek.common.utils.testdata import get_dummy_uploaded_image
-from geotrek.flatpages.forms import FlatPageForm
 from geotrek.flatpages.models import FlatPage
 
 from geotrek.flatpages.tests.factories import FlatPageFactory
@@ -31,53 +32,63 @@ class AdminSiteTest(TestCase):
         self.assertContains(response, "One looove")
 
 
-class FlatPageFormTest(TestCase):
-    def login(self):
+class FlatPageAdminFormViewsTestCase(TestCase):
+    def setUp(self) -> None:
         profile = UserProfileFactory(
             user__username='spammer',
             user__password='pipo'
         )
         user = profile.user
+
+        user.is_staff = True
+        user.save()
+        add_perm = Permission.objects.get(codename="add_flatpage")
+        view_perm = Permission.objects.get(codename="view_flatpage")
+        update_perm = Permission.objects.get(codename="change_flatpage")
+        delete_perm = Permission.objects.get(codename="delete_flatpage")
+        user.user_permissions.add(add_perm, view_perm, update_perm, delete_perm)
+
         success = self.client.login(username=user.username, password='pipo')
         self.assertTrue(success)
-        return user
+        self.user = user
 
-    def test_save_without_cover(self):
-        user = self.login()
+    def test_save_without_cover_image(self):
         data = {
             'title_en': 'Title',
             'external_url_en': 'http://geotrek.fr',
             'target': 'all',
         }
-        form = FlatPageForm(data, user=user)
-        self.assertTrue(form.is_valid())
-        form.save()
+
+        url = reverse('admin:flatpages_flatpage_add')
+        response = self.client.post(url, data=data)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(FlatPage.objects.count(), 1)
         page = FlatPage.objects.get()
         self.assertEqual(page.title, 'Title')
         self.assertEqual(page.external_url, 'http://geotrek.fr')
         self.assertEqual(page.target, 'all')
 
-    def test_save_with_cover(self):
-        user = self.login()
+    def test_save_with_cover_image(self):
         data = {
             'title_en': 'Title',
             'external_url_en': 'http://geotrek.fr',
             'target': 'all',
             'cover_image_author': 'Someone',
+            'cover_image': get_dummy_uploaded_image("flatpage_cover.png"),
         }
-        file_data = {
-            'cover_image': get_dummy_uploaded_image(),
-        }
-        form = FlatPageForm(data, file_data, user=user)
-        self.assertTrue(form.is_valid())
-        form.save()
+
+        url = reverse('admin:flatpages_flatpage_add')
+        response = self.client.post(url, data=data)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(FlatPage.objects.count(), 1)
         page = FlatPage.objects.get()
         attachment = Attachment.objects.get()
         self.assertEqual(attachment.content_object, page)
         self.assertEqual(attachment.author, 'Someone')
 
-    def test_save_delete_cover(self):
-        user = self.login()
+    def test_save_delete_cover_image(self):
         page = FlatPage.objects.create(
             title='Title',
             external_url='http://geotrek.fr',
@@ -89,7 +100,7 @@ class FlatPageFormTest(TestCase):
             attachment_file=get_dummy_uploaded_image(),
             author='Someone',
             filetype=filetype,
-            creator=user
+            creator=self.user
         )
         data = {
             'title_en': 'Title',
@@ -98,7 +109,9 @@ class FlatPageFormTest(TestCase):
             'cover_image_author': '',
             'cover_image-clear': 'on',
         }
-        form = FlatPageForm(data, instance=page, user=user)
-        self.assertTrue(form.is_valid())
-        form.save()
+
+        url = reverse('admin:flatpages_flatpage_change', args=[page.pk])
+        response = self.client.post(url, data=data)
+
+        self.assertEqual(response.status_code, 302)
         self.assertFalse(Attachment.objects.exists())
