ab4a8965186edc57183f956a29e6566791a0baaa
Marc-Antoine Dupré <marc-antoine.dupre@makina-corpus.com>
2024-03-26 08:46:10 +0100

    [wip] move FlatPageForm tests into FlatPageAdminChangeFormView (testing both Form+Admin)

--- geotrek/flatpages/tests/test_views.py
+++ geotrek/flatpages/tests/test_views.py
@@ -2,13 +2,12 @@ from django.conf import settings
 from django.contrib.auth.models import Permission
 from django.contrib.contenttypes.models import ContentType
 from django.test import TestCase
+from django.urls import reverse
 from geotrek.authent.models import User
+from geotrek.authent.tests.factories import UserProfileFactory
 from geotrek.common.models import FileType, Attachment
 from geotrek.common.utils.testdata import get_dummy_uploaded_image
-from geotrek.flatpages.models import MenuItem
-
-
-# from geotrek.flatpages.tests.factories import FlatPageFactory
+from geotrek.flatpages.models import MenuItem, FlatPage
 
 
 # TODO: to be removed, used for randov2 synchronization which is deprecated.
@@ -187,3 +186,86 @@ class MenuItemAdminChangeFormView(TestCase):
 
         qs = Attachment.objects.filter(object_id=menu_item.id, content_type=menu_item_content_type).all()
         self.assertEqual(len(qs), 0)
+
+
+class FlatPageAdminChangeFormView(TestCase):
+    def login(self):
+        profile = UserProfileFactory(
+            user__username='spammer',
+            user__password='pipo'
+        )
+        user = profile.user
+
+        # Gives permissions to access AdminSite and to CRUD FlatPages
+        user.is_staff = True
+        user.save()
+        add_perm = Permission.objects.get(codename="add_flatpage")
+        view_perm = Permission.objects.get(codename="view_flatpage")
+        update_perm = Permission.objects.get(codename="change_flatpage")
+        delete_perm = Permission.objects.get(codename="delete_flatpage")
+        user.user_permissions.add(add_perm, view_perm, update_perm, delete_perm)
+
+        success = self.client.login(username=user.username, password='pipo')
+        self.assertTrue(success)
+        return user
+
+    def test_save_without_cover_image(self):
+        self.login()
+        data = {
+            "title_en": "Hello English World!",
+            "_position": "first-child",
+        }
+        url = reverse('admin:flatpages_flatpage_add')
+        response = self.client.post(url, data=data)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(FlatPage.objects.count(), 1)
+        page = FlatPage.objects.get()
+        self.assertEqual(page.title, 'Hello English World!')
+
+    def test_save_with_cover_image(self):
+        self.login()
+
+        with get_dummy_uploaded_image().open() as image_file:
+            data = {
+                "title_en": "Hello World!",
+                "_position": "first-child",
+                "cover_image": image_file,
+                'cover_image_author': 'Someone',
+            }
+            url = reverse('admin:flatpages_flatpage_add')
+            response = self.client.post(url, data=data)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(FlatPage.objects.count(), 1)
+        page = FlatPage.objects.get()
+        self.assertEqual(Attachment.objects.count(), 1)
+        attachment = Attachment.objects.get()
+        self.assertEqual(attachment.content_object, page)
+        self.assertEqual(attachment.author, 'Someone')
+
+    def test_save_delete_cover_image(self):
+        user = self.login()
+        page = FlatPage(
+            title='Title',
+        )
+        FlatPage.add_root(instance=page)
+        page.save()
+        filetype = FileType.objects.create(type="Photographie")
+        Attachment.objects.create(
+            content_object=page,
+            attachment_file=get_dummy_uploaded_image(),
+            author='Someone',
+            filetype=filetype,
+            creator=user
+        )
+
+        url = reverse('admin:flatpages_flatpage_change', args=[page.pk])
+        response = self.client.post(url, data={
+            "title_en": "Hello World!",
+            "_position": "first-child",
+            'cover_image-clear': 'on',
+        })
+
+        self.assertEqual(response.status_code, 302)
+        self.assertFalse(Attachment.objects.exists())
