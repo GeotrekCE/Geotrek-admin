f8091482d74b6de11d0a95d70836623801fe903f
Marc-Antoine Dupré <marc-antoine.dupre@makina-corpus.com>
2024-03-26 23:41:26 +0100

    Remove obsolete tests for old FlatPage

--- geotrek/flatpages/tests/test_models.py
+++ geotrek/flatpages/tests/test_models.py
@@ -1,17 +1,14 @@
 from django.test import TestCase
-from geotrek.flatpages.models import FlatPage
 
 from geotrek.flatpages.tests.factories import FlatPageFactory
 
 
 class FlatPageModelTest(TestCase):
+
+    # FIXME: to remove? Slug was used for sync randov2
     def test_slug_is_taken_from_title(self):
         fp = FlatPageFactory(title="C'est pour toi")
         self.assertEqual(fp.slug, 'cest-pour-toi')
-
-    def test_target_is_all_by_default(self):
-        fp = FlatPageFactory()
-        self.assertEqual(fp.target, 'all')
 
     # FIXME: to remove? Should be tested in the mixin
     def test_publication_date_is_filled_if_published(self):
@@ -21,38 +18,6 @@ class FlatPageModelTest(TestCase):
         fp.published = True
         fp.save()
         self.assertIsNotNone(fp.publication_date)
-
-    def test_validation_does_not_fail_if_url_and_content_are_falsy(self):
-        fp = FlatPageFactory(external_url="  ",
-                             content="<p></p>")
-        fp.clean()
-
-    def test_validation_does_not_fail_if_url_is_none(self):
-        fp = FlatPageFactory(external_url=None,
-                             content="<p></p>")
-        fp.clean()
-
-    def test_retrieve_by_order(self):
-        try:
-            fp = FlatPageFactory.create_batch(5)
-            for index, flatpage in enumerate(FlatPage.objects.all()):
-                if index == 0:
-                    continue
-                self.assertGreater(flatpage.order, int(fp[index - 1].order))
-        finally:
-            for f in fp:
-                f.clean()
-
-    def test_retrieve_by_id_if_order_is_the_same(self):
-        try:
-            fp = FlatPageFactory.create_batch(5, order=0)
-            for index, flatpage in enumerate(FlatPage.objects.all()):
-                if index == 0:
-                    continue
-                self.assertGreater(flatpage.id, fp[index - 1].id)
-        finally:
-            for f in fp:
-                f.clean()
 
     # FIXME: to remove?
     def test_is_public(self):
