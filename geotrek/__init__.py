3fbfd9868e38c818d58e48c435889eedf0912856
J-E Castagnede <j.e.castagnede@gmail.com>
2024-03-27 09:06:23 +0100

    move to toml config

--- geotrek/__init__.py
+++ geotrek/__init__.py
@@ -1,6 +1,17 @@
 import os
+import subprocess
+import toml
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with open(os.path.join(here, 'VERSION')) as file_version:
-    __version__ = file_version.read().strip()
+with open(os.path.join(os.path.dirname(here), "pyproject.toml")) as file_version:
+    toml_content = toml.loads(file_version.read())
+    __version__ = toml_content["project"]["version"]
+
+
+def cmd():
+    cmd = [
+        "python",
+        "../manage.py",
+    ]
+    subprocess.run(cmd)
