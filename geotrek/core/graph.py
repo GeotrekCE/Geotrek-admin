f93857156ec12de8340034b19af8dfa672023ef1
JustineFricou <jfr@makina-corpus.com>
2024-04-29 13:43:30 +0200

    Moving initial graph generation into the PathRouter class

--- geotrek/core/graph.py
+++ geotrek/core/graph.py
@@ -14,69 +14,66 @@ from geotrek.common.utils import sqlfunction
 from .models import Path
 
 
-def path_modifier(path):
-    length = 0.0 if math.isnan(path.length) else path.length
-    return {"id": path.pk, "length": length}
-
-
-def get_key_optimizer():
-    next_id = iter(range(1, 1000000)).__next__
-    mapping = defaultdict(next_id)
-    return lambda x: mapping[x]
-
-
-def graph_edges_nodes_of_qs(qs):
-    """
-    return a graph on the form:
-    nodes {
-        coord_point_a {
-            coord_point_b: edge_id
-        }
-    }
-    edges {
-        edge_id: {
-            nodes: [point_a, point_b, ...]
-            ** extra settings (length etc.)
-        }
-    }
-
-
-    coord_poPathRouterint are tuple of float
-    """
-
-    key_modifier = get_key_optimizer()
-    value_modifier = path_modifier
-
-    edges = defaultdict(dict)
-    nodes = defaultdict(dict)
-
-    for path in qs:
-        coords = path.geom.coords
-        start_point, end_point = coords[0], coords[-1]
-        k_start_point, k_end_point = key_modifier(start_point), key_modifier(end_point)
-
-        v_path = value_modifier(path)
-        v_path['nodes_id'] = [k_start_point, k_end_point]
-        edge_id = v_path['id']
-
-        nodes[k_start_point][k_end_point] = edge_id
-        nodes[k_end_point][k_start_point] = edge_id
-        edges[edge_id] = v_path
-
-    return {
-        'edges': dict(edges),
-        'nodes': dict(nodes),
-    }
-
-
 class PathRouter:
     def __init__(self):
         # To generate IDs for temporary nodes and edges:
         self.id_count = 90000000
 
-        graph = graph_edges_nodes_of_qs(Path.objects.exclude(draft=True))
+        graph = self.graph_edges_nodes_of_qs(Path.objects.exclude(draft=True))
         self.nodes = graph['nodes']
         self.edges = graph['edges']
+
+    def graph_edges_nodes_of_qs(self, qs):
+        """
+        return a graph on the form:
+        nodes {
+            coord_point_a {
+                coord_point_b: edge_id
+            }
+        }
+        edges {
+            edge_id: {
+                nodes: [point_a, point_b, ...]
+                ** extra settings (length etc.)
+            }
+        }
+
+
+        coord_poPathRouterint are tuple of float
+        """
+
+        def path_modifier(path):
+            length = 0.0 if math.isnan(path.length) else path.length
+            return {"id": path.pk, "length": length}
+
+        def get_key_optimizer():
+            next_id = iter(range(1, 1000000)).__next__
+            mapping = defaultdict(next_id)
+            return lambda x: mapping[x]
+
+        key_modifier = get_key_optimizer()
+        value_modifier = path_modifier
+
+        edges = defaultdict(dict)
+        nodes = defaultdict(dict)
+
+        for path in qs:
+            coords = path.geom.coords
+            start_point, end_point = coords[0], coords[-1]
+            k_start_point, k_end_point = key_modifier(start_point), key_modifier(end_point)
+
+            v_path = value_modifier(path)
+            v_path['nodes_id'] = [k_start_point, k_end_point]
+            edge_id = v_path['id']
+
+            nodes[k_start_point][k_end_point] = edge_id
+            nodes[k_end_point][k_start_point] = edge_id
+            edges[edge_id] = v_path
+
+        return {
+            'edges': dict(edges),
+            'nodes': dict(nodes),
+        }
 
     def generate_id(self):
         new_id = self.id_count
