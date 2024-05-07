6e74eea2065c3bd937214967c7e7debbc8989161
Marc-Antoine Dupré <marc-antoine.dupre@makina-corpus.com>
2024-05-07 20:00:45 +0200

    Back to development

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ copyright = f'2013-{datetime.date.today().year}, Makina Corpus'
 # The short X.Y version.
 version = '2.105'
 # The full version, including alpha/beta/rc tags.
-release = '2.105.1'
+release = '2.105.1+dev'
 
 exclude_patterns = ['_build']
 
