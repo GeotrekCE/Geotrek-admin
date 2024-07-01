bc949d6ccb232046a891a5162dbe0ce97f89e4f2
Justine Fricou <justine.fricou@gmail.com>
2024-07-01 17:31:24 +0200

    Fix typo in cypress.config.js

--- benchmarking/cypress.config.js
+++ benchmarking/cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     baseUrl: 'http://geotrek.local:8000',
   },
   video: false,
-  defaultcommandTimeout: 5000000,
+  defaultCommandTimeout: 5000000,
   requestTimeout: 5000000,
   responseTimeout: 5000000,
   projectId: "ktpy7v"
