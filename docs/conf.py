71a0f05ed4ddea996ee60be5cd89ac107b4d67b2
Joaquim Nallar <jna@makina-corpus.com>
2024-07-12 15:33:36 +0200

    Release 2.108.0

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = f'2013-{datetime.date.today().year}, Makina Corpus'
 # built documents.
 #
 # The short X.Y version.
-version = '2.107'
+version = '2.108'
 # The full version, including alpha/beta/rc tags.
-release = '2.107.1+dev'
+release = '2.108.0'
 
 exclude_patterns = ['_build']
 
