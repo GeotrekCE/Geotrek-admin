e0692e6dcd2157566c4aa1dafccae353f6098e5d
Justine Fricou <justine.fricou@gmail.com>
2024-07-23 14:54:08 +0200

    Modify database extensions

--- geotrek/common/templates/common/sql/pre_20_extensions.sql
+++ geotrek/common/templates/common/sql/pre_20_extensions.sql
@@ -1,5 +1,6 @@
 -- Used to ensure extension is enabled even in test database (migrations disabled)
 -- Otherwise UUIDs on objects can not be generated on insert (including path splits)
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
-CREATE EXTENSION IF NOT EXISTS "postgis_raster" CASCADE;
+CREATE EXTENSION IF NOT EXISTS "postgis";
+CREATE EXTENSION IF NOT EXISTS "postgis_raster";
 CREATE EXTENSION IF NOT EXISTS "pgrouting" CASCADE;
