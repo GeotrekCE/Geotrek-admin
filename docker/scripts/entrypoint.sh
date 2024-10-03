b09082860cff2b419f1a15d8a74eca0673b7613d
J-E Castagnede <j.e.castagnede@gmail.com>
2024-10-03 17:26:03 +0200

    Docker file to python3.10

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -26,9 +26,6 @@ fi
 # Activate venv
 . /opt/venv/bin/activate
 
-# Defaults POSTGRES_HOST to Docker host IP
-export POSTGRES_HOST=${POSTGRES_HOST:-`ip route | grep default | sed 's/.* \([0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+\) .*/\1/'`}
-
 # Defaults SECRET_KEY to a random value
 SECRET_KEY_FILE=/opt/geotrek-admin/var/conf/secret_key
 if [ -z $SECRET_KEY ]; then
