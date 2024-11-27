2d23f06757b0ce99b89370ce7f4fcd1530c6bb4c
J-E Castagnede <j.e.castagnede@gmail.com>
2024-11-27 15:01:24 +0100

    update to cypress 13

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -12,5 +12,6 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: "http://geotrek.local:8000",
     supportFile: "support/e2e.js",
+    specPattern: "e2e/**/*.cy.{js,jsx,ts,tsx}",
     },
   });
