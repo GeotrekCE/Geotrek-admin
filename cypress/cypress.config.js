51b21e6fc2f7f10c361e82671102c7c37637033c
J-E Castagnede <j.e.castagnede@gmail.com>
2024-11-27 11:43:53 +0100

    update to cypress 13

---
+++ cypress/cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: 'ktpy7v',
+  redirectionLimit:400,
+  requestTimeout: 50000,
+  defaultCommandTimeout: 80000,
+  e2e: {
+    baseUrl: "http://geotrek.local:8000",
+    },
+  }
+});
