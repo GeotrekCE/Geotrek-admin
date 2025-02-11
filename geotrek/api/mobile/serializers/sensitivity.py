296703597ea6ca131689629de36228c24a2bf529
Justine Fricou <justine.fricou@gmail.com>
2025-02-11 14:59:46 +0100

    Remove species_id from SensitiveArea's mobile API serializer

--- geotrek/api/mobile/serializers/sensitivity.py
+++ geotrek/api/mobile/serializers/sensitivity.py
@@ -10,7 +10,6 @@ if 'geotrek.sensitivity' in settings.INSTALLED_APPS:
         geometry = geo_serializers.GeometryField(read_only=True, precision=7, source='geom2d_transformed')
         name = serializers.ReadOnlyField(source='species.name')
         description = serializers.ReadOnlyField()
-        species_id = serializers.SerializerMethodField(source='species.pk')
         practices = serializers.PrimaryKeyRelatedField(many=True, source='species.practices', read_only=True)
         info_url = serializers.URLField(source='species.url')
         period = serializers.SerializerMethodField()
@@ -20,14 +19,9 @@ if 'geotrek.sensitivity' in settings.INSTALLED_APPS:
             id_field = 'pk'
             geo_field = 'geometry'
             fields = (
-                'id', 'pk', 'name', 'description', 'species_id', 'practices',
+                'id', 'pk', 'name', 'description', 'practices',
                 'contact', 'info_url', 'period', 'geometry',
             )
-
-        def get_species_id(self, obj):
-            if obj.species.category == sensitivity_models.Species.SPECIES:
-                return obj.species_id
-            return None
 
         def get_period(self, obj):
             return [getattr(obj.species, 'period{:02}'.format(p)) for p in range(1, 13)]
