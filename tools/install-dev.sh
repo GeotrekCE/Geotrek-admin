94178da840a16c0d4607e8841231bf469adbab3e
J-E Castagnede <j.e.castagnede@gmail.com>
2025-03-10 11:47:34 +0100

    change dev script

--- tools/install-dev.sh
+++ tools/install-dev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This script is used to install Geotrek-admin >= 2.33, dev mode
+# This script is used to install Geotrek-admin >= 2.33
 
 set -e
 
@@ -11,7 +11,7 @@ else
 	exit 1
 fi
 
-if [ "`locale charmap`" != "UTF-8" ]; then
+if [ "$(locale charmap)" != "UTF-8" ]; then
 	echo "ERROR! Your user locale charmap is not UTF-8"
 	exit 1
 fi
@@ -21,18 +21,18 @@ if ! `localectl status | grep -q "System Locale: LANG=.*UTF-8"`; then
 	exit 1
 fi
 
-if [ "$*" == "--nodb" ]; then
+if [ "$NODB" == "true" ]; then
 	postgis_and_routing=""
-elif [ -n "$*" ]; then
-	echo "Usage: $0 [--nodb]"
-	exit 1
 else
 	postgis_and_routing="postgresql-pgrouting"
 fi
 
 sudo apt update
-sudo apt install -y $postgis_and_routing wget software-properties-common
-echo "deb [arch=amd64] https://packages.geotrek.fr/ubuntu $(lsb_release -sc) dev" | sudo tee /etc/apt/sources.list.d/geotrek.list
-wget -O- "https://packages.geotrek.fr/geotrek.gpg.key" | sudo apt-key add -
+sudo apt install -y $postgis_and_routing curl ca-certificates software-properties-common
+sudo install -d /usr/share/geotrek
+sudo curl -o /usr/share/geotrek/apt.geotrek.org.key --fail https://packages.geotrek.fr/geotrek.gpg.key
+sudo rm -f /etc/apt/sources.list.d/geotrek.list
+echo "deb [signed-by=/usr/share/geotrek/apt.geotrek.org.key] https://packages.geotrek.fr/ubuntu $(lsb_release -cs) dev" | sudo tee /etc/apt/sources.list.d/geotrek.list
 sudo apt update
-sudo apt install -y geotrek-admin
+sudo apt install --no-install-recommends -y postgis  # force install postgis scripts only to use loaddem command, even if script does not manage database installation
+sudo apt install --no-install-recommends -y geotrek-admin
