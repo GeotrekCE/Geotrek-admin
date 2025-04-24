eaafd37aaaf973f0624dae00b613f4bb1b436b47
PartyNell <nell.party16@gmail.com>
2025-04-24 16:36:52 +0200

    add OpenStreetMap Signage parser

--- geotrek/signage/parsers.py
+++ geotrek/signage/parsers.py
@@ -1,5 +1,14 @@
-from geotrek.common.parsers import GeotrekParser
+from django.contrib.gis.geos import Point
+from django.conf import settings
+from django.utils.translation import gettext as _
+
 from geotrek.signage.models import Signage
+from geotrek.core.models import Path, Topology
+from geotrek.common.parsers import (
+    GeotrekParser,
+    GlobalImportError,
+    OpenStreetMapParser,
+)
 
 
 class GeotrekSignageParser(GeotrekParser):
@@ -32,3 +41,70 @@ class GeotrekSignageParser(GeotrekParser):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.next_url = f"{self.url}/api/v2/signage"
+
+
+class OpenStreetMapSignageParser(OpenStreetMapParser):
+    """Parser to import POI from OpenStreetMap"""
+
+    type = None
+    model = Signage
+    eid = "eid"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.query_settings.osm_element_type = "node"
+
+        if self.type:
+            self.constant_fields["type"] = self.type
+
+    fields = {
+        "eid": ("type", "id"),  # ids are unique only for object of the same type
+        "name": "tags.name",
+        "description": "tags.description",
+        "geom": ("lon", "lat"),
+    }
+    constant_fields = {
+        "published": True,
+    }
+    natural_keys = {
+        "type": "label",
+    }
+    field_options = {"geom": {"required": True}, "type": {"required": True}}
+    topology = Topology.objects.none()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.type:
+            self.constant_fields["type"] = self.type
+
+    def start(self):
+        super().start()
+        if settings.TREKKING_TOPOLOGY_ENABLED and not Path.objects.exists():
+            raise GlobalImportError(
+                _("You need to add a network of paths before importing POIs")
+            )
+
+    def filter_geom(self, src, val):
+        # convert OSM geometry to point
+        lng, lat = val
+
+        geom = Point(float(lng), float(lat), srid=self.osm_srid)  # WGS84
+        geom.transform(settings.SRID)
+
+        # create topology
+        self.topology = Topology.objects.none()
+        if settings.TREKKING_TOPOLOGY_ENABLED:
+            # Use existing topology helpers to transform a Point(x, y)
+            # to a path aggregation (topology)
+            geometry = geom.transform(settings.API_SRID, clone=True)
+            geometry.coord_dim = 2
+            serialized = f'{{"lng": {geometry.x}, "lat": {geometry.y}}}'
+            self.topology = Topology.deserialize(serialized)
+            # Move deserialization aggregations to the POI
+        return geom
+
+    def parse_obj(self, row, operation):
+        super().parse_obj(row, operation)
+        if settings.TREKKING_TOPOLOGY_ENABLED and self.obj.geom and self.topology:
+            self.obj.mutate(self.topology)
