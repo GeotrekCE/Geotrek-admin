152980e7544a5778a7093159c706a1f698e699ff
PartyNell <nell.party16@gmail.com>
2025-04-25 16:17:17 +0200

    apply suggestions

--- geotrek/tourism/parsers.py
+++ geotrek/tourism/parsers.py
@@ -1396,7 +1396,7 @@ class InformationDeskOpenStreetMapParser(OpenStreetMapParser):
     eid = "eid"
 
     fields = {
-        "eid": ("type", "id"),  # ids are unique only for object of the same type,
+        "eid": "id",
         "phone": ("tags.contact:phone", "tags.phone"),
         "email": ("tags.contact:email", "tags.email"),
         "website": ("tags.contact:website", "tags.website"),
