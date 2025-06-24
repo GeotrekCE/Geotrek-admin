420dffb84f0639bd7c44079235186552cb3f1d5d
Justine Fricou <justine.fricou@gmail.com>
2025-06-24 16:00:58 +0200

    Add apidae infrastructure parser

--- geotrek/infrastructure/parsers.py
+++ geotrek/infrastructure/parsers.py
@@ -1,8 +1,11 @@
-from django.contrib.gis.geos import Point
+from django.contrib.gis.geos import Point, GEOSGeometry
 
 from geotrek.common.parsers import GeotrekParser, OpenStreetMapParser
+from geotrek.trekking.parsers import ApidaeBaseParser
 from geotrek.core.mixins.parsers import PointTopologyParserMixin
 from geotrek.infrastructure.models import Infrastructure
+from django.core.exceptions import ImproperlyConfigured
+from django.utils.translation import gettext as _
 
 
 class GeotrekInfrastructureParser(GeotrekParser):
@@ -28,6 +31,54 @@ class GeotrekInfrastructureParser(GeotrekParser):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.next_url = f"{self.url}/api/v2/infrastructure"
+
+
+class ApidaeInfrastructureParser(PointTopologyParserMixin, ApidaeBaseParser):
+    model = Infrastructure
+    eid = "eid"
+    separator = None
+    infrastructure_type = None
+
+    responseFields = [
+        "id",
+        "localisation.geolocalisation.geoJson",
+        "nom",
+        "presentation",
+        "prestations.tourismesAdaptes",
+    ]
+    fields = {
+        "eid": "id",
+        "geom": "localisation.geolocalisation.geoJson",
+        "name": "nom.libelleFr",
+        "description": ("presentation.descriptifCourt.libelleFr", "presentation.descriptifDetaille.libelleFr"),
+        "accessibility": "prestations.tourismesAdaptes",
+    }
+    natural_keys = {
+        "type": "label",
+    }
+    field_options = {
+        "type": {"create": True}
+    }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.infrastructure_type:
+            self.constant_fields = self.constant_fields.copy()
+            self.constant_fields["type"] = self.infrastructure_type
+        else:
+            msg = _("An infrastructure type must be specified in the parser configuration.")
+            raise ImproperlyConfigured(msg)
+
+    def build_geos_geometry(self, src, val):
+        return GEOSGeometry(str(val))
+
+    def filter_description(self, src, val):
+        short_descr, detailed_descr = val
+        return detailed_descr or short_descr
+
+    def filter_accessibility(self, src, val):
+        accessibilities = [a.get('libelleFr') for a in val]
+        return "\n".join(accessibilities)
 
 
 class OpenStreetMapInfrastructureParser(PointTopologyParserMixin, OpenStreetMapParser):
