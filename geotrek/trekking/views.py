960cf7c16d99eb35a1225d9f0f7a0ead8a3c0e95
PartyNell <nell.party16@gmail.com>
2025-07-03 17:13:07 +0200

    add OSM API authentification

--- geotrek/trekking/views.py
+++ geotrek/trekking/views.py
@@ -1,10 +1,4 @@
-import requests
-import json
-
-import xml.etree.ElementTree as ET
-
 from django.conf import settings
-from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.gis.db.models.functions import Transform
 from django.db.models.query import Prefetch
@@ -13,8 +7,7 @@ from django.shortcuts import get_object_or_404
 from django.utils import translation
 from django.utils.decorators import method_decorator
 from django.utils.html import escape
-from django.utils.translation import gettext as _
-from django.views.generic import CreateView, DetailView, FormView
+from django.views.generic import CreateView, FormView, View
 from django.views.generic.detail import BaseDetailView
 from mapentity.helpers import alphabet_enumeration
 from mapentity.views import (
@@ -33,8 +26,14 @@ from rest_framework import permissions as rest_permissions
 from rest_framework import viewsets
 
 from geotrek.authent.decorators import same_structure_required
-from geotrek.common.forms import AttachmentAccessibilityForm, OSMForm
-from geotrek.common.mixins.views import CompletenessMixin, CustomColumnsMixin
+from geotrek.common.forms import AttachmentAccessibilityForm
+from geotrek.common.mixins.views import (
+    CompletenessMixin,
+    CustomColumnsMixin,
+    OSMDetailMixin,
+    OSMComparisonViewMixin,
+    OSMValidationViewMixin
+)
 from geotrek.common.models import (
     Attachment,
     HDViewPoint,
@@ -45,7 +44,6 @@ from geotrek.common.models import (
 from geotrek.common.permissions import PublicOrReadPermMixin
 from geotrek.common.views import DocumentBookletPublic, DocumentPublic, MarkupPublic
 from geotrek.common.viewsets import GeotrekMapentityViewSet
-from geotrek.common.utils.translation import get_translated_fields
 from geotrek.core.models import AltimetryMixin
 from geotrek.core.views import CreateFromTopologyMixin
 from geotrek.infrastructure.models import Infrastructure
@@ -398,7 +396,7 @@ class POIFormatList(MapEntityFormat, POIList):
             yield poi
 
 
-class POIDetail(CompletenessMixin, MapEntityDetail):
+class POIDetail(CompletenessMixin, OSMDetailMixin, MapEntityDetail):
     queryset = (
         POI.objects.existing()
         .prefetch_related(
@@ -413,6 +411,7 @@ class POIDetail(CompletenessMixin, MapEntityDetail):
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
         context["can_edit"] = self.get_object().same_structure(self.request.user)
+        context.update(self.get_osm_context_data())
         return context
 
 
@@ -476,263 +475,18 @@ class POIViewSet(GeotrekMapentityViewSet):
             qs = qs.prefetch_related("attachments")
         return qs
 
-class POIOSMCompare(DetailView):
+
+class POIOSMCompare(OSMComparisonViewMixin):
     model = POI
-    template_name = "trekking/poi_osm_comparaison.html"
 
     mapping = {
         "name": "name",
         "description": "description",
     }
 
-    def translation(self):
-        default_lang = settings.MODELTRANSLATION_DEFAULT_LANGUAGE
 
-        for field in get_translated_fields(self.model):
-            tags = self.mapping.get(field)
-            if not tags:
-                continue
-
-            is_str = isinstance(tags, str)
-            tags = [tags] if is_str else list(tags)
-
-            # Protect the class from multiple translation mappings as field is a static attribute
-            is_translated = [tag for tag in tags if tag.endswith(f":{default_lang}")]
-            if is_translated:
-                continue
-
-            for lang in settings.MODELTRANSLATION_LANGUAGES:
-                if lang != default_lang:
-                    geotrek_field = f"{field}_{lang}"
-                    translated = [tag + ":" + lang for tag in tags]
-                    self.mapping[geotrek_field] = (
-                        translated[0] if is_str else tuple(translated)
-                    )
-
-            translated = [f"{tag}:{default_lang}" for tag in tags]
-            self.mapping[field] = (
-                (translated[0], tags[0]) if is_str else tuple(translated + tags)
-            )
-
-    def get_osm_object(self, geotrek_object, **kwargs):
-        eid = geotrek_object.eid
-
-        type = "node" if eid[0] == "N" else \
-               "way" if eid[0] == "W" else \
-               "relation"
-        id = eid[1:]
-
-        response = requests.get(f"https://master.apis.dev.openstreetmap.org/api/0.6/{type}/{id}.json")
-        if response.status_code == 404:
-            raise Exception(f"OpenStreetMap object {type}({id}) not found")
-        elif response.status_code == 410:
-            raise Exception(f"OpenStreetMap object {type}({id}) has been deleted")
-
-        return response.json()['elements'][0]
-
-    def map_fields(self, geotrek, osm):
-        osm_tags = osm.get("tags", {})
-        context = [{
-            "geotrek_field": "eid",
-            "geotrek_value": geotrek.eid,
-            "osm_field": "ID",
-            "osm_value": f"{osm.get('type')}({osm.get('id')})"
-        }]
-
-        for geotrek_field, osm_fields in sorted(self.mapping.items()):
-            fields = (osm_fields,) if isinstance(osm_fields, str) else osm_fields
-            osm_value, osm_field = next(
-                ((value, field) for field in fields if (value := osm_tags.get(field))),
-                ("", fields[0])
-            )
-
-            context.append({
-                "geotrek_field": geotrek_field,
-                "geotrek_value": getattr(geotrek, geotrek_field, ""),
-                "osm_field": osm_field,
-                "osm_value": osm_value
-            })
-
-        return context
-
-    def get_context_data(self, **kwargs):
-        # add translation fields
-        self.translation()
-
-        # get geotrek object
-        geotrek_object = self.model.objects.get(pk=self.kwargs["pk"])
-
-        # get OpenStreetMap object
-        osm_object = self.get_osm_object(geotrek_object)
-
-        # create context
-        context = super().get_context_data(**kwargs)
-        context.update({
-            "objects": self.map_fields(geotrek_object, osm_object),
-            "osm_object_serialized": json.dumps(osm_object),
-        })
-
-        return context
-
-
-class POIOSMValidate(FormView):
+class POIOSMValidate(OSMValidationViewMixin):
     template_name = "trekking/poi_osm_validation.html"
-    form_class = OSMForm
-    success_url = "/poi/list/"
-
-    TOKEN = '4NnQ0aihS61B0lGqa2d6EdvRV9pt3MMVGbFVT53BVB8'
-    USER_AGENT = 'Geotrek-admin/0.1'
-    BASE_URL = 'https://master.apis.dev.openstreetmap.org/api/0.6'
-
-    def get_updated_osm_object(self):
-        # Récupération de l'objet OSM depuis les paramètres GET
-        osm_object_serialized = self.request.GET.get("osm_object", "{}")
-        osm_object = json.loads(osm_object_serialized)
-
-        # Mise à jour des tags avec les valeurs GET supplémentaires
-        for key, value in self.request.GET.items():
-            if key != "osm_object":
-                try:
-                    tag, val = value.split("|", 1)
-                    osm_object["tags"][tag] = val or osm_object["tags"].get(tag)
-                except ValueError:
-                    continue  # Ignore les valeurs mal formées
-
-        return osm_object_serialized, osm_object
-
-    def get_context_data(self, **kwargs):
-        osm_object_serialized, osm_object = self.get_updated_osm_object()
-
-        context = super().get_context_data(**kwargs)
-
-        context.update({
-            "object": osm_object,
-            "osm_object_serialized": osm_object_serialized
-        })
-        return context
-
-    def get_success_url(self):
-        pk = self.kwargs["pk"]
-        return f"/poi/{pk}"
-
-    def form_valid(self, form):
-        headers = {
-            'Authorization': f'Bearer {self.TOKEN}',
-            'User-Agent': self.USER_AGENT
-        }
-
-        xml_headers = headers.copy()
-        xml_headers["Content-Type"] = "text/xml"
-
-        osm_object_serialized, osm_object = self.get_updated_osm_object()
-
-        # create changeset
-        url = f"{self.BASE_URL}/changeset/create"
-
-        changeset_osm = ET.Element("osm", {"version": "0.6"})
-        changeset = ET.SubElement(changeset_osm, "changeset")
-        ET.SubElement(changeset, "tag", k="created_by", v=self.USER_AGENT)
-        ET.SubElement(changeset, "tag", k="comment", v=self.request.GET.get("comment", ""))
-
-        changeset_xml = ET.tostring(changeset_osm, encoding='utf-8', xml_declaration=True).decode()
-
-        response = requests.put(url,
-                     headers=xml_headers,
-                     data = changeset_xml)
-
-        if response.status_code == 400:
-            msg = _("Bad Request: Changeset")
-            messages.error(self.request, msg)
-            return super().form_valid(form)
-        elif response.status_code == 405:
-            msg = _("Method Not Allowed: Changeset")
-            messages.error(self.request, msg)
-            return super().form_valid(form)
-
-        changeset_id = response.content.decode()
-
-        # update object
-        url = f"{self.BASE_URL}/{osm_object['type']}/{osm_object['id']}"
-
-        element_osm = ET.Element("osm",  {"version": "0.6"})
-
-        elements_tags = ["changeset", "id", "lat", "lon", "version", "visible"]
-
-        attributs = {k: str(v) for k, v in osm_object.items() if k in elements_tags}
-        attributs["changeset"] = changeset_id
-
-        element = ET.SubElement(element_osm, osm_object["type"], attributs)
-
-        if "nodes" in osm_object:
-            for node in osm_object["nodes"]:
-                ET.SubElement(element, "nd", {"ref": str(node)})
-
-        if "members" in osm_object:
-            for member in osm_object["members"]:
-                ET.SubElement(element, "member", {
-                    "type": str(member["type"]),
-                    "ref": str(member["ref"]),
-                    "role": str(member["role"]),
-                })
-
-        # tags
-        for key, value in osm_object["tags"].items():
-            ET.SubElement(element, "tag", {
-                "k": key,
-                "v": value
-            })
-
-        element_xml = ET.tostring(element_osm, encoding='utf-8', xml_declaration=True).decode()
-
-        print("\n\n\n", element_xml, "\n\n\n")
-
-        response = requests.put(url,
-                                headers=xml_headers,
-                                data=element_xml)
-
-        if response.status_code == 400:
-            msg = _("Bad Request: Element")
-            messages.error(self.request, msg)
-            return super().form_valid(form)
-        elif response.status_code == 404:
-            msg = _("Element not found")
-            messages.error(self.request, msg)
-            return super().form_valid(form)
-        elif response.status_code == 409:
-            msg = _("Changeset closed")
-            messages.error(self.request, msg)
-            return super().form_valid(form)
-        elif response.status_code == 412:
-            msg = _(f"Missing nodes/ways")
-            messages.error(self.request, msg)
-            return super().form_valid(form)
-        elif response.status_code == 429:
-            msg = _("Too Many Requests")
-            messages.error(self.request, msg)
-            return super().form_valid(form)
-
-        # close changeset
-
-        url = f"{self.BASE_URL}/changeset/{changeset_id}/close"
-
-        response = requests.put(url,headers=headers)
-
-        if response.status_code == 404:
-            msg = _("Changeset not found")
-            messages.error(self.request, msg)
-            return super().form_valid(form)
-        elif response.status_code == 405:
-            msg = _("Method Not Allowed: Changeset")
-            messages.error(self.request, msg)
-            return super().form_valid(form)
-        elif response.status_code == 409:
-            msg = _("Changeset already closed")
-            messages.error(self.request, msg)
-            return super().form_valid(form)
-
-        msg = _("Saved")
-        messages.success(self.request, msg)
-        return super().form_valid(form)
 
 
 class TrekPOIViewSet(viewsets.ModelViewSet):
