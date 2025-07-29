801140c5cd6801e18d537ce59c6a984d581812fe
kayarhabby <pasciahershekaya@gmail.com>
2025-07-29 17:17:27 +0200

    réorganisation du projet, suppression des fichiers non utilisés et refactorisation des codes js de trek

--- geotrek/diving/static/diving/main.js
+++ geotrek/diving/static/diving/main.js
@@ -1,32 +1,3 @@
-$(window).on('entity:map', function (e, data) {
-    var modelname = 'dive';
-    var layername = `${modelname}_layer`;
-	var url = window.SETTINGS.urls[layername];
-    var loaded_dive = false;
-    var map = data.map;
-    var style = L.Util.extend({ clickable: false },
-        window.SETTINGS.map.styles[modelname] || {});
-    // Show dive layer in application maps
-	var layer = new L.ObjectsLayer(null, {
-		modelname: modelname,
-		style: style,
-	});
-
-    if (data.modelname != modelname){
-	    map.layerscontrol.addOverlay(layer, tr('Diving'), tr('Diving'));
-    };
-
-    map.on('layeradd', function (e) {
-        var options = e.layer.options || { 'modelname': 'None' };
-        if (! loaded_dive) {
-            if (options.modelname == modelname && options.modelname != data.modelname) {
-                e.layer.load(url);
-                loaded_dive = true;
-            }
-        }
-    });
-});
-
 /*
 $(window).on('entity:map', function (e, data) {
     var modelname = 'dive';
