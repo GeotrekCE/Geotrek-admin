e7452a85aa4037746c1bcee0cb8f9891783c3c29
kayarhabby <pasciahershekaya@gmail.com>
2025-08-06 12:10:04 +0200

    update

--- geotrek/core/widgets.py
+++ geotrek/core/widgets.py
@@ -1,22 +1,121 @@
 """
-
-We distinguish two types of widgets : Geometry and Topology.
-
-Geometry widgets receive a WKT string, deserialized by GEOS.
-Leaflet.Draw is used for edition.
-
-Topology widgets receive a JSON string, deserialized by Topology.deserialize().
-Geotrek custom code is used for edition.
+Widgets de topologie indépendants avec leur propre logique de rendu.
+Basés sur BaseGeometryWidget, complètement séparés de MapWidget.
 """
 
 import json
-
+from django.core import validators
+from django.template.defaultfilters import slugify
+from django.contrib.gis.forms.widgets import BaseGeometryWidget
 from mapentity.widgets import MapWidget
 
 from .models import Topology
 
 
-class SnappedLineStringWidget(MapWidget):
+class BaseTopologyWidget(BaseGeometryWidget):
+    """
+    Widget de base pour les topologies, complètement indépendant de MapWidget.
+    Utilise uniquement le template topology_widget_fragment.html.
+    """
+    template_name = "core/topology_widget_fragment.html"
+    display_raw = False
+    modifiable = True
+    is_line_topology = False
+    is_point_topology = False
+
+    def serialize(self, value):
+        """Sérialise une topologie en JSON."""
+        return value.serialize() if value else ""
+
+    def deserialize(self, value):
+        """Désérialise une valeur en objet Topology."""
+        if isinstance(value, int):
+            return Topology.objects.get(pk=value)
+        try:
+            return Topology.deserialize(value)
+        except ValueError:
+            return None
+
+    def _get_topology_attrs(self, name, attrs=None):
+        """
+        Prépare les attributs nécessaires pour le rendu du template de topologie.
+        Reprend la logique de MapWidget mais pour les topologies.
+        """
+        attrs = attrs or {}
+
+        # Génération des IDs pour les éléments HTML et JavaScript
+        map_id_css = slugify(attrs.get('id', name))
+        map_id = map_id_css.replace('-', '_')
+
+        attrs.update({
+            'id': map_id,
+            'id_css': map_id_css,
+            'id_map': map_id_css + '_map',
+            'modifiable': self.modifiable,
+            'is_line_topology': self.is_line_topology,
+            'is_point_topology': self.is_point_topology,
+            'target_map': attrs.get('target_map', getattr(self, 'target_map', None)),
+        })
+        return attrs
+
+    def get_context(self, name, value, attrs):
+        """
+        Prépare le contexte pour le rendu du template topology_widget_fragment.html.
+        """
+        # Gestion des valeurs vides
+        value = None if value in validators.EMPTY_VALUES else value
+
+        # Récupération du contexte parent
+        context = super().get_context(name, value, attrs)
+
+        # Ajout des attributs spécifiques à la topologie
+        topology_attrs = self._get_topology_attrs(name, attrs)
+        context.update(topology_attrs)
+
+        # Ajout de la valeur sérialisée pour le template
+        context['serialized'] = self.serialize(value)
+
+        return context
+
+    def render(self, name, value, attrs=None, renderer=None):
+        """
+        Rendu du widget de topologie.
+        Gère la conversion des valeurs entières en objets Topology.
+        """
+        if isinstance(value, int):
+            try:
+                value = Topology.objects.get(pk=value)
+            except Topology.DoesNotExist:
+                value = None
+
+        # Mise à jour des attributs avec les flags de topologie
+        attrs = attrs or {}
+        attrs.update({
+            'is_line_topology': self.is_line_topology,
+            'is_point_topology': self.is_point_topology,
+        })
+
+        return super().render(name, value, attrs, renderer)
+
+
+class LineTopologyWidget(BaseTopologyWidget):
+
+    is_line_topology = True
+
+
+class PointTopologyWidget(BaseTopologyWidget):
+
+    is_point_topology = True
+
+
+class PointLineTopologyWidget(BaseTopologyWidget):
+
+    is_line_topology = True
+    is_point_topology = True
+
+
+# Garder SnappedLineStringWidget tel quel - pas de modification
+class SnappedLineStringWidget(BaseTopologyWidget):
     # geometry_field_class = "MapEntity.GeometryField.GeometryFieldSnap"
 
     def serialize(self, value):
@@ -32,52 +131,3 @@ class SnappedLineStringWidget(MapWidget):
             value = json.loads(value)
             value = value["geom"]
         return super().deserialize(value)
-
-
-class BaseTopologyWidget(MapWidget):
-    """A widget allowing to create topologies on a map."""
-
-    template_name = "core/topology_widget_fragment.html"
-    # geometry_field_class = "MapEntity.GeometryField.TopologyField"
-    is_line_topology = False
-    is_point_topology = False
-
-    def serialize(self, value):
-        return value.serialize() if value else ""
-
-    def deserialize(self, value):
-        if isinstance(value, int):
-            return Topology.objects.get(pk=value)
-        try:
-            return Topology.deserialize(value)
-        except ValueError:
-            return None
-
-    def render(self, name, value, attrs=None, renderer=None):
-        """Renders the fields. Parent class calls `serialize()` with the value."""
-        if isinstance(value, int):
-            value = Topology.objects.get(pk=value)
-        attrs = attrs or {}
-        attrs.update(
-            is_line_topology=self.is_line_topology,
-            is_point_topology=self.is_point_topology,
-        )
-        return super().render(name, value, attrs, renderer)
-
-
-class LineTopologyWidget(BaseTopologyWidget):
-    """A widget allowing to select a list of paths."""
-
-    is_line_topology = True
-
-
-class PointTopologyWidget(BaseTopologyWidget):
-    """A widget allowing to point a position with a marker."""
-
-    is_point_topology = True
-
-
-class PointLineTopologyWidget(PointTopologyWidget, LineTopologyWidget):
-    """A widget allowing to point a position with a marker or a list of paths."""
-
-    pass
