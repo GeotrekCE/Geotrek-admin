f630f192fac48ae74d7037c2815b20615bfa5e4b
Jean-Etienne Castagnede <j.e.castagnede@gmail.com>
2025-09-23 16:08:49 +0200

    bump mapentity to 8.14.3 (#4968)
    * bump mapentity to 8.14.3
    
    * Fix deps
    
    * bump mapentity to 8.14.3

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     scripts=["manage.py"],
     install_requires=[
         "Django==4.2.*",
-        "mapentity@git+https://github.com/makinacorpus/django-mapentity.git@master#egg=mapentity",
+        "mapentity",
         "chardet",
         "cairosvg",
         "cairocffi",
