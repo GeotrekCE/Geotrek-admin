e34c3064b3252d36bdac1a3bec973e95e037062d
PartyNell <nell.party16@gmail.com>
2025-10-02 09:21:03 +0200

    add columns cities, coord_x and coord_y into CSV export for feedback

--- geotrek/feedback/views.py
+++ geotrek/feedback/views.py
@@ -12,6 +12,7 @@ from mapentity import views as mapentity_views
 from rest_framework.authentication import BasicAuthentication, SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
 
+from geotrek.common.functions import ST_X, ST_Y
 from geotrek.common.mixins.views import CustomColumnsMixin
 from geotrek.common.viewsets import GeotrekMapentityViewSet
 
@@ -42,7 +43,14 @@ class ReportList(CustomColumnsMixin, mapentity_views.MapEntityList):
     searchable_columns = ["id", "eid"]
 
     def get_queryset(self):
-        qs = super().get_queryset()  # Filtered by FilterSet
+        qs = (
+            super()
+            .get_queryset()
+            .annotate(
+                coord_x=ST_X(Transform("geom", 4326)),
+                coord_y=ST_Y(Transform("geom", 4326)),
+            )
+        )  # Filtered by FilterSet
         if (
             settings.SURICATE_WORKFLOW_ENABLED
             and not settings.SURICATE_WORKFLOW_SETTINGS.get("SKIP_MANAGER_MODERATION")
@@ -96,6 +104,9 @@ class ReportFormatList(mapentity_views.MapEntityFormat, ReportList):
         "date_update",
         "current_user",
         "provider",
+        "cities",
+        "coord_x",
+        "coord_y",
     ]
 
     def get_columns(self):
