fa486492e2a82ad3dc0591264b319a7fa7c1b8fa
J-E Castagnede <j.e.castagnede@gmail.com>
2025-11-14 16:35:39 +0100

    perf(api): improve zoning

--- geotrek/zoning/forms.py
+++ geotrek/zoning/forms.py
@@ -7,28 +7,30 @@ from geotrek.zoning.models import City, District
 
 
 class MapFilterForm(forms.Form):
-    if settings.LAND_BBOX_CITIES_ENABLED and City.objects.exists():
-        bbox_city = forms.ModelChoiceField(
-            queryset=City.objects.all(),
-            widget=autocomplete.ModelSelect2(
-                url="zoning:city-autocomplete-bbox",
-                attrs={
-                    "data-placeholder": _("City"),
-                    #'data-theme': 'bootstrap4'
-                },
-            ),
-            required=False,
-        )
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if settings.LAND_BBOX_CITIES_ENABLED and City.objects.exists():
+            self.fields["bbox_city"] = forms.ModelChoiceField(
+                queryset=City.objects.all(),
+                widget=autocomplete.ModelSelect2(
+                    url="zoning:city-autocomplete-bbox",
+                    attrs={
+                        "data-placeholder": _("City"),
+                        #'data-theme': 'bootstrap4'
+                    },
+                ),
+                required=False,
+            )
 
-    if settings.LAND_BBOX_DISTRICTS_ENABLED and District.objects.exists():
-        bbox_district = forms.ModelChoiceField(
-            queryset=District.objects.all(),
-            widget=autocomplete.ModelSelect2(
-                url="zoning:district-autocomplete-bbox",
-                attrs={
-                    "data-placeholder": _("District"),
-                    #'data-theme': 'bootstrap4'
-                },
-            ),
-            required=False,
-        )
+        if settings.LAND_BBOX_DISTRICTS_ENABLED and District.objects.exists():
+            self.fields["bbox_district"] = forms.ModelChoiceField(
+                queryset=District.objects.all(),
+                widget=autocomplete.ModelSelect2(
+                    url="zoning:district-autocomplete-bbox",
+                    attrs={
+                        "data-placeholder": _("District"),
+                        #'data-theme': 'bootstrap4'
+                    },
+                ),
+                required=False,
+            )
