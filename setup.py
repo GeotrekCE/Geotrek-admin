b3a681901474e87ea828d7204d56c2e19f4f447e
PartyNell <nell.party@makina-corpus.com>
2025-12-02 16:25:14 +0100

    Use batch_editing branch of mapentity

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     scripts=["manage.py"],
     install_requires=[
         "Django==5.2.*",
-        "mapentity",
+        "mapentity@git+https://github.com/makinacorpus/django-mapentity@batch_editing#egg=mapentity",
         "chardet",
         "cairosvg",
         "cairocffi",
