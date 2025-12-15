07714ce798bbbea162d1ce5a811acbad27d835f1
PartyNell <nell.party@makina-corpus.com>
2025-12-15 09:25:38 +0100

    Change warning message for multi action when a selected items isn't in the same structure than the user

--- geotrek/common/mixins/views.py
+++ geotrek/common/mixins/views.py
@@ -259,7 +259,7 @@ class BelongStructureMixin:
         if not superuser and filtered_queryset.count() != queryset.count():
             messages.warning(
                 self.request,
-                _("Access to the requested resource is restricted by structure"),
+                _("Access is restricted because not all selected items belong to your structure. Use the structure filter to select only authorized items."),
             )
             return HttpResponseRedirect(self.get_redirect_url())
 
