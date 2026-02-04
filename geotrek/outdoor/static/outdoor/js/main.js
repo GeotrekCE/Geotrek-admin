64865d0adc60651bc5af03dca6fc8fa880e95fe5
J-E Castagnede <j.e.castagnede@gmail.com>
2026-02-04 22:44:26 +0100

    refactor: migrate from Leaflet to Maplibre for GISModelAdmin integration

--- geotrek/outdoor/static/outdoor/js/main.js
+++ geotrek/outdoor/static/outdoor/js/main.js
@@ -1,32 +1,3 @@
-window.addEventListener('entity:map', () => {
-    const map = window.MapEntity.currentMap.map;
-
-    ['site', 'course'].forEach((modelname) => {
-        const layername = `${modelname}_layer`;
-        const layerUrl = window.SETTINGS.urls[layername];
-        let style = window.SETTINGS.map.styles[modelname] ?? window.SETTINGS.map.styles['autres'];
-        const nameHTML = tr(modelname);
-        const category = tr('Outdoor');
-        let primaryKey = generateUniqueId();
-
-        // Show site and course layers in application maps
-        const objectsLayer = new MaplibreObjectsLayer(null, {
-            style: style,
-            modelname: modelname,
-            readonly: false,
-            nameHTML: nameHTML,
-            category: category,
-            primaryKey: primaryKey,
-            dataUrl: layerUrl,
-            isLazy: true
-        });
-
-        objectsLayer.initialize(map.getMap());
-        objectsLayer.registerLazyLayer(modelname, category, nameHTML, primaryKey, layerUrl);
-
-    });
-});
-
 $(window).on('entity:view:add entity:view:update', function (e, data) {
     if (data.modelname == 'site') {
         // Refresh site types by practice
