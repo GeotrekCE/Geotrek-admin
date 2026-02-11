aded90d7c3e942ca6cd8906d35018bfae6090974
J-E Castagnede <j.e.castagnede@gmail.com>
2026-02-11 11:47:59 +0100

    feat: integrate django-mapbox-baselayer and update map configuration

--- geotrek/settings/base.py
+++ geotrek/settings/base.py
@@ -320,6 +320,7 @@ PROJECT_APPS += (
     "mptt",
     "treebeard",
     "maintenance_mode",
+    "mapbox_baselayer",
 )
 
 INSTALLED_APPS = (
@@ -515,19 +516,7 @@ MAPLIBRE_CONFIG_OVERRIDES = {
     "DEFAULT_CENTER": CENTER,
     "DEFAULT_ZOOM": 5,
     "SCALE": "metric",
-    "BOUNDS": BOUNDS,
-    "TILES": [
-        (
-            "OpenStreetMap",
-            "https://a.tile.openstreetmap.org/{z}/{x}/{y}.png",
-            '&copy; <a href="https://www.openstreetmap.org/copyright">Contributeurs d\'OpenStreetMap</a>',
-        ),
-        (
-            "OpenTopoMap",
-            "https://a.tile.opentopomap.org/{z}/{x}/{y}.png",
-            'map data: © <a href="https://openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | map style: © <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
-        ),
-    ],
+    "BOUNDS": BOUNDS
 }
 
 # define forced layers from LEAFLET_CONFIG when map center in polygon
