bfc78e452b2b87391c42244728c050643ff00c0e
Justine Fricou <justine.fricou@gmail.com>
2026-02-13 14:38:49 +0100

    Remove useless line

--- geotrek/core/filters.py
+++ geotrek/core/filters.py
@@ -26,7 +26,6 @@ class ValidTopologyFilterSet(FilterSet):
             model = Topology
             fields = ["coupled", "is_valid_topology"]
 
-        coupled = BooleanFilter()
         is_valid_topology = BooleanFilter(
             label=_("Valid topology"), method="filter_valid_topology"
         )
