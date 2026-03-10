f681407b03e70e8d0e918704a83836afee400cd4
J-E Castagnede <j.e.castagnede@gmail.com>
2026-03-10 17:29:11 +0100

    fix tests

--- geotrek/core/managers.py
+++ geotrek/core/managers.py
@@ -7,6 +7,11 @@ class PathManager(models.Manager):
     # Use this manager when walking through FK/M2M relationships
     use_for_related_fields = True
 
+    def get_queryset(self):
+        """Hide all ``Path`` records that are not marked as visible."""
+        qs = super().get_queryset().filter(visible=True)
+        return qs
+
 
 class PathInvisibleManager(models.Manager):
     use_for_related_fields = True
