6875370080bf0aca2dd5200d517e23cce02aa0da
J-E Castagnede <j.e.castagnede@gmail.com>
2026-03-13 15:10:21 +0100

    chore: update Cypress version and add end-to-end tests

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,89 +1,15 @@
-Cypress.Commands.add('loginByCSRF', (username, password) => {
-    cy.request('/login/?next=/')
-      .its('body')
-      .then((body) => {
-        // we can use Cypress.$ to parse the string body
-        // thus enabling us to query into it easily
-        const $html = Cypress.$(body);
-        cy.request({
-          method: 'POST',
-          url: '/login/?next=/',
-          failOnStatusCode: true, // dont fail so we can make assertions
-          form: true, // we are submitting a regular form body
-          body: {
-            username,
-            password,
-            "csrfmiddlewaretoken": $html.find('input[name=csrfmiddlewaretoken]').val(), // insert this as part of form body
-         }
-        });
-      });
-    });
-
-Cypress.Commands.add('mockTiles', (username, password) => {
-    cy.intercept("https://*.tile.opentopomap.org/*/*/*.png", {fixture: "images/tile.png"}).as("tiles");
+Cypress.Commands.add('login', (username = 'admin', password = 'admin') => {
+  cy.session([username, password], () => {
+    cy.visit('/login/')
+    cy.get('input[name="username"]').type(username)
+    cy.get('input[name="password"]').type(password)
+    cy.get('form').submit()
+    cy.url().should('not.include', '/login/')
+  })
 });
 
-
-Cypress.Commands.add('setTinyMceContent', (tinyMceId, content) => {
-  cy.window().then((win) => {
-    const editor = win.tinymce.get(tinyMceId);
-    editor.setContent(content);
-  });
-});
-
-Cypress.Commands.add('getTinyMceContent', (tinyMceId, content) => {
-  cy.window().then((win) => {
-    const editor = win.tinymce.get(tinyMceId);
-    return editor.getContent();
-  });
-});
-
-Cypress.Commands.add('getCoordsOnMap', (pathPk, percentage) => {
-  cy.getPath(pathPk).then(path => {
-    let domPath = path.get(0);
-
-    // Get the coordinates relative to the map element
-    let pathLength = domPath.getTotalLength();
-    let lengthAtPercentage = percentage * pathLength / 100;
-    return domPath.getPointAtLength(lengthAtPercentage);
-  });
-});
-
-Cypress.Commands.add('getCoordsOnPath', (pathPk, percentage) => {
-  cy.getPath(pathPk).then(path => {
-    cy.getCoordsOnMap(pathPk, percentage).then(coordsOnMap => {
-      // Convert the coords so they are relative to the path
-      cy.getMap().then(map => {
-        let domMap = map.get(0);
-        let domPath = path.get(0);
-
-        // Get the coords of the map and the path relative to the root DOM element
-        let mapCoords = domMap.getBoundingClientRect();
-        let pathCoords = domPath.getBoundingClientRect();
-        let horizontalDelta = pathCoords.x - mapCoords.x;
-        let verticalDelta = pathCoords.y - mapCoords.y;
-
-        // Return the coords relative to the path element
-        return {
-          x: coordsOnMap.x - horizontalDelta,
-          y: coordsOnMap.y - verticalDelta,
-        }
-      });
-    });
-  });
-});
-
-Cypress.Commands.add('getMap', () => cy.get('[id="id_topology-map"]'));
-Cypress.Commands.add('getPath', pathPk => cy.get(`[data-test=pathLayer-${pathPk}]`));
-
-Cypress.Commands.add('clickOnPath', (pathPk, percentage) => {
-  // Get the coordinates of the click and execute it
-  cy.getCoordsOnPath(pathPk, percentage).then(clickCoords => {
-    let startTime;
-    cy.getPath(pathPk)
-    .then((path) => {startTime = performance.now(); return path})
-    .click(clickCoords.x, clickCoords.y, {force: true})
-    // Return startTime so it is yielded by the command
-    .then(() => startTime);
-  });
+Cypress.Commands.add('mockTiles', () => {
+    cy.intercept("https://*.openstreetmap.org/*/*/*.png", {fixture: "images/tile_osm.png"}).as("tiles_osm");
+    cy.intercept("https://*.tile.opentopomap.org/*/*/*.png", {fixture: "images/tile_otm.png"}).as("tiles_otm");
+    cy.intercept(/data\.geopf\.fr\/wmts\?LAYER=CADASTRALPARCELS/, {fixture: "images/tile_overlay.png"}).as("tiles_overlay");
 });
