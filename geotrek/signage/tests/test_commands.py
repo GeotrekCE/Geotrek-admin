6f223d50d2e5eabd817cd5dce7a9f8b04f3971c7
Justine Fricou <justine.fricou@gmail.com>
2026-06-25 10:47:50 +0200

    Delete obsolete unit tests

--- geotrek/signage/tests/test_commands.py
+++ geotrek/signage/tests/test_commands.py
@@ -312,22 +312,6 @@ class SignageCommandTest(TestCase):
                 f"Field '{element}' not found in data source", output.getvalue()
             )
 
-    def test_no_line_fail_rolling_back(self):
-        self.path.delete()
-        StructureFactory.create(name="structure")
-        filename = os.path.join(os.path.dirname(__file__), "data", "line.geojson")
-        output = StringIO()
-        with self.assertRaises(GEOSException):
-            call_command(
-                "loadsignage",
-                filename,
-                type_default="label",
-                name_default="name",
-                stdout=output,
-            )
-        self.assertIn("An error occured, rolling back operations.", output.getvalue())
-        self.assertEqual(Signage.objects.count(), 0)
-
     def test_update_same_eid(self):
         output = StringIO()
         filename = os.path.join(os.path.dirname(__file__), "data", "signage.shp")
