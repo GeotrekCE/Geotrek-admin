eda7a0856f4d23f2ce2eb6e4d9501457e63c58e0
Nell Party <nell.party@makina-corpus.com>
2026-06-30 09:52:15 +0200

    Seperate write and read API on report GTAM serializer

--- geotrek/feedback/serializers.py
+++ geotrek/feedback/serializers.py
@@ -64,10 +64,38 @@ class ReportGeojsonSerializer(MapentityGeojsonModelSerializer):
 
 class ReportGTAMSerializer(rest_serializers.ModelSerializer):
     geom = GeometryField(precision=7, transform=settings.API_SRID)
-    activity = ReportActivityGTAMSerializer()
-    category = ReportCategoryGTAMSerializer()
-    problem_magnitude = ReportProblemMagnitudeGTAMSerializer()
-    status = ReportStatusGTAMSerializer()
+
+    # read-only
+    activity = ReportActivityGTAMSerializer(read_only=True)
+    category = ReportCategoryGTAMSerializer(read_only=True)
+    problem_magnitude = ReportProblemMagnitudeGTAMSerializer(read_only=True)
+    status = ReportStatusGTAMSerializer(read_only=True)
+
+    # write-only
+    activity_id = rest_serializers.PrimaryKeyRelatedField(
+        source="activity",
+        write_only=True,
+        allow_null=True,
+        queryset=feedback_models.ReportActivity.objects.all(),
+    )
+    category_id = rest_serializers.PrimaryKeyRelatedField(
+        source="category",
+        write_only=True,
+        allow_null=True,
+        queryset=feedback_models.ReportCategory.objects.all(),
+    )
+    problem_magnitude_id = rest_serializers.PrimaryKeyRelatedField(
+        source="problem_magnitude",
+        write_only=True,
+        allow_null=True,
+        queryset=feedback_models.ReportProblemMagnitude.objects.all(),
+    )
+    status_id = rest_serializers.PrimaryKeyRelatedField(
+        source="status",
+        write_only=True,
+        allow_null=True,
+        queryset=feedback_models.ReportStatus.objects.all(),
+    )
 
     class Meta:
         model = feedback_models.Report
@@ -78,10 +106,16 @@ class ReportGTAMSerializer(rest_serializers.ModelSerializer):
             "email",
             "comment",
             "geom",
+            # read-only
             "activity",
             "category",
             "problem_magnitude",
             "status",
+            # write-only
+            "activity_id",
+            "category_id",
+            "problem_magnitude_id",
+            "status_id",
         ]
         geo_field = "geom"
 
