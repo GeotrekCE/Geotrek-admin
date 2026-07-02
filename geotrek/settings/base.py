6a3e10e5fde8c3aec6dc469494449102235c443b
PartyNell <94644916+PartyNell@users.noreply.github.com>
2026-07-02 09:48:54 +0200

    Add REFRESH_TOKEN_LIFETIME to SIMPLE_JWT settings

--- geotrek/settings/base.py
+++ geotrek/settings/base.py
@@ -967,6 +967,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=3),
 }
 
 ALLOW_PATH_DELETION_TOPOLOGY = True
