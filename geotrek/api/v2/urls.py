4d610364df229c2f90acf7a6d5d1fbd4a53ddd80
Nell Party <nell.party@makina-corpus.com>
2026-07-15 11:00:48 +0200

    Remove JWT authentification endpoint from api v2

--- geotrek/api/v2/urls.py
+++ geotrek/api/v2/urls.py
@@ -237,9 +237,6 @@ _urlpatterns += [
     ),
     path("version", api_views.GeotrekVersionAPIView.as_view()),
     path("", include(router.urls)),
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
 ]
 if "geotrek.flatpages" in settings.INSTALLED_APPS:
     _urlpatterns += [
