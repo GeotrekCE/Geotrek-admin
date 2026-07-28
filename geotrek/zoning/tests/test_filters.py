73c9a97f657944a2f6c130986dd3d6c32773d4a6
J-E Castagnede <j.e.castagnede@gmail.com>
2026-07-28 17:28:48 +0200

    feat(vigilance): rename 'Possibly practicable' to 'Partially practicable' in choices and translations

--- geotrek/zoning/tests/test_filters.py
+++ geotrek/zoning/tests/test_filters.py
@@ -1,14 +1,22 @@
+import datetime
+
 from django.conf import settings
 from django.test import TestCase
+from django.views.generic.dates import timezone_today
 
 from geotrek.core.filters import PathFilterSet
 from geotrek.core.tests.factories import PathFactory
 from geotrek.trekking.tests.factories import TrekFactory
+from geotrek.zoning.choices import Practicability
+from geotrek.zoning.filters import VigilanceAreaFilterSet
+from geotrek.zoning.models import VigilanceArea
 from geotrek.zoning.tests.factories import (
     CityFactory,
     DistrictFactory,
     RestrictedAreaFactory,
     RestrictedAreaTypeFactory,
+    VigilanceAreaFactory,
+    VigilanceAreaTypeFactory,
 )
 
 
@@ -120,3 +128,61 @@ class ZoningFilterTest(TestCase):
         )
 
         self.assertEqual(len(filter.qs), 0)
+
+
+class VigilanceAreaFilterTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.today = timezone_today()
+        cls.yesterday = cls.today - datetime.timedelta(days=1)
+        cls.tomorrow = cls.today + datetime.timedelta(days=1)
+        cls.area_type = VigilanceAreaTypeFactory.create()
+        cls.va = VigilanceAreaFactory.create(
+            name="Alpha Area",
+            vigilance_area_type=cls.area_type,
+            start_date=cls.yesterday,
+            end_date=cls.tomorrow,
+            practicability=Practicability.PRACTICABLE,
+            published=True,
+        )
+
+    def test_filter_by_name(self):
+        qs = VigilanceArea.objects.all()
+        f = VigilanceAreaFilterSet(data={"name": "Alpha"}, queryset=qs)
+        self.assertIn(self.va, f.qs)
+        f = VigilanceAreaFilterSet(data={"name": "Beta"}, queryset=qs)
+        self.assertNotIn(self.va, f.qs)
+
+    def test_filter_by_practicability(self):
+        qs = VigilanceArea.objects.all()
+        f = VigilanceAreaFilterSet(
+            data={"practicability": Practicability.PRACTICABLE}, queryset=qs
+        )
+        self.assertIn(self.va, f.qs)
+        f = VigilanceAreaFilterSet(
+            data={"practicability": Practicability.NOT_PRACTICABLE}, queryset=qs
+        )
+        self.assertNotIn(self.va, f.qs)
+
+    def test_filter_by_vigilance_area_type(self):
+        qs = VigilanceArea.objects.all()
+        f = VigilanceAreaFilterSet(
+            data={"vigilance_area_type": [self.area_type.pk]}, queryset=qs
+        )
+        self.assertIn(self.va, f.qs)
+
+    def test_filter_by_period_active(self):
+        qs = VigilanceArea.objects.all()
+        f = VigilanceAreaFilterSet(data={"period_active": "true"}, queryset=qs)
+        self.assertIn(self.va, f.qs)
+
+    def test_filter_by_dates(self):
+        qs = VigilanceArea.objects.all()
+        f = VigilanceAreaFilterSet(
+            data={"before": self.today + datetime.timedelta(days=2)}, queryset=qs
+        )
+        self.assertIn(self.va, f.qs)
+        f = VigilanceAreaFilterSet(
+            data={"after": self.tomorrow + datetime.timedelta(days=5)}, queryset=qs
+        )
+        self.assertNotIn(self.va, f.qs)
