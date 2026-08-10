9fa472580791fc51b0b18ae5c5718c769f8ae1c2
Nell Party <nell.party@makina-corpus.com>
2026-08-10 16:06:29 +0200

    Fix zoning SQL cleanup

--- geotrek/zoning/templates/zoning/sql/pre_10_cleanup.sql
+++ geotrek/zoning/templates/zoning/sql/pre_10_cleanup.sql
@@ -1,5 +1,6 @@
 -- Clean views before migration
 
-DROP VIEW IF EXISTS f_v_zonage CASCADE;
-DROP VIEW IF EXISTS f_v_secteur CASCADE;
-DROP VIEW IF EXISTS f_v_commune CASCADE;
+DROP VIEW IF EXISTS v_cities CASCADE;
+DROP VIEW IF EXISTS v_districts CASCADE;
+DROP VIEW IF EXISTS v_restrictedareas CASCADE;
+DROP VIEW IF EXISTS v_vigilanceareas CASCADE;
